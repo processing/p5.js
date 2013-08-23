e64b08f760bd0b01d51715114876dd36716e4aa2
lauren mccarthy <laurmccarthy@gmail.com>
2013-08-23 13:00:30 -0400

    fisinhing ch10 examples

--- examples/learningprocessing/chp10/example_10_10/catcher.js
+++ examples/learningprocessing/chp10/example_10_10/catcher.js
@@ -4,17 +4,36 @@
 
 // Ported by Lauren McCarthy
 
-// Example 10-8: Fancier looking raindrop
+// Example 10-10: The raindrop catching game
 
-var setup = function() {
-  background(255);
-  smooth();
+function Catcher(tempR) {
+  this.r = tempR; // radius
+  this.col = [50, 10, 10, 150]; // color
+  this.x = 0; // location
+  this.y = 0;
+}
+  
+Catcher.prototype.setLocation = function(tempX, tempY) {
+  this.x = tempX;
+  this.y = tempY;
 };
 
-var draw = function() {
-  for (var i = 2; i < 8; i++ ) {
-    noStroke();
-    fill(0);
-    ellipse(width/2, height/2 + i*4, i*2, i*2);
+Catcher.prototype.display = function() {
+  stroke(0);
+  fill(this.col);
+  ellipse(this.x, this.y, this.r*2, this.r*2);
+};
+
+// A function that returns true or false based on
+// if the catcher intersects a raindrop
+Catcher.prototype.intersect = function(d) {
+  // Calculate distance
+  var distance = dist(this.x, this.y, d.x, d.y); 
+  
+  // Compare distance to sum of radii
+  if (distance < this.r + d.r) { 
+    return true;
+  } else {
+    return false;
   }
 };
