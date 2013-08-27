7fbadde90d670d3c6705ea4eab344bf3facd1ca1
lauren mccarthy <laurmccarthy@gmail.com>
2013-08-27 07:43:41 -0400

    adding push and pop style closes #33

--- experiments/lauren/sketch.js
+++ experiments/lauren/sketch.js
@@ -6,6 +6,33 @@ var setup = function() {
 
 var draw = function() {
 	console.log(focused);
+	background(255);
+
+	ellipse(0, 50, 33, 33);  // Left circle
+
+	pushStyle();  // Start a new style
+		strokeWeight(10);
+		fill(204, 153, 0);
+		ellipse(50, 50, 33, 33);  // Middle circle
+	popStyle();  // Restore original style
+
+	ellipse(100, 50, 33, 33);  // Right circle
+
+	ellipse(0, 50, 33, 33);  // Left circle
+
+	pushStyle();  // Start a new style
+		strokeWeight(10);
+		fill(204, 153, 0);
+		ellipse(33, 50, 33, 33);  // Left-middle circle
+
+		pushStyle();  // Start another new style
+			stroke(0, 102, 153);
+			ellipse(66, 50, 33, 33);  // Right-middle circle
+		popStyle();  // Restore previous style
+
+	popStyle();  // Restore original style
+
+	ellipse(100, 50, 33, 33);  // Right circle
 };
 
 var keyPressed = function() {
