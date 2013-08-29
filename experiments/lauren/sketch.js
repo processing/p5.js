3a1f99c56ec63ecb890cc984b07a1a3fc264aa8c
lauren mccarthy <laurmccarthy@gmail.com>
2013-08-29 15:32:02 -0400

    checking on keycode and str stuff

--- experiments/lauren/sketch.js
+++ experiments/lauren/sketch.js
@@ -2,53 +2,7 @@
 
 var setup = function() {
 	console.log("setup");
-// 	var a=-200, b=-40, c=-90;
-// 	var sa = nf(a, 10);
-// 	println(sa);  // Prints "0000000200"
-// 	var sb = nf(b, 5);
-// 	println(sb);  // Prints "00040"
-// 	var sc = nf(c, 3);
-// 	println(sc);  // Prints "090"
 
-// 	var d = -200.94, e = -40.2, f = -9.012;
-// 	var sd = nf(d, 5, 4);
-// 	println(sd);  // Prints "0000000200.9400"
-
-
-// 	var se = nf(e, 5, 4);
-// 	println(se);  // Prints "0000000200.9400"
-
-
-// 	var sf = nf(f, 5, 4);
-// 	println(sf);  // Prints "0000000200.9400"
-
-// 	// var se = nf(e, 5, 3);
-// 	// println(se);  // Prints "00040.200"
-// 	// var sf = nf(f, 3, 5);
-// 	// println(sf);  // Prints "009.01200"
-
-// 	println(nf([d, e, f], 5, 4));
-// 
-	// var a=200, b=-40, c=90; 
-	// var sa = nfs(a, 10); 
-	// println(sa);  // Prints "+0000000200" 
-	// var sb = nfs(b, 5); 
-	// println(sb);  // Prints "-00040" 
-	// var sc = nfs(c, 3); 
-	// println(sc);  // Prints "+090" 
-	 
-	// var d = -200.94, e = 40.2, f = -9.012; 
-	// var sd = nfs(d, 10, 4); 
-	// println(sd);  // Prints "-0000000200.9400" 
-	// var se = nfs(e, 5, 3); 
-	// println(se);  // Prints "+00040.200" 
-	// var sf = nfs(f, 3, 5); 
-	// println(sf);  // Prints "-009.01200" 
-
-	// console.log(nfs([d, e, f], 5, 3));
-	var a1 = [ " inconsistent ", " spacing" ];  // Note spaces
-	var a2 = trim(a1);
-	println(a2);
 
 };
 
