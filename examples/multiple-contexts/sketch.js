69d5cf4b5c1fdd5739d2bf94e3395d2f3527952b
Evelyn Eastmond <evhan55@gmail.com>
2014-01-28 11:06:04 -0500

    Adding a multiple contexts example to test mouse input

---
+++ examples/multiple-contexts/sketch.js
@@ -0,0 +1,20 @@
+var canvas1;
+var canvas2;
+
+function setup() {
+  canvas1 = createGraphics(200, 200);
+  canvas1.position(50, 50);
+
+  canvas2 = createGraphics(600, 400);
+  canvas2.position(300, 50);
+};
+
+function draw() {
+  context(canvas1);
+  background(120, 180, 200);
+  ellipse(mouseX, mouseY, 100, 100);
+
+  context(canvas2);
+  background(50, 120, 80);
+  rect(mouseX, mouseY, width / 2, height / 2);
+};
