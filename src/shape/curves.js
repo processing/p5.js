9f8711b74a8147662d6a6c603bb0b8750b37f3d4
Harsh Agrawal <h.agrawal092@gmail.com>
2014-02-28 00:59:43 +0530

    Implemented bezierPoint and bezierTangent function in curves.js

--- src/shape/curves.js
+++ src/shape/curves.js
@@ -18,14 +18,12 @@ define(function (require) {
 
   };
 
-  Processing.prototype.bezierPoint = function() {
-    // TODO
-
+  Processing.prototype.bezierPoint = function(a, b, c, d, t) {
+    return (1 - t) * (1 - t) * (1 - t) * a + 3 * (1 - t) * (1 - t) * t * b + 3 * (1 - t) * t * t * c + t * t * t * d;
   };
 
-  Processing.prototype.bezierTangent = function() {
-    // TODO
-
+  Processing.prototype.bezierTangent = function(a, b, c, d, t) {
+    return (3 * t * t * (-a + 3 * b - 3 * c + d) + 6 * t * (a - 2 * b + c) + 3 * (-a + b));
   };
 
   Processing.prototype.curve = function() {
