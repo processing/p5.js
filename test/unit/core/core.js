081c935bffa9f5dd5229a4f2cbd4516b06e33b1a
Evelyn Eastmond <evhan55@gmail.com>
2014-03-06 11:46:22 -0500

    adding TDD style tests to Core

--- test/unit/core/core.js
+++ test/unit/core/core.js
@@ -1,3 +1,15 @@
-describe('Core', function() {
+suite('Core', function(){
+
+  setup(function(){
+    // ...
+  });
+
+  suite('sampleTest', function(){
+
+    test('1 should not equal 2', function(){
+      assert.equal(1, 2);
+    });
+
+  });
 
 });
