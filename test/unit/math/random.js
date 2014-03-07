3f18082282d0c6eb98fe742fb2be5e6a9177c0e9
Evelyn Eastmond <evhan55@gmail.com>
2014-03-07 00:48:21 -0500

    fixing TDD random tests

--- test/unit/math/random.js
+++ test/unit/math/random.js
@@ -1,43 +1,44 @@
 suite('Random', function() {
 
-  var random = Processing.prototype.random();
+  var random = Processing.prototype.random;
+  var result;
 
   suite('random()', function() {
 
+    setup(function() {
+      result = random();
+    });
+
     test('should return a number', function() {
-      assert.typeOf(random, 'number');
+      assert.typeOf(result, 'number');
     });
 
   });
 
-  suite('random(10)', function() {
+  suite('random(5)', function() {
 
-    var result = random(10);
-
-    test('should return a number >= 0', function() {
-      assert.isTrue(result >= 0);
+    setup(function() {
+      result = random(5);
     });
 
-    test('should return a number < 10', function() {
-      assert.isTrue(result < 10);
+    test('should return a number 0 <= n < 5', function() {
+      assert.isTrue(result >= 0);
+      assert.isTrue(result < 5);
     });
 
   });
 
   suite('random(1, 10)', function() {
 
-    var result = random(1, 10);
-
-    test('should return a number >= 1', function() {
-      assert.isTrue(result >= 1);
+    setup(function() {
+      result = random(1, 10);
     });
 
-    test('should return a number < 10', function() {
+    test('should return a number 1 <= n < 10', function() {
+      assert.isTrue(result >= 1);
       assert.isTrue(result < 10);
     });
 
   });
 
 });
-
-
