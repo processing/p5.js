170a08aa10a986c5ca4cfe94ad496e2e0be76729
Daniel Shiffman <daniel@shiffman.net>
2014-03-13 15:53:31 -0400

    unit tests for noise

---
+++ test/unit/math/noise.js
@@ -0,0 +1,23 @@
+suite('Noise', function() {
+  
+  // This could use some better  testing!
+  // Just checking that we get an actual number now
+  // 1D noise only
+  suite('Processing.prototype.noise', function() {
+    var noise = Processing.prototype.noise;
+
+    var result;
+    suite('noise()', function() {
+      setup(function() {
+        result = noise(0);
+      });
+      test('should return a number', function() {
+        assert.typeOf(result, 'number');
+      });
+      test('should return a number 0 < n < 1', function() {
+        assert.isTrue(result > 0);
+        assert.isTrue(result < 1);
+      });
+    });
+  });
+});
