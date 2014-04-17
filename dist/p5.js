9536405c7e2ad9ab06b64765277b9cacf62de208
lauren mccarthy <laurmccarthy@gmail.com>
2014-04-17 18:48:59 -0400

    print mapping to sketch line now closes #154

--- dist/p5.js
+++ dist/p5.js
@@ -2636,11 +2636,12 @@ var outputimage = function (require, core) {
 var outputtext_area = function (require, core) {
         'use strict';
         var p5 = core;
-        p5.prototype.print = function () {
-            if (window.console && console.log) {
-                console.log.apply(console, arguments);
-            }
-        };
+        if (window.console && console.log) {
+            p5.prototype.print = console.log.bind(console);
+        } else {
+            p5.prototype.print = function () {
+            };
+        }
         p5.prototype.println = p5.prototype.print;
         return p5;
     }({}, core);
