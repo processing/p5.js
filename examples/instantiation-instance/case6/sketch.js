69e91999fb72a0e92bcf125b7da97b3a9bccb36c
lauren mccarthy <laurmccarthy@gmail.com>
2014-04-19 12:29:19 -0400

    examlpes

---
+++ examples/instantiation-instance/case6/sketch.js
@@ -0,0 +1,53 @@
+var s0 = function( sketch ) {
+
+  var gray = 0; 
+
+  sketch.setup = function() {
+    sketch.createCanvas(400, 400);
+  };
+
+  sketch.draw = function() {
+    sketch.background(gray);
+    sketch.rect(sketch.width/2, sketch.height/2, 50, 50);
+  };
+
+  sketch.mousePressed = function() {
+    gray += 10;
+  };
+
+  sketch.mouseMoved = function() {
+    console.log("sketch0 x:"+sketch.mouseX+" y:"+sketch.mouseY);
+  };
+};
+
+var myp5_0 = new p5(s0, 'canvas0');
+
+
+
+var s1 = function( sketch ) {
+
+  var gray = 0; 
+
+  sketch.setup = function() {
+    sketch.createCanvas(400, 400);
+  };
+
+  sketch.draw = function() {
+    sketch.background(255, 0, 100);
+    sketch.fill(gray);
+    sketch.noStroke();
+    sketch.rect(sketch.width/2, sketch.height/2, 50, 50);
+  };
+
+  sketch.mousePressed = function() {
+    gray += 10;
+  };
+
+  sketch.mouseMoved = function() {
+    console.log("sketch1 x:"+sketch.mouseX+" y:"+sketch.mouseY);
+  };
+};
+
+
+var myp5_1 = new p5(s1, 'canvas1');
+
