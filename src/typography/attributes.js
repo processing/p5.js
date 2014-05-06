988a884669a2ae7fff653cbcfdef91be35f13c44
lauren mccarthy <laurmccarthy@gmail.com>
2014-05-06 13:59:47 -0400

    removing ()"

--- src/typography/attributes.js
+++ src/typography/attributes.js
@@ -13,7 +13,7 @@ define(function (require) {
   /**
    * Sets the current alignment for drawing text. The parameters LEFT, CENTER, and RIGHT set the display characteristics of the letters in relation to the values for the x and y parameters of the text() function. 
    * 
-   * @method textAlign()
+   * @method textAlign
    * @param {Number/Constant} alignX horizontal alignment, either LEFT, CENTER, or RIGHT
    */
   p5.prototype.textAlign = function(a) {
@@ -25,7 +25,7 @@ define(function (require) {
   /**
    * Calculates and returns the height of any character or text string.
    *
-   * @method textHeight()
+   * @method textHeight
    * @param {String} str the String of characters to measure
    */
   p5.prototype.textHeight = function(s) {
@@ -35,7 +35,7 @@ define(function (require) {
   /**
    * Sets the spacing between lines of text in units of pixels. This setting will be used in all subsequent calls to the text() function. 
    *
-   * @method textLeading()
+   * @method textLeading
    * @param {Number} leading the size in pixels for spacing between lines
    */
   p5.prototype.textLeading = function(l) {
@@ -45,7 +45,7 @@ define(function (require) {
   /**
    * Sets the current font size. This size will be used in all subsequent calls to the text() function. Font size is measured in units of pixels.
    *
-   * @method textSize()
+   * @method textSize
    * @param {Number} size the size of the letters in units of pixels
    */
   p5.prototype.textSize = function(s) {
@@ -55,7 +55,7 @@ define(function (require) {
   /**
    * Sets the style of the text to NORMAL, ITALIC, or BOLD. Note this is overriden by CSS styling.
    *
-   * @method textStyle()
+   * @method textStyle
    * @param {Number/Constant} style styling for text, either NORMAL, ITALIC, or BOLD
    */
   p5.prototype.textStyle = function(s) {
@@ -67,7 +67,7 @@ define(function (require) {
   /**
    * Calculates and returns the width of any character or text string.
    *
-   * @method textWidth()
+   * @method textWidth
    * @param {String} str the String of characters to measure
    */
   p5.prototype.textWidth = function(s) {
