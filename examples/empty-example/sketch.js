06872521ff7cbf88cffce66510878ebceb05a408
lauren mccarthy <laurmccarthy@gmail.com>
2014-05-13 10:12:05 -0400

    st

--- examples/empty-example/sketch.js
+++ examples/empty-example/sketch.js
@@ -1,19 +1,13 @@
-
-// Keyboard input
-// Draw a rectangle when any key is pressed
-
 function setup() {
-  createCanvas(600, 600);
-  smooth();
-  strokeWeight(8);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(204);
+  background(42, 169, 217);
 
-  if (isKeyPressed() == true) { // If the key is pressed,
-    line(120, 120, 480, 480); // draw a line
-  } else { // Otherwise,
-    rect(240, 240, 120, 120); // draw a rectangle
-  }
+  fill(242, 228, 21);
+  ellipse(100,100,100,100);
+
+  fill(162,217, 39);
+  rect(300,100,150,150);
 }
