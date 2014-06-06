e7553260fb719f8c55b24e1ffb0d6de4319fd671
lauren mccarthy <laurmccarthy@gmail.com>
2014-06-06 14:11:12 -0400

    adding capture

---
+++ examples/addons/p5.dom/capture/sketch.js
@@ -0,0 +1,17 @@
+var playing = false;
+var capture;
+
+
+
+function setup() {
+  createCanvas(340, 260);
+  capture = createCapture();
+  //capture.hide();
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(150);
+  video(capture, 10, 10, 320, 240);
+  filter('INVERT');
+}
