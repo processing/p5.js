bdc59bc5894b87b8683be9c8317e489138cd6d2d
Jason Sigal <jason.sigal@gmail.com>
2014-06-20 03:12:30 -0400

    example: control playback rate with mouseX

--- examples/addons/p5Sound/draw_entire_waveform/sketch.js
+++ examples/addons/p5Sound/draw_entire_waveform/sketch.js
@@ -4,27 +4,29 @@
 
 var soundFile;
 
-var p, peakCount, peakCountSlider;
+var p, peakCount;
 
 function preload() {
   // create a SoundFile with a reference to this and a path to a sound file
-  soundFile = new SoundFile('lucky_dragons_-_power_melody.wav');
-
+  soundFile = new SoundFile('lucky_dragons_-_power_melody.wav','lucky_dragons_-_power_melody.mp3');
 }
 
 function setup() {
   createCanvas(800, 400);
   background(0);
-  peakCountSlider = createSlider(8, 2000, 20);
-  p = createP('peak count: ' + peakCount);
+  p = createP('peaks to draw: ' + peakCount);
+  createP('Press Spacebar to play<br>Up/Down to change playback rate<br>R to reverse the buffer<br> S to stop all audio<br>P to pause')
 }
 
 
 function draw() {
   background(0);
 
-  peakCount = peakCountSlider.value();
-  p.html('peak count: ' + peakCount);
+  peakCount = map(this.mouseY, height, 0, 5, 2000); //peakCountSlider.value();
+  if (peakCount < 8) {
+    peakCount = 8;
+  }
+
 
   // only draw the waveform if the soundfile has loaded, otherwise it will throw an error
   if (soundFile.isLoaded()) {
@@ -40,17 +42,36 @@ function draw() {
   }
 
   drawCursor();
+  var newRate = Math.round(map(mouseX,0,800,-10,10))/10;
+  soundFile.rate(newRate);
+  p.html('MouseY = Visible Amplitude Peaks: ' + peakCount+'<br>MouseX = Playback Rate: '+newRate);
 }
+
 
 function drawCursor() {
   noStroke();
   fill(0,255,0);
   rect(map(soundFile.currentTime(), 0, soundFile.duration(), 0, width), 0, 5, height);
-
 }
 
+// Keyboard Controls
 function keyPressed(k) {
-  if (k.keyCode == 32) {
-    soundFile.play();
+  console.log(k.keyCode);
+  // "S" stops all sound sources
+  if (k.keyCode == 83) {
+    soundFile.stopAll();
   }
+  // "P" pauses current source
+  if (k.keyCode == 80) {
+    soundFile.pause();
+  }
+  // "R" reverses sound file
+  if (k.keyCode == 82) {
+    soundFile.reverse();
+  }
+  // Spacebar: play (creates a new sound source from the same buffer, does not stop playback)
+  if (k.keyCode == 32) {
+    soundFile.loop();
+  }
+
 }
