ab3b9fed4425fe6be048dceb38244cc78262bac2
Jason Sigal <jason.sigal@gmail.com>
2014-06-23 22:06:11 -0400

    cleanup

--- examples/addons/p5.sound/micLevel/sketch.js
+++ examples/addons/p5.sound/micLevel/sketch.js
@@ -6,14 +6,12 @@ function setup() {
    createCanvas(400,400);
    mic = new AudioIn();
    mic.on();
-
-   // the Mic reads level using p5.sound's Amplitude object.
-   // We can call methods on this Amplitude, just like any other.
-   mic.amplitude.toggleNormalize();
 }
 
 function draw() {
    background(0);
-   micLevel = mic.getLevel(.9);
+
+   // getLevel takes an optional smoothing value, or defaults to 0.0
+   micLevel = mic.getLevel();
    ellipse(width/2, height - micLevel*height, 100, 100);
 }
