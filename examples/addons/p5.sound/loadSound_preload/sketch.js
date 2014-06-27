bfa6849829c72af2f620b2923d8e6be6ccdd2442
Jason Sigal <jason.sigal@gmail.com>
2014-06-27 04:05:18 -0400

    loadSound works in preload or with callbacks + 1 example of each.

---
+++ examples/addons/p5.sound/loadSound_preload/sketch.js
@@ -0,0 +1,29 @@
+// Sound samples from Damscray - "Dancing Tiger",
+// Creative Commons BY-NC-SA
+
+
+function preload(){
+  sample1 = loadSound(['sound.ogg', 'sound.mp3']);
+}
+
+function setup() {
+  createCanvas(400,200);
+
+  text('File is ready!  Click to pause.', 50, 10);
+
+  sample1.rate(.8);
+  sample1.reverseBuffer();
+  sample1.loop();
+
+  peaks = sample1.getPeaks();
+  beginShape();
+  for (i = 0; i< peaks.length; i++){
+    vertex(map(i, 0, peaks.length, 0, width), map(peaks[i], -1, 1, height, 0) );
+  }
+  endShape();
+
+}
+
+function mousePressed(){
+  sample1.pause();
+}
