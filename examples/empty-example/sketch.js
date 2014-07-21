96977176bddfc1bf74908278d593970dd48cefff
lauren mccarthy <laurmccarthy@gmail.com>
2014-07-21 16:56:41 -0400

    updating layout for ref

--- examples/empty-example/sketch.js
+++ examples/empty-example/sketch.js
@@ -1,13 +1,8 @@
 function setup() {
+  // put setup code here
   createCanvas(600, 400);
 }
 
 function draw() {
-  background(42, 169, 217);
-
-  fill(242, 228, 21);
-  ellipse(100,100,100,100);
-
-  fill(162,217, 39);
-  rect(300,100,150,150);
+  // put drawing code here
 }
