75072b3936eb614beba0861d90a876dd35c8376e
Jason Sigal <jason.sigal@gmail.com>
2014-07-24 14:09:04 -0400

    updating examples

--- examples/addons/p5.sound/micFFT/sketch.js
+++ examples/addons/p5.sound/micFFT/sketch.js
@@ -8,18 +8,18 @@ function setup() {
    createCanvas(512,400);
    mic = new AudioIn();
    mic.on();
-   fft = new FFT();
+   fft = new FFT(.6, 1024);
    fft.setInput(mic);
 }
 
 function draw() {
-   background(255);
+   background(200);
 
    var spectrum = fft.processFreq(.01, 1024);
 
    beginShape();
    for (i = 0; i<spectrum.length; i++) {
-    vertex(i, height - spectrum[i]);
+    vertex(i, map(spectrum[i], -140, 0, height, 0) );
    }
    endShape();
 
