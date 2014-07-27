11eb5b5d1b169823becc1efcfde4462195dadf61
lauren mccarthy <laurmccarthy@gmail.com>
2014-07-26 22:10:01 -0400

    adding mouseReleased, related to issue #270

--- examples/addons/p5.dom/instance/sketch.js
+++ examples/addons/p5.dom/instance/sketch.js
@@ -24,9 +24,12 @@ var s0 = function( sketch ) {
 
   sketch.keyPressed = function() {
     v.loop();
-    h1.html('click to pause');
+    h1.html('click here to pause');
   };
 
+  sketch.mousePressed = function() {
+    console.log('press')
+  }
 
 };
 
