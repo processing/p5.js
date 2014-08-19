2af92c02fac0d40e5f57be4f6ab4893dfdd9cdba
lauren mccarthy <laurmccarthy@gmail.com>
2014-08-19 19:03:28 -0400

    minor fix to string

--- src/data/string_functions.js
+++ src/data/string_functions.js
@@ -193,7 +193,7 @@ define(function (require) {
    * @return {String|Array}         formatted String 
    */
   p5.prototype.nfp = function() {
-    var nfRes = this.nf(arguments);
+    var nfRes = this.nf.apply(this, arguments);
     if (nfRes instanceof Array) {
       return nfRes.map(addNfp);
     } else {
@@ -225,7 +225,7 @@ define(function (require) {
    * @return {String|Array}         formatted String 
    */
   p5.prototype.nfs = function() {
-    var nfRes = this.nf(arguments);
+    var nfRes = this.nf.apply(this, arguments);
     if (nfRes instanceof Array) {
       return nfRes.map(addNfs);
     } else {
