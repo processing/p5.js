8fbb537c19c92dd840878fa625b770f9486fa7a3
Jordan Shaw <jrdnshw@gmail.com>
2014-09-04 12:44:44 -0400

    fixed typo

--- src/typography/attributes.js
+++ src/typography/attributes.js
@@ -11,8 +11,8 @@ define(function (require) {
   var constants = require('constants');
 
   /**
-   * Sets the current alignment for drawing text. The parameters LEFT, CENTER, and RIGHT set the display characteristics of the letters in relation to the values for the x and y parameters of the text() function. 
-   * 
+   * Sets the current alignment for drawing text. The parameters LEFT, CENTER, and RIGHT set the display characteristics of the letters in relation to the values for the x and y parameters of the text() function.
+   *
    * @method textAlign
    * @param {Number/Constant} alignX horizontal alignment, either LEFT, CENTER, or RIGHT
    */
@@ -33,7 +33,7 @@ define(function (require) {
   };
 
   /**
-   * Sets the spacing between lines of text in units of pixels. This setting will be used in all subsequent calls to the text() function. 
+   * Sets the spacing between lines of text in units of pixels. This setting will be used in all subsequent calls to the text() function.
    *
    * @method textLeading
    * @param {Number} leading the size in pixels for spacing between lines
@@ -53,7 +53,7 @@ define(function (require) {
   };
 
   /**
-   * Sets the style of the text to NORMAL, ITALIC, or BOLD. Note this is overriden by CSS styling.
+   * Sets the style of the text to NORMAL, ITALIC, or BOLD. Note this is overridden by CSS styling.
    *
    * @method textStyle
    * @param {Number/Constant} style styling for text, either NORMAL, ITALIC, or BOLD
