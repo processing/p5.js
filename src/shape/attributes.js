8cb27c71dbee0362894bc06950bf94a945a9660b
Jordan Shaw <jrdnshw@gmail.com>
2014-09-04 12:42:54 -0400

    fixed some typos in the docs

--- src/shape/attributes.js
+++ src/shape/attributes.js
@@ -13,12 +13,12 @@ define(function (require) {
 
 
   /**
-   * Modifies the location from which ellipses are drawn by changing the way in which parameters given to ellipse() are intepreted.
+   * Modifies the location from which ellipses are drawn by changing the way in which parameters given to ellipse() are interpreted.
    *
    * The default mode is ellipseMode(CENTER), which interprets the first two parameters of ellipse() as the shape's center point, while the third and fourth parameters are its width and height.
    *
    * ellipseMode(RADIUS) also uses the first two parameters of ellipse() as the shape's center point, but uses the third and fourth parameters to specify half of the shapes's width and height.
-   * 
+   *
    * ellipseMode(CORNER) interprets the first two parameters of ellipse() as the upper-left corner of the shape, while the third and fourth parameters are its width and height.
    *
    * ellipseMode(CORNERS) interprets the first two parameters of ellipse() as the location of one corner of the ellipse's bounding box, and the third and fourth parameters as the location of the opposite corner.
@@ -51,8 +51,8 @@ define(function (require) {
   };
 
   /**
-   * Modifies the location from which rectangles are drawn by changing the way in which parameters given to rect() are intepreted.
-   * 
+   * Modifies the location from which rectangles are drawn by changing the way in which parameters given to rect() are interpreted.
+   *
    * The default mode is rectMode(CORNER), which interprets the first two parameters of rect() as the upper-left corner of the shape, while the third and fourth parameters are its width and height.
    *
    * rectMode(CORNERS) interprets the first two parameters of rect() as the location of one corner, and the third and fourth parameters as the location of the opposite corner.
@@ -62,7 +62,7 @@ define(function (require) {
    * rectMode(RADIUS) also uses the first two parameters of rect() as the shape's center point, but uses the third and fourth parameters to specify half of the shapes's width and height.
    *
    * The parameter must be written in ALL CAPS because Processing is a case-sensitive language.
-   * 
+   *
    * @method rectMode
    * @param {Number/Constant} mode either CORNER, CORNERS, CENTER, or RADIUS
    * @return {p5} the p5 object
@@ -77,7 +77,7 @@ define(function (require) {
 
   /**
    * Draws all geometry with smooth (anti-aliased) edges. smooth() will also improve image quality of resized images. Note that smooth() is active by default; noSmooth() can be used to disable smoothing of geometry, images, and fonts.
-   * 
+   *
    * @method smooth
    * @return {p5} the p5 object
    */
@@ -90,7 +90,7 @@ define(function (require) {
 
   /**
    * Sets the style for rendering line endings. These ends are either squared, extended, or rounded, each of which specified with the corresponding parameters: SQUARE, PROJECT, and ROUND. The default cap is ROUND.
-   * 
+   *
    * @method strokeCap
    * @param {Number/Constant} cap either SQUARE, PROJECT, or ROUND
    * @return {p5} the p5 object
