edabfc27a907ac7e842e567edbb337348eaf0779
Darby Rathbone <darby.rathbone@gmail.com>
2014-12-08 09:32:14 -0600

    added blur

--- src/core/core.js
+++ src/core/core.js
@@ -166,7 +166,8 @@ define(function (require) {
       'touchstart': null,
       'touchmove': null,
       'touchend': null,
-      'resize': null
+      'resize': null,
+      'blur'
     };
     this._loadingScreenId = 'p5_loading';
 
