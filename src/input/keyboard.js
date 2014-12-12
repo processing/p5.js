37a1436b041bf423ee004cf16cf6e5103bc0d64d
johnpasquarello <john.luke.pasquarello@gmail.com>
2014-12-12 15:39:05 -0500

    Added a few functions -onDeviceMove() --is called whenever the acceleration difference is above move_threshold
    -_updatePAccelerations()
    --called in the draw method along with updatePMouseCoords
    
    -setMoveThreshold(val)
    --sets move_threshold
    --note: I think the unit for these is meters pre second per second but I'm not sure
    
    All in all this is just for the ability to have a "onDeviceMove" method
    which is pretty cool

--- src/input/keyboard.js
+++ src/input/keyboard.js
@@ -137,24 +137,38 @@ define(function (require) {
   p5.prototype.accelerationX = 0;
   p5.prototype.accelerationY = 0;
   p5.prototype.accelerationZ = 0;
+  
+  p5.prototype.pAccelerationX = 0;
+  p5.prototype.pAccelerationY = 0;
+  p5.prototype.pAccelerationZ = 0;
+
+  var move_threshold = 0.5;
+
+  p5.prototype.setMoveThreshold = function(val){
+    if(typeof val === 'number'){
+      move_threshold = val;
+    }
+  };
+
+  p5.prototype._updatePAccelerations = function(){
+    this._setProperty('pAccelerationX', this.accelerationX);
+    this._setProperty('pAccelerationY', this.accelerationY);
+    this._setProperty('pAccelerationZ', this.accelerationZ);
+  };
 
   p5.prototype.ondevicemotion = function (e) {
     this._setProperty('accelerationX', e.accelerationIncludingGravity.x);
     this._setProperty('accelerationY', e.accelerationIncludingGravity.y);
     this._setProperty('accelerationZ', e.accelerationIncludingGravity.z);
     
-    var onTurn = this.onTurn || window.onTurn;
-    if (typeof keyPressed === 'function') {
-      var executeDefault  = onTurn(e);
-      if(executeDefault === false) {
-        e.preventDefault();
+    var onDeviceMove = this.onDeviceMove || window.onDeviceMove;
+    if (typeof onDeviceMove === 'function') {
+      if(Math.abs(this.accelerationX - this.pAccelerationX) > move_threshold ||
+         Math.abs(this.accelerationY - this.pAccelerationY) > move_threshold ||
+         Math.abs(this.accelerationZ - this.pAccelerationZ) > move_threshold ){
+        onDeviceMove();
       }
     }
-    /*
-    var onTurn = this.onTurn || window.onTurn;
-    if(typeof keyPressed === 'function'){
-       var executeDefault = onTurn(e);
-    }*/
   };
 
 
