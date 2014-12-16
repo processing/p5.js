00b9a39bb2e64e19c2f157074a495dc3ba3d425a
Lauren McCarthy <laurmccarthy@gmail.com>
2014-12-16 08:42:56 -0500

    merge 3d start

--- examples/3d/sketch.js
+++ examples/3d/sketch.js
@@ -4,11 +4,11 @@ function setup(){
   //webgl context gets initialized properly
     // createCanvas(displayWidth,displayHeight); //2d context
     createCanvas(displayWidth, displayHeight, 'webgl');
+    colorMode(HSB);
 }
 
 function draw(){
-  // background(0,0,255);
-
-  stroke(random(255),random(255),random(255),255);
-  line(random(displayWidth),0,random(20), mouseX, mouseY, random(100));
+  //background(0,0,255);
+  stroke(random(255),random(255),random(255), 255);
+  line(random(displayWidth),0,random(20), mouseX+displayWidth/2, mouseY+displayHeight/2, random(100));
 }
