c80ab7df692d51503b08336473c7fce014ed47ae
John Pasquarello <john.luke.pasquarello@gmail.com>
2015-01-10 01:52:15 -0500

    added an orientation variable

--- src/input/acceleration.js
+++ src/input/acceleration.js
@@ -6,6 +6,9 @@ define(function (require){
   'use strict';
 
   var p5 = require('core');
+  
+  p5.prototype.deviceOrientation = undefined;
+  //not sure what a good default would be
 
   p5.prototype.accelerationX = 0;
   p5.prototype.accelerationY = 0;
@@ -36,6 +39,20 @@ define(function (require){
     this._setProperty('accelerationX', e.accelerationIncludingGravity.x);
     this._setProperty('accelerationY', e.accelerationIncludingGravity.y);
     this._setProperty('accelerationZ', e.accelerationIncludingGravity.z);
+
+    //sets orientation property
+    //device is horizontal
+    if(window.orientation === 90 || window.orientation === -90){
+      this._setProperty('deviceOrientation', 'landscape');
+    }
+    else if (window.orientation === 0){ //device is vertical
+      this._setProperty('deviceOrientation', 'portrait');
+    }
+    else if (window.orientation === undefined){
+      //in case the device doesn't support this function
+      //I don't know if this is a good idea
+      this._setProperty('deviceOrientation', 'undefined');
+    }
 
     var onDeviceMove = this.onDeviceMove || window.onDeviceMove;
     if (typeof onDeviceMove === 'function') {
