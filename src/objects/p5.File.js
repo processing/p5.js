75249eb9cdc9a829fc012b6c398c7e4090ff4287
Daniel Shiffman <daniel@shiffman.net>
2015-01-29 21:29:51 +0000

    start to p5.File

---
+++ src/objects/p5.File.js
@@ -0,0 +1,41 @@
+/**
+ * @module Data
+ * @submodule Data
+ * @for p5.File
+ */
+define(function(require) {
+
+  var p5 = require('core');
+
+  /**
+   * Base class for a file
+   * Using this for createFileInput
+   *
+   * @class p5.File
+   * @constructor
+   * @param {File} file File that is wrapped
+   * @param {Object} [pInst] pointer to p5 instance
+   */
+  p5.File = function(file, pInst) {
+    /**
+     * Underlying File object. All normal File methods can be called on this.
+     *
+     * @property file
+     */
+    this.file = file;
+
+    this._pInst = pInst;
+    var typeList = file.type.split('/');
+
+
+    this.type = typeList[0];
+    this.subtype = typeList[1];
+    this.name = file.name;
+    this.size = file.size;
+    
+    // Data not loaded yet
+    this.data = undefined;
+  };
+
+  return p5.File;
+});
