0d28cb1fab119c29e1fee76fb73d9b21bf2c8c5e
Mikael Lindqvist <li.mikael@gmail.com>
2015-02-23 15:59:44 +0100

    convert to string in text function

---
+++ examples/text/text.js
@@ -0,0 +1,14 @@
+var x = 100;
+var y = 100;
+
+function setup() {
+	createCanvas(512, 512);
+}
+
+function draw() {
+	clear();
+	text("hello world", 10, 10);
+
+	a = ["i", "am", "an", "array"];
+	text(a, 10, 30);
+}
