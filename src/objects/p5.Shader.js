32edbd617b6ead7d1343801e68daaacf61846c62
Miles Peyton <milespeyton@gmail.com>
2015-05-26 16:39:26 -0400

    Add p5.Shader.js

---
+++ src/objects/p5.Shader.js
@@ -0,0 +1,16 @@
+/**
+ * @module ? 
+ * @submodule ? 
+ * @requires ? 
+ */
+define(function (require) {
+
+  'use strict';
+
+  var p5 = require('core');
+  
+  p5.Shader = function () {
+   this.vertexSource
+   this.fragmentSource
+  };
+}
