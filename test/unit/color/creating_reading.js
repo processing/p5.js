dc4c953e75a7a8f949ca444939ea5324e9d6b5c5
David Newbury <david.newbury@gmail.com>
2015-05-29 11:23:00 -0400

    brightness() now supports arrays.

--- test/unit/color/creating_reading.js
+++ test/unit/color/creating_reading.js
@@ -45,27 +45,43 @@ suite('CreatingReading', function() {
     });
   });
 
-  suite('p5.prototype.red,green,blue', function() {
-    var color, colorArr;
+  suite('p5.prototype.red,green,blue,brightness', function() {
+    var color, rgbaColor, colorArr, colorAndAlphaArr;
 
     setup(function() {
       colorArr = [1,2,3];
+      colorAndAlphaArr = [1,2,3,4];
       color = myp5.color(colorArr);
+      rgbaColor = myp5.color(colorAndAlphaArr);
     });
 
     test('p5.prototype.red', function() {
+      assert.equal(myp5.red(colorAndAlphaArr), 1);
       assert.equal(myp5.red(colorArr), 1);
       assert.equal(myp5.red(color), 1);
+      assert.equal(myp5.red(rgbaColor), 1);
     });
 
     test('p5.prototype.green', function() {
+      assert.equal(myp5.green(colorAndAlphaArr), 2);
       assert.equal(myp5.green(colorArr), 2);
       assert.equal(myp5.green(color), 2);
+      assert.equal(myp5.green(rgbaColor), 2);
+
     });
 
     test('p5.prototype.blue', function() {
+      assert.equal(myp5.blue(colorAndAlphaArr), 3);
       assert.equal(myp5.blue(colorArr), 3);
       assert.equal(myp5.blue(color), 3);
+      assert.equal(myp5.blue(rgbaColor), 3);
+    });
+
+    test('p5.prototype.brightness', function() {
+      assert.equal(myp5.brightness(colorAndAlphaArr), 3);
+      assert.equal(myp5.brightness(colorArr), 3);
+      assert.equal(myp5.brightness(color), 3);
+      assert.equal(myp5.brightness(rgbaColor), 3);
     });
   });
 
