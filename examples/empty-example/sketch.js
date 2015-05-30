00c02b88f3dcb25fc3975b0eeb416e7d9bab47fd
Caroline Record <recordcaroline@gmail.com>
2015-05-30 17:25:02 -0400

    file errors + welcome

--- examples/empty-example/sketch.js
+++ examples/empty-example/sketch.js
@@ -1,8 +1,10 @@
 function setup() {
   // put setup code here
   createCanvas(600, 400);
+  loadXML("circle.xml","justLoaded");
 }
 
 function draw() {
   // put drawing code here
+  //ellipse();
 }
