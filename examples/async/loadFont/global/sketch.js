bc95f163d8234c5afad8eb48371e6f7cafcf3e7f
Daniel Howe <daniel@rednoise.org>
2015-06-04 22:33:18 -0400

    minor fix to calculation of font-metrics and memoization of textBounds
    example for minor update to calculation of font-metrics
    
    linting

--- examples/async/loadFont/global/sketch.js
+++ examples/async/loadFont/global/sketch.js
@@ -1,15 +1,12 @@
-// In this example, we want to load a font
-// image and use it in setup().
+// In this example, we want to load a font image and use it in setup().
 //
 // Since setup() happens quickly at the beginning, the font doesn't
 // have time to properly load before setup() is done.
 //
-// We are introducing preload() where you can run load
-// operations that are guaranteed to complete by setup().
-// This is called asynchronous loading, because it happens whenever
-// the computer is done and ready, not necessarily when you call it.
-
-var font1, font2, x=30, y=80, words = 'cean eans';
+// So we use preload() for this type of load operations, which guarantees
+// such functions will complete before setup() is called.
+//
+var font1, font2, x=30, y=80, words = 'Green Ideas';
 
 function preload() {
   font1 = loadFont('SourceSansPro-Regular.otf');
@@ -20,6 +17,7 @@ function setup() {
 
   createCanvas(400, 200);
 
+  fill(0,100,0);
   textFont(font1, 58);
   text(words, x, y);
 
@@ -34,15 +32,14 @@ function setup() {
   line(0,y+descent,width,y+descent);
   line(x,0,x,200);
 
-  var tb = font1.textBounds(words, x, y); // tight
+  var tb = font1.textBounds(words, x, y); // tight bounds
   noFill();
   stroke(200,0,0);
   rect(tb.x,tb.y,tb.w,tb.h);
 
-  //fill(0);
   fill(100);
   stroke(100,0,0);
   textSize(40);
   textFont(font2);
-  text('sleep furiously', x+20, y+80);
+  text('sleep furiously', x+20, y+60);
 };
