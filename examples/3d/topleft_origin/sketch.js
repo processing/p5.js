4f7762f4274d667d20254146852c68d27d57a6ca
Karen <karenpenglabs@gmail.com>
2015-07-24 15:57:36 -0400

    delete orignMode and tweak examples

--- examples/3d/topleft_origin/sketch.js
+++ examples/3d/topleft_origin/sketch.js
@@ -6,23 +6,22 @@ function draw(){
 
   background(255);
 
-  originMode('TOP_LEFT');
+  translate( -width/2, -height/2, -600);
 
-  translate(0, 0, -800);
   rotateY(frameCount * 0.01);
 
   var gap = 200;
-  var width = 100;
-  var height = 100;
+  var w = 100;
+  var h = 100;
 
   for(var i = 0; i < 5; i++){
     for(var j = 0; j < 5; j++){
       fill( i * 40, j * 40, 0);
       quad(
         i * gap, j * gap, 0,
-        i * gap + width, j * gap, 0,
-        i * gap, j * gap + height, 0,
-        i * gap + width, j * gap + height/2 * (sin(frameCount * 0.1 + i + j) + 1), 0
+        i * gap + w, j * gap, 0,
+        i * gap, j * gap + h, 0,
+        i * gap + w, j * gap + h/2 * (sin(frameCount * 0.1 + i + j) + 1), 0
         );
     }
   }
