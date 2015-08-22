80d2dae4b4b72225723940ae8ad2b26d5df280e5
Daniel Shiffman <daniel@shiffman.net>
2015-08-22 17:24:46 -0400

    dropdown test

--- examples/addons/p5.dom/dropdown_test/sketch.js
+++ examples/addons/p5.dom/dropdown_test/sketch.js
@@ -2,24 +2,22 @@ var dropdown;
 
 function setup() {
   dropdown = createSelect(); // or create dropdown?
+
+  // just mucking around
   dropdown.option('apple','1');
   dropdown.option('orange','2');
-  //dropdown.option('pear', 'pear');
   dropdown.option('pear');
 
+  // Set what it starts as
   dropdown.value('2');
 
   dropdown.changed(mySelectEvent);
 }
 
-function mousePressed() {
-  println(dropdown.value());
-}
-
 function draw() {
   background(0);
-  if (dropdown.value() === 'pear') {
-    background(0, 255, 0);
+  if (dropdown.selected() === '1') {
+    background(255, 0, 0);
   }
 }
 
