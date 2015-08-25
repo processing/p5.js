f89a81b0f732df92ed0e6f80a753a4846ab64414
indefinit <kevin.siwoff@gmail.com>
2015-08-25 11:00:40 -0400

    implements video as texture in webgl: WIP still buggy

--- examples/3d/material/texture/sketch.js
+++ examples/3d/material/texture/sketch.js
@@ -1,16 +1,23 @@
 /**
  * webgl texture example
+ * video source: https://vimeo.com/90312869
  */
 var img;
+var vid;
 var theta = 0;
-
-function preload(){
-  //load our image
-  img = loadImage("assets/cat.jpg");
-}
 
 function setup(){
   createCanvas(windowWidth, windowHeight, 'webgl');
+  
+  //2D renderer
+  // createCanvas(windowWidth, windowHeight);
+
+  img = loadImage("assets/cat.jpg");
+  vid = createVideo(["assets/360video_256crop_v2.mp4"]);
+  vid.loop();
+  vid.hide();
+
+
 }
 
 function draw(){
@@ -18,12 +25,22 @@ function draw(){
   camera(0, 0, 200);
   perspective(60 / 180 * Math.PI, width/height, 0.1, 100);
   push();
+    translate(-100,0,0);
     rotateZ(theta * mouseX * 0.001);
     rotateX(theta * mouseX * 0.001);
     rotateY(theta * mouseX * 0.001);
-    // pass image as texture
+    //pass image as texture
+    texture(vid);
+    sphere(45);
+  pop();
+
+  push();
+    translate(100,0,0);
+    rotateZ(theta * 0.1);
+    rotateX(theta * 0.1);
+    rotateY(theta * 0.1);
     texture(img);
-    box(40);
+    box(45);
   pop();
   theta += 0.05;
 }
--- src/3d/material.js
+++ src/3d/material.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var p5 = require('../core/core');
+require('./p5.Texture');
 
 /**
 * [normal description]
@@ -12,6 +13,10 @@ p5.prototype.normalMaterial = function(){
 };
 
 /**
+ * @TODO refactor into its own p5.Texture class ||
+ * create a texture hash map data structure to keep
+ * track of active textures.
+ * 
  * [textureMaterial description]
  * @return {[type]} [description]
  * @example
@@ -39,10 +44,15 @@ p5.prototype.texture = function(image){
   var shaderProgram = this._graphics.getShader('normalVert',
     'textureFrag');
   gl.useProgram(shaderProgram);
-  //create a texture on the graphics card
   var tex = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, tex);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
 
+  // Currently buggy, likely bc of p5 object types
+  // if(!this._isPowerOf2(image.width) || !this._isPowerOf2(image.height)){
+  //   image.width = _nextHighestPOT(image.width);
+  //   image.height = _nextHighestPOT(image.height);
+  // }
   if (image instanceof p5.Image) {
     image.loadPixels();
     var data = new Uint8Array(image.pixels);
@@ -50,14 +60,22 @@ p5.prototype.texture = function(image){
       gl.RGBA, image.width, image.height,
       0, gl.RGBA, gl.UNSIGNED_BYTE, data);
   }
+  //if param is a video
+  else if (image instanceof p5.MediaElement){
+    if(!image.loadedmetadata) {return;}
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+    gl.UNSIGNED_BYTE, image.elt);
+  }
   else {
     //@TODO handle following cases:
     //- 2D canvas (p5 inst)
-    //- video and pass into fbo
   }
+  
   if (_isPowerOf2(image.width) && _isPowerOf2(image.height)) {
     gl.generateMipmap(gl.TEXTURE_2D);
   } else {
+    image.width = _nextHighestPOT(image.width);
+    image.height = _nextHighestPOT(image.height);
     gl.texParameteri(gl.TETXURE_2D,
       gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TETXURE_2D,
@@ -65,20 +83,38 @@ p5.prototype.texture = function(image){
     gl.texParameteri(gl.TETXURE_2D,
       gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   }
+  //this is where we'd activate multi textures
+  //eg. gl.activeTexture(gl.TEXTURE0 + (unit || 0));
+  //but for now we just have a single texture.
+  //@TODO need to extend this functionality
+  gl.activeTexture(gl.TEXTURE0 + 0);
   gl.bindTexture(gl.TEXTURE_2D, tex);
   gl.uniform1i(gl.getUniformLocation(shaderProgram, 'uSampler'), 0);
   return this;
 };
 
 /**
- * Helper function; Checks whether val is a pot
+ * Helper functions; Checks whether val is a pot
  * more info on power of 2 here:
  * https://www.opengl.org/wiki/NPOT_Texture
  * @param  {Number}  value
  * @return {Boolean}
  */
-function _isPowerOf2 (value) {
+function _isPowerOf2 (value){
   return (value & (value - 1)) === 0;
+}
+
+/**
+ * returns the next highest power of 2 value
+ * @param  {Number} value [description]
+ * @return {Number}       [description]
+ */
+function _nextHighestPOT (value){
+  --value;
+  for (var i = 1; i < 32; i <<= 1) {
+    value = value | value >> i;
+  }
+  return value + 1;
 }
 
 p5.prototype.basicMaterial = function(r, g, b, a){
