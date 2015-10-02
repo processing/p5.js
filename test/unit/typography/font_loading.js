bfa33d633a96721690c07e3df5389a80a2a9e920
Jared Sprague <jared@caramelcode.com>
2015-10-02 03:19:50 -0400

    simplified unit vars in unit tests

--- test/unit/typography/font_loading.js
+++ test/unit/typography/font_loading.js
@@ -1,12 +1,11 @@
-var myFont1, myFont2;
-
 // Test loading font in preload() with success callback
 var mySketch = function (this_p5) {
+  var myFont;
   this_p5.preload = function () {
     suite('Test in preload() with success callback', function () {
       test('Load asynchronously and use success callback', function (done) {
-        myFont1 = this_p5.loadFont('../examples/p5.Font/acmesa.ttf', function () {
-          assert.ok(myFont1);
+        myFont = this_p5.loadFont('../examples/p5.Font/acmesa.ttf', function () {
+          assert.ok(myFont);
           done();
         });
       });
@@ -16,9 +15,9 @@ var mySketch = function (this_p5) {
   this_p5.setup = function () {
     suite('setup() after preload() with success callback', function () {
       test('Resource should be loaded now if preload() finished', function (done) {
-        assert.isTrue(myFont1 instanceof p5.Font);
-        assert.isObject(myFont1);
-        assert.isObject(myFont1.font);
+        assert.isTrue(myFont instanceof p5.Font);
+        assert.isObject(myFont);
+        assert.isObject(myFont.font);
         done();
       });
     });
@@ -28,16 +27,17 @@ new p5(mySketch, null, true);
 
 // Test loading font in preload() without success callback
 mySketch = function (this_p5) {
+  var myFont;
   this_p5.preload = function () {
-    myFont2 = this_p5.loadFont('../examples/p5.Font/acmesa.ttf');
+    myFont = this_p5.loadFont('../examples/p5.Font/acmesa.ttf');
   };
 
   this_p5.setup = function () {
     suite('setup() after preload() without success callback', function () {
       test('Resource should be loaded now if preload() finished', function (done) {
-        assert.isTrue(myFont2 instanceof p5.Font);
-        assert.isObject(myFont2);
-        assert.isObject(myFont2.font);
+        assert.isTrue(myFont instanceof p5.Font);
+        assert.isObject(myFont);
+        assert.isObject(myFont.font);
         done();
       });
     });
