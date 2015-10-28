1a414ed781cdcbecaca1dee3edd2ce5944ab6053
Joe Cridge <joe.cridge@me.com>
2015-10-28 20:40:53 +0000

    Refactor color_utils.js
    The code is functionally the same as before, I've just simplified and
    commented a bit.

--- src/color/color_utils.js
+++ src/color/color_utils.js
@@ -6,280 +6,244 @@
 
 var p5 = require('../core/core');
 
+/**
+ * Conversions adapted from <http://www.easyrgb.com/math.html>.
+ *
+ * In these functions, hue is always in the range [0,1); all other components
+ * are in the range [0,1]. 'Brightness' and 'value' are used interchangeably.
+ */
 p5.ColorUtils = {};
 
+
 /**
- * For a color expressed as an HSBA array, return the corresponding RGBA value
- * @param {Array} hsba An 'array' object that represents a list of HSB colors
- * @return {Array} an array of RGBA values, on a scale of 0-1
+ * Convert an HSBA array to HSLA.
+ */
+p5.ColorUtils.hsbaToHSLA = function(hsba) {
+  var hue = hsba[0];
+  var sat = hsba[1];
+  var val = hsba[2];
+
+  // Calculate lightness.
+  var li = (2 - sat) * val / 2;
+
+  // Convert saturation.
+  if (li !== 0) {
+    if (li === 1) {
+      sat = 0;
+    } else if (li < 0.5) {
+      sat = sat / (2 - sat);
+    } else {
+      sat = sat * val / (2 - li * 2);
+    }
+  }
+
+  // Hue and alpha stay the same.
+  return [hue, sat, li, hsba[3] || 1];
+};
+
+/**
+ * Convert an HSBA array to RGBA.
  */
 p5.ColorUtils.hsbaToRGBA = function(hsba) {
-  var h = hsba[0];
-  var s = hsba[1];
-  var v = hsba[2];
-  var a = hsba[3] || 1;
-  // Adapted from http://www.easyrgb.com/math.html
-  // hsv values = 0 - 1, rgb values = 0 - 255
+  var hue = hsba[0] * 6;  // We will split hue into 6 sectors.
+  var sat = hsba[1];
+  var val = hsba[2];
+
   var RGBA = [];
-  if(s===0){
-    RGBA = [v, v, v, a];
+
+  if (sat === 0) {
+    RGBA = [val, val, val, hsba[3] || 1];  // Return early if grayscale.
   } else {
-    // h must be < 1
-    var var_h = h * 6;
-    if (var_h===6) {
-      var_h = 0;
+    var sector = Math.floor(hue);
+    var tint1 = val * (1 - sat);
+    var tint2 = val * (1 - sat * (hue - sector));
+    var tint3 = val * (1 - sat * (1 + sector - hue));
+    var red, green, blue;
+    if (sector === 0) {  // Red to yellow.
+      red = val;
+      green = tint3;
+      blue = tint1;
+    } else if (sector === 1) {  // Yellow to green.
+      red = tint2;
+      green = val;
+      blue = tint1;
+    } else if (sector === 2) {  // Green to cyan.
+      red = tint1;
+      green = val;
+      blue = tint3;
+    } else if (sector === 3) {  // Cyan to blue.
+      red = tint1;
+      green = tint2;
+      blue = val;
+    } else if (sector === 4) {  // Blue to magenta.
+      red = tint3;
+      green = tint1;
+      blue = val;
+    } else {  // Magenta to red.
+      red = val;
+      green = tint1;
+      blue = tint2;
     }
-    //Or ... var_i = floor( var_h )
-    var var_i = Math.floor( var_h );
-    var var_1 = v*(1-s);
-    var var_2 = v*(1-s*(var_h-var_i));
-    var var_3 = v*(1-s*(1-(var_h-var_i)));
-    var r;
-    var g;
-    var b;
-    if(var_i===0){
-      r = v;
-      g = var_3;
-      b = var_1;
-    }else if(var_i===1){
-      r = var_2;
-      g = v;
-      b = var_1;
-    }else if(var_i===2){
-      r = var_1;
-      g = v;
-      b = var_3;
-    }else if(var_i===3){
-      r = var_1;
-      g = var_2;
-      b = v;
-    }else if (var_i===4){
-      r = var_3;
-      g = var_1;
-      b = v;
-    }else{
-      r = v;
-      g = var_1;
-      b = var_2;
-    }
-    RGBA = [r, g, b, a];
+    RGBA = [red, green, blue, hsba[3] || 1];
   }
+
   return RGBA;
 };
 
 /**
- * For a color expressed as an RGBA array, return the corresponding HSBA value
- *
- * @param {Array} rgba An 'array' object that represents a list of RGB colors
- * @return {Array} an array of HSB values
+ * Convert an HSLA array to HSBA.
  */
-p5.ColorUtils.rgbaToHSBA = function(rgba) {
-  var r = rgba[0];
-  var g = rgba[1];
-  var b = rgba[2];
-  var a = rgba[3] || 1;
+p5.ColorUtils.hslaToHSBA = function(hsla) {
+  var hue = hsla[0];
+  var sat = hsla[1];
+  var li = hsla[2];
 
-  var min = Math.min(r, g, b); //Min. value of RGB
-  var max = Math.max(r, g, b); //Max. value of RGB
-  var delta_max = max - min;             //Delta RGB value
-
-  var h;
-  var s;
-  var v = max;
-
-  if (delta_max === 0) { //This is a gray, no chroma...
-    h = 0; //HSV results from 0 to 1
-    s = 0;
+  // Calculate brightness.
+  var val;
+  if (li < 0.5) {
+    val = (1 + sat) * li;
+  } else {
+    val = li + sat - li * sat;
   }
-  else { //Chromatic data...
-    s = delta_max/max;
 
-    var delta_r = ( ( ( max - r ) / 6 ) + ( delta_max / 2 ) ) / delta_max;
-    var delta_g = ( ( ( max - g ) / 6 ) + ( delta_max / 2 ) ) / delta_max;
-    var delta_b = ( ( ( max - b ) / 6 ) + ( delta_max / 2 ) ) / delta_max;
+  // Convert saturation.
+  sat = 2 * (val - li) / val;
 
-    if (r === max) {
-      h = delta_b - delta_g;
-    } else if (g === max) {
-      h = 1/3 + delta_r - delta_b;
-    } else if (b === max) {
-      h = 2/3 + delta_g - delta_r;
-    }
-
-    if (h < 0) {
-      h += 1;
-    }
-    if (h > 1) {
-      h -= 1;
-    }
-  }
-  return [h, s, v, a];
+  // Hue and alpha stay the same.
+  return [hue, sat, val, hsla[3] || 1];
 };
 
 /**
- * For a color expressed as an HSLA array, return the corresponding RGBA value
+ * Convert an HSLA array to RGBA.
  *
- * @param  {Array} hsla An 'array' object that represents a list of HSL colors
- * @return {Array} an array of RGBA values, on a scale of 0-1
+ * We need to change basis from HSLA to something that can be more easily be
+ * projected onto RGBA. We will choose hue and brightness as our first two
+ * components, and pick a convenient third one ('zest') so that we don't need
+ * to calculate formal HSBA saturation.
  */
 p5.ColorUtils.hslaToRGBA = function(hsla){
-  var h = hsla[0];
-  var s = hsla[1];
-  var l = hsla[2];
-  var a = hsla[3] || 1;
+  var hue = hsla[0] * 6;  // We will split hue into 6 sectors.
+  var sat = hsla[1];
+  var li = hsla[2];
 
-  // Adapted from http://www.easyrgb.com/math.html
-  // hsl values = 0 - 1, rgb values = 0 - 1
-  var rgba = [];
-  if(s === 0){
-    rgba = [l, l, l, a];
+  var RGBA = [];
+
+  if (sat === 0) {
+    RGBA = [li, li, li, hsla[3] || 1]; // Return early if grayscale.
   } else {
-    var m, n, r, g, b;
 
-    n = l < 0.5 ? l * (1 + s) : (l + s) - (s * l);
-    m = 2 * l - n;
+    // Calculate brightness.
+    var val;
+    if (li < 0.5) {
+      val = (1 + sat) * li;
+    } else {
+      val = li + sat - li * sat;
+    }
 
-    var convert = function(x, y, hue){
-      if (hue < 0) {
-        hue += 1;
-      } else if (hue > 1) {
-        hue -= 1;
+    // Define zest.
+    var zest = 2 * li - val;
+
+    // Implement projection (project onto green by default).
+    var hzvToRGB = function(hue, zest, val) {
+      if (hue < 0) {  // Hue must wrap to allow projection onto red and blue.
+        hue += 6;
+      } else if (hue >= 6) {
+        hue -= 6;
       }
-
-      if ( ( 6 * hue ) < 1 ) {
-        return ( x + ( y - x ) * 6 * hue );
-      } else if ( ( 2 * hue ) < 1 ) {
-        return ( y );
-      } else if ( ( 3 * hue ) < 2 ) {
-        return ( x + ( y - x ) * ( ( 2 / 3 ) - hue ) * 6 );
-      } else {
-        return x;
+      if (hue < 1) {  // Red to yellow (increasing green).
+        return (zest + (val - zest) * hue);
+      } else if (hue < 3) {  // Yellow to cyan (greatest green).
+        return val;
+      } else if (hue < 4) {  // Cyan to blue (decreasing green).
+        return (zest + (val - zest) * (4 - hue));
+      } else {  // Blue to red (least green).
+        return zest;
       }
     };
 
-    r = convert( m, n, h + ( 1 / 3 ) );
-    g = convert( m, n, h );
-    b = convert( m, n, h - ( 1 / 3 ) );
-
-    rgba = [r, g, b, a];
+    // Perform projections, offsetting hue as necessary.
+    RGBA = [hzvToRGB(hue + 2, zest, val),
+            hzvToRGB(hue    , zest, val),
+            hzvToRGB(hue - 2, zest, val),
+            hsla[3] || 1];
   }
 
-  return rgba;
+  return RGBA;
 };
 
 /**
- * For a color expressed as an RGBA array, return the corresponding HSBA value
- *
- * @param {Array} rgba An 'array' object that represents a list of RGB colors
- * @return {Array} an array of HSL values
+ * Convert an RGBA array to HSBA.
+ */
+p5.ColorUtils.rgbaToHSBA = function(rgba) {
+  var red = rgba[0];
+  var green = rgba[1];
+  var blue = rgba[2];
+
+  var val = Math.max(red, green, blue);
+  var chroma = val - Math.min(red, green, blue);
+
+  var hue, sat;
+  if (chroma === 0) {  // Return early if grayscale.
+    hue = 0;
+    sat = 0;
+  }
+  else {
+    sat = chroma / val;
+    if (red === val) {  // Magenta to yellow.
+      hue = (green - blue) / chroma;
+    } else if (green === val) { // Yellow to cyan.
+      hue = 2 + (blue - red) / chroma;
+    } else if (blue === val) {  // Cyan to magenta.
+      hue = 4 + (red - green) / chroma;
+    }
+    if (hue < 0) {  // Confine hue to the interval [0, 1).
+      hue += 6;
+    } else if (hue >= 6) {
+      hue -= 6;
+    }
+  }
+
+  return [hue / 6, sat, val, rgba[3] || 1];
+};
+
+/**
+ * Convert an RGBA array to HSLA.
  */
 p5.ColorUtils.rgbaToHSLA = function(rgba) {
-  var r = rgba[0];
-  var g = rgba[1];
-  var b = rgba[2];
-  var a = rgba[3] || 1;
+  var red = rgba[0];
+  var green = rgba[1];
+  var blue = rgba[2];
 
-  var min = Math.min(r, g, b); //Min. value of RGB
-  var max = Math.max(r, g, b); //Max. value of RGB
-  var delta_max = max - min;             //Delta RGB value
+  var val = Math.max(red, green, blue);
+  var min = Math.min(red, green, blue);
+  var li = val + min;  // We will halve this later.
+  var chroma = val - min;
 
-  var h;
-  var s;
-  var l = (max + min) / 2;
-
-  var delta_r;
-  var delta_g;
-  var delta_b;
-
-  if (delta_max === 0) { // This is a gray, no chroma...
-    h = 0;             // HSL results from 0 to 1
-    s = 0;
-  } else {              // Chromatic data...
-
-    delta_r = ( ( ( max - r ) / 6 ) + ( delta_max / 2 ) ) / delta_max;
-    delta_g = ( ( ( max - g ) / 6 ) + ( delta_max / 2 ) ) / delta_max;
-    delta_b = ( ( ( max - b ) / 6 ) + ( delta_max / 2 ) ) / delta_max;
-
-    if ( r === max ){
-      h = delta_b - delta_g;
-    } else if ( g === max ){
-      h = ( 1 / 3 ) + delta_r - delta_b;
-    } else if ( b === max ) {
-      h = ( 2 / 3 ) + delta_g - delta_r;
-    }
-
-    if ( h < 0 ) {
-      h += 1;
-    }
-
-    if ( h > 1 ) {
-      h -= 1;
-    }
-
-    if ( l < 0.5 ){
-      s = delta_max / ( max + min );
+  var hue, sat;
+  if (chroma === 0) {  // Return early if grayscale.
+    hue = 0;
+    sat = 0;
+  } else {
+    if (li < 1) {
+      sat = chroma / li;
     } else {
-      s = delta_max / ( 2 - max - min );
+      sat = chroma / (2 - chroma);
     }
-
-  }
-  return [h, s, l, a];
-};
-
-/**
- * For a color expressed as an hsla array, return the corresponding HSBA value
- *
- * @param {Array} hsla An 'array' object that represents a list of HSLA colors
- * @return {Array} an array of HSBA values
- */
-p5.ColorUtils.hslaToHSBA = function(hsla) {
-  var h = hsla[0];
-  var s = hsla[1];
-  var l = hsla[2];
-  var a = hsla[3] || 1;
-
-  var v;
-
-  //Hue and Alpha stay the same
-  s *= l < 0.5 ? l : 1 - l;
-  v = l + s;
-  s = 2 * s / (l + s);
-
-  return[ h, s, v, a];
-};
-
-/**
- * For a color expressed as an hsba array, return the corresponding HSLA value
- *
- * @param {Array} hsba An 'array' object that represents a list of HSBA colors
- * @return {Array} an array of HSLA values
- */
-p5.ColorUtils.hsbaToHSLA = function(hsba) {
-  var h = hsba[0];
-  var s = hsba[1];
-  var v = hsba[2];
-  var a = hsba[3] || 1;
-
-  //Hue and Alpha stay the same
-  //Lightness is (2 - s) * v / 2
-  var l = (2 - s) * v / 2;
-
-  //Saturation is very different between the two color spaces
-  //If l < 0.5 set it to s / (2 - s)
-  //Otherwise s * v / (2 - (2 - s) * v)
-  if( l !== 0 ){
-    if( l === 1 ){
-      s = 0;
+    if (red === val) {  // Magenta to yellow.
+      hue = (green - blue) / chroma;
+    } else if (green === val) {  // Yellow to cyan.
+      hue = 2 + (blue - red) / chroma;
+    } else if (blue === val) {  // Cyan to magenta.
+      hue = 4 + (red - green) / chroma;
     }
-    else if( l < 0.5 ){
-      s = s / (2 - s);
-    }
-    else{
-      s = s * v / (2 - l * 2);
+    if (hue < 0) {  // Confine hue to the interval [0, 1).
+      hue += 6;
+    } else if (hue >= 6) {
+      hue -= 6;
     }
   }
 
-  return [ h, s, l, a];
+  return [hue / 6, sat, li / 2, rgba[3] || 1];
 };
 
 module.exports = p5.ColorUtils;
