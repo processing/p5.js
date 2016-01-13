15671454354252f6f2cd3d779de6e3e1a2d10466
arihant parsoya <parsoyaarihant@gmail.com>
2016-01-13 11:33:36 +0530

    implemented bezier

--- src/3d/p5.Renderer3D.Immediate.js
+++ src/3d/p5.Renderer3D.Immediate.js
@@ -127,10 +127,9 @@ function(mode, isCurve, isBezier,isQuadratic, isContour, shapeKind){
 p5.Renderer3D.prototype._bindImmediateBuffers = function(vertices, colors){
   this._setDefaultCamera();
   var gl = this.GL;
-  var shaderKey = this._getCurShaderId();
-  var shaderProgram = this.mHash[shaderKey];
+  var shaderProgram = this._getColorVertexShader();
   //vertex position Attribute
-  //@todo refactor for elegance
+  //@todo this is messy.
   shaderProgram.vertexPositionAttribute =
     gl.getAttribLocation(shaderProgram, 'aPosition');
   gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
@@ -147,12 +146,12 @@ p5.Renderer3D.prototype._bindImmediateBuffers = function(vertices, colors){
   gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,
     4, gl.FLOAT, false, 0, 0);
   //matrix
-  this._setMatrixUniforms(shaderKey);
-  //@todo implement in shader
+  var mId = 'immediateVert|vertexColorFrag';
+  this._setMatrixUniforms(mId);
   //set our default point size
-  // this._setUniform1f(mId,
-  //   'uPointSize',
-  //   this.pointSize);
+  this._setUniform1f(mId,
+    'uPointSize',
+    this.pointSize);
   return this;
 };
 
@@ -176,6 +175,59 @@ p5.Renderer3D.prototype._getColorVertexShader = function(){
     shaderProgram = this.mHash[mId];
   }
   return shaderProgram;
+};
+
+//////////////////////////////////////////////
+//Bezier
+//////////////////////////////////////////////
+
+/**
+ * @method bezier
+ * @param  {Number} x1 x-coordinate for the first anchor point
+ * @param  {Number} y1 y-coordinate for the first anchor point
+ * @param  {Number} z1 z-coordinate for the first anchor point
+ * @param  {Number} x2 x-coordinate for the first control point
+ * @param  {Number} y2 y-coordinate for the first control point
+ * @param  {Number} z2 z-coordinate for the first control point
+ * @param  {Number} x3 x-coordinate for the first anchor point
+ * @param  {Number} y3 y-coordinate for the first anchor point
+ * @param  {Number} z3 z-coordinate for the first anchor point
+ * @param  {Number} x4 x-coordinate for the first control point
+ * @param  {Number} y4 y-coordinate for the first control point
+ * @param  {Number} z4 z-coordinate for the first control point
+ * @return {p5.Renderer3D}   [description]
+ * @TODO implement bezier in 3D
+ * @example
+ * <div>
+ * <code>
+ *background(0, 0, 0);
+ *noFill();
+ *stroke(255);
+ *bezier(250,250,0, 100,100,0, 100,0,0, 0,100,0);
+ * </code>
+ * </div>
+ */
+//this implementation of bezier curve is based on Bernstein polynomial
+p5.Renderer3D.prototype.bezier = function(args){
+  var bezierDetail=args[12] || 20;//value of Bezier detail
+  this.beginShape();
+  var coeff=[0,0,0,0];//  Bernstein polynomial coeffecients
+  var vertex=[0,0,0]; //(x,y,z) coordinates of points in bezier curve
+  for(var i=0; i<=bezierDetail; i++){
+    coeff[0]=Math.pow(1-(i/bezierDetail),3);
+    coeff[1]=(3*(i/bezierDetail)) * (Math.pow(1-(i/bezierDetail),2));
+    coeff[2]=(3*Math.pow(i/bezierDetail,2)) * (1-(i/bezierDetail));
+    coeff[3]=Math.pow(i/bezierDetail,3);
+    vertex[0]=args[0]*coeff[0] + args[3]*coeff[1] +
+              args[6]*coeff[2] + args[9]*coeff[3];
+    vertex[1]=args[1]*coeff[0] + args[4]*coeff[1] +
+              args[7]*coeff[2] + args[10]*coeff[3];
+    vertex[2]=args[2]*coeff[0] + args[5]*coeff[1] +
+              args[8]*coeff[2] + args[11]*coeff[3];
+    this.vertex(vertex[0],vertex[1],vertex[2]);
+  }
+  this.endShape();
+  return this;
 };
 
 module.exports = p5.Renderer3D;
