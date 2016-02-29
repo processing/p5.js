25a8ce31a677bc29083424ea3fdbc3d593e2c018
Harkirat Singh <harkirat96@gmail.com>
2016-03-01 00:36:57 +0530

    Fixes #1269

--- src/events/mouse.js
+++ src/events/mouse.js
@@ -317,6 +317,9 @@ p5.prototype._updateNextMouseCoords = function(e) {
      e.type === 'touchend') {
     this._setProperty('_nextMouseX', this._nextTouchX);
     this._setProperty('_nextMouseY', this._nextTouchY);
+  } else if (e.type === 'mousedown') {
+    this._setProperty('touchX', e.clientX);
+    this._setProperty('touchY', e.clientY);
   } else {
     if(this._curElement !== null) {
       var mousePos = getMousePos(this._curElement.elt, e);
