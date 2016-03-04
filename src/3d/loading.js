fb926d91db6d428859f7e79fb299382b609e291a
Matthew Kaney <matthew@mindofmatthew.com>
2016-03-04 12:21:43 -0500

    Add example to documentation

--- src/3d/loading.js
+++ src/3d/loading.js
@@ -157,7 +157,21 @@ function parseObj( model, lines ) {
  * @example
  * <div>
  * <code>
- * // example goes here
+ * //draw a spinning teapot
+ * var teapot;
+ *
+ * function setup(){
+ *   createCanvas(100, 100, WEBGL);
+ *
+ *   teapot = loadModel('teapot.obj');
+ * }
+ *
+ * function draw(){
+ *   background(200);
+ *   rotateX(frameCount * 0.01);
+ *   rotateY(frameCount * 0.01);
+ *   model(teapot);
+ * }
  * </code>
  * </div>
  */
