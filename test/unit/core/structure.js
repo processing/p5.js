f3d0afe635621eeecf331b1e2c61dc67a2b42158
Brad Buchanan <bradley.c.buchanan@gmail.com>
2016-03-24 11:22:38 -0700

    Better tests, and actually run them.
    Adds the new unit test file to the test index files so that they run.
    
    Instead of checking that particular properties are preserved by push/pop, run a deepEqual check against the render state before and after each of the operations that push is documented to preserve.  This should have a better chance of catching future subtle breaks to push/pop behavior.  Verified that the new tests actually catch the problem with _strokeSet and _fillSet that I was originally trying to fix.

--- test/unit/core/structure.js
+++ test/unit/core/structure.js
@@ -10,22 +10,149 @@ suite('Structure', function() {
   });
 
   suite('p5.prototype.push and p5.prototype.pop', function() {
-    test('should save and restore _fillSet', function() {
-      var originalFillSet = myp5._renderer._fillSet;
+    function getRenderState() {
+      var state = {};
+      for (var key in myp5._renderer) {
+        var value = myp5._renderer[key];
+        if (typeof value !== 'function') {
+          state[key] = value;
+        }
+      }
+      return state;
+    }
+
+    function assertCanPreserveRenderState(work) {
+      var originalState = getRenderState();
       myp5.push();
-      myp5._renderer._fillSet = !originalFillSet;
-      assert.not.equal(myp5._renderer._fillSet, originalFillSet);
+      work();
       myp5.pop();
-      assert.equal(myp5._renderer._fillSet, originalFillSet);
+      assert.deepEqual(getRenderState(), originalState);
+    }
+
+    test('leak no state after fill()', function() {
+      myp5.noFill();
+      assertCanPreserveRenderState(function () {
+        myp5.fill('red');
+      });
     });
 
-    test('should save and restore _strokeSet', function() {
-      var originalStrokeSet = myp5._renderer._strokeSet;
-      myp5.push();
-      myp5._renderer._strokeSet = !originalStrokeSet;
-      assert.not.equal(myp5._renderer._strokeSet, originalStrokeSet);
-      myp5.pop();
-      assert.equal(myp5._renderer._strokeSet, originalStrokeSet);
+    test('leak no state after noFill()', function () {
+      myp5.fill('red');
+      assertCanPreserveRenderState(function () {
+        myp5.noFill();
+      });
+    });
+
+    test('leak no state after stroke()', function() {
+      myp5.noStroke();
+      assertCanPreserveRenderState(function () {
+        myp5.stroke('red');
+      });
+    });
+
+    test('leak no state after noStroke()', function () {
+      myp5.stroke('red');
+      assertCanPreserveRenderState(function () {
+        myp5.noStroke();
+      });
+    });
+
+    test('leak no state after tint()', function() {
+      myp5.noTint();
+      assertCanPreserveRenderState(function () {
+        myp5.tint(255, 0, 0);
+      });
+    });
+
+    test('leak no state after noTint()', function () {
+      myp5.tint(255, 0, 0);
+      assertCanPreserveRenderState(function () {
+        myp5.noTint();
+      });
+    });
+
+    test('leak no state after strokeWeight()', function () {
+      myp5.strokeWeight(1);
+      assertCanPreserveRenderState(function () {
+        myp5.strokeWeight(10);
+      });
+    });
+
+    test('leak no state after strokeCap()', function () {
+      myp5.strokeCap(p5.ROUND);
+      assertCanPreserveRenderState(function () {
+        myp5.strokeCap(p5.SQUARE);
+      });
+    });
+
+    test('leak no state after strokeJoin()', function () {
+      myp5.strokeJoin(p5.BEVEL);
+      assertCanPreserveRenderState(function () {
+        myp5.strokeJoin(p5.MITER);
+      });
+    });
+
+    test('leak no state after imageMode()', function () {
+      myp5.imageMode(p5.CORNER);
+      assertCanPreserveRenderState(function () {
+        myp5.imageMode(p5.CENTER);
+      });
+    });
+
+    test('leak no state after rectMode()', function () {
+      myp5.rectMode(p5.CORNER);
+      assertCanPreserveRenderState(function () {
+        myp5.rectMode(p5.CENTER);
+      });
+    });
+
+    test('leak no state after ellipseMode()', function () {
+      myp5.ellipseMode(p5.CORNER);
+      assertCanPreserveRenderState(function () {
+        myp5.ellipseMode(p5.CENTER);
+      });
+    });
+
+    test('leak no state after colorMode()', function () {
+      myp5.colorMode(p5.HSB);
+      assertCanPreserveRenderState(function () {
+        myp5.colorMode(p5.RGB);
+      });
+    });
+
+    test('leak no state after textAlign()', function () {
+      myp5.textAlign(p5.RIGHT, p5.BOTTOM);
+      assertCanPreserveRenderState(function () {
+        myp5.textAlign(p5.CENTER, p5.CENTER);
+      });
+    });
+
+    test('leak no state after textFont()', function () {
+      myp5.textFont('Georgia');
+      assertCanPreserveRenderState(function () {
+        myp5.textFont('Helvetica');
+      });
+    });
+
+    test('leak no state after textStyle()', function () {
+      myp5.textStyle(p5.ITALIC);
+      assertCanPreserveRenderState(function () {
+        myp5.textStyle(p5.BOLD);
+      });
+    });
+
+    test('leak no state after textSize()', function () {
+      myp5.textSize(12);
+      assertCanPreserveRenderState(function () {
+        myp5.textSize(16);
+      });
+    });
+
+    test('leak no state after textLeading()', function () {
+      myp5.textLeading(20);
+      assertCanPreserveRenderState(function () {
+        myp5.textLeading(30);
+      });
     });
   });
 
