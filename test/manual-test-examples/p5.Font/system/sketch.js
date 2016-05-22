d621f7e4692d352b303c82e24ef19a48ce53e186
Michael Hadley <michaelwesthadley@gmail.com>
2016-05-22 14:28:32 -0500

    Add manual test for leading with system fonts
    This matches the test for leading with custom p5.Fonts here:
    23ed35b435a681654c00ce523c8bcb6665dbd3da

--- test/manual-test-examples/p5.Font/system/sketch.js
+++ test/manual-test-examples/p5.Font/system/sketch.js
@@ -313,17 +313,97 @@ var textAllAlignmentsSketch = function(p) {
 
 var textLeadingSketch = function(p) {
   p.setup = function() {
-    p.createCanvas(240, 160);
+    p.createCanvas(400, 200);
+    p.textFont("Arial");
     p.fill(0);
-    p.strokeWeight(0);
     p.textSize(12);
-    //leadig
+
+    p.line(0,100,p.width,100);
+    p.textAlign(p.LEFT, p.TOP);
+    p.strokeWeight(0);
+
+    var s10 = 'LEFT/TOP@10px',
+      s20 = s10.replace('1','2'),
+      s30 = s10.replace('1','3');
+
     p.textLeading(10);  // Set leading to 10
-    p.text("Leading10px\nLeading10px\nLeading10px", 10, 30);
+    p.text(s10+'\n'+s10+'\n'+s10, 10, 100);
     p.textLeading(20);  // Set leading to 20
-    p.text("Leading20px\nLeading20px\nLeading20px", 90, 30);
+    p.text(s20+'\n'+s20+'\n'+s20, 140, 100);
     p.textLeading(30);  // Set leading to 30
-    p.text("Leading30px\nLeading30px\nLeading30px", 170, 30);
+    p.text(s30+'\n'+s30+'\n'+s30, 270, 100);
+  };
+};
+
+var textLeadingSketch2 = function(p) {
+  p.setup = function() {
+    p.createCanvas(400, 200);
+    p.textFont("Arial");
+    p.fill(0);
+    p.textSize(12);
+
+    p.line(0,100,p.width,100);
+    p.textAlign(p.LEFT, p.CENTER);
+    p.strokeWeight(0);
+
+    var s10 = 'LEFT/CENTER@10px',
+      s20 = s10.replace('1','2'),
+      s30 = s10.replace('1','3');
+
+    p.textLeading(10);  // Set leading to 10
+    p.text(s10+'\n'+s10+'\n'+s10, 10, 100);
+    p.textLeading(20);  // Set leading to 20
+    p.text(s20+'\n'+s20+'\n'+s20, 140, 100);
+    p.textLeading(30);  // Set leading to 30
+    p.text(s30+'\n'+s30+'\n'+s30, 270, 100);
+  };
+};
+
+var textLeadingSketch3 = function(p) {
+  p.setup = function() {
+    p.createCanvas(400, 200);
+    p.textFont("Arial");
+    p.fill(0);
+    p.textSize(12);
+
+    p.line(0,100,p.width,100);
+    p.textAlign(p.LEFT, p.BASELINE);
+    p.strokeWeight(0);
+
+    var s10 = 'LEFT/BASELINE@10px',
+      s20 = s10.replace('1','2'),
+      s30 = s10.replace('1','3');
+
+    p.textLeading(10);  // Set leading to 10
+    p.text(s10+'\n'+s10+'\n'+s10, 10, 100);
+    p.textLeading(20);  // Set leading to 20
+    p.text(s20+'\n'+s20+'\n'+s20, 140, 100);
+    p.textLeading(30);  // Set leading to 30
+    p.text(s30+'\n'+s30+'\n'+s30, 270, 100);
+  };
+};
+
+var textLeadingSketch4 = function(p) {
+  p.setup = function() {
+    p.createCanvas(400, 200);
+    p.textFont("Arial");
+    p.fill(0);
+    p.textSize(12);
+
+    p.line(0,100,p.width,100);
+    p.textAlign(p.LEFT, p.BOTTOM);
+    p.strokeWeight(0);
+
+    var s10 = 'LEFT/BOTTOM@10px',
+      s20 = s10.replace('1','2'),
+      s30 = s10.replace('1','3');
+
+    p.textLeading(10);  // Set leading to 10
+    p.text(s10+'\n'+s10+'\n'+s10, 10, 100);
+    p.textLeading(20);  // Set leading to 20
+    p.text(s20+'\n'+s20+'\n'+s20, 140, 100);
+    p.textLeading(30);  // Set leading to 30
+    p.text(s30+'\n'+s30+'\n'+s30, 270, 100);
   };
 };
 
@@ -557,6 +637,9 @@ new p5(textFontSketch, 'textFontSketch');
 new p5(textAlignSketch, 'textAlignSketch');
 new p5(textAllAlignmentsSketch, 'textAllAlignmentsSketch');
 new p5(textLeadingSketch, 'textLeadingSketch');
+new p5(textLeadingSketch2, 'textLeadingSketch2');
+new p5(textLeadingSketch3, 'textLeadingSketch3');
+new p5(textLeadingSketch4, 'textLeadingSketch4');
 new p5(textSizeSketch, 'textSizeSketch');
 new p5(textStyleSketch, 'textStyleSketch');
 new p5(textWidthSketch, 'textWidthSketch');
