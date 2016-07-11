02c285c69a3d4d822fb17054362dcd256edc5ba4
indefinit <kevin.siwoff@gmail.com>
2016-07-11 18:39:35 -0400

    2d primitives in webgl now follow exact same api as in 2d mode: no z-coord

--- src/webgl/primitives.js
+++ src/webgl/primitives.js
@@ -610,15 +610,14 @@ p5.RendererGL.prototype.ellipse = function
 (args){
   var x = args[0];
   var y = args[1];
-  var z = args[2];
-  var width = args[3];
-  var height = args[4];
+  var width = args[2];
+  var height = args[3];
   //detailX and Y are optional 6th & 7th
   //arguments
-  var detailX = args[5] || 24;
-  var detailY = args[6] || 16;
+  var detailX = args[4] || 24;
+  var detailY = args[5] || 16;
   var gId = 'ellipse|'+args[0]+'|'+args[1]+'|'+args[2]+'|'+
-  args[3]+'|'+args[4];
+  args[3];
   if(!this.geometryInHash(gId)){
     var _ellipse = function(){
       var u,v,p;
@@ -628,13 +627,12 @@ p5.RendererGL.prototype.ellipse = function
           u = j / this.detailX;
           var theta = 2 * Math.PI * u;
           if(v === 0){
-            p = new p5.Vector(x, y, z);
+            p = new p5.Vector(x, y, 0);
           }
           else{
             var _x = x + width * Math.sin(theta);
             var _y = y + height * Math.cos(theta);
-            var _z = z;
-            p = new p5.Vector(_x, _y, _z);
+            p = new p5.Vector(_x, _y, 0);
           }
           this.vertices.push(p);
           this.uvs.push([u,v]);
@@ -654,16 +652,15 @@ p5.RendererGL.prototype.ellipse = function
 p5.RendererGL.prototype.rect = function
 (args){
   var gId = 'rect|'+args[0]+'|'+args[1]+'|'+args[2]+'|'+
-  args[3]+'|'+args[4];
-  var x,y,z;
-  var width = args[3];
-  var height = args[4];
-  var detailX = args[5] || 24;
-  var detailY = args[6] || 16;
+  args[3];
+  var x,y;
+  var width = args[2];
+  var height = args[3];
+  var detailX = args[4] || 24;
+  var detailY = args[5] || 16;
   if(this._rectMode === constants.CENTER){
     x = args[0] - width / 2;
     y = args[1] - height / 2;
-    z = args[2];
   }
   if(!this.geometryInHash(gId)){
     var _rect = function(){
@@ -675,7 +672,7 @@ p5.RendererGL.prototype.rect = function
           p = new p5.Vector(
             (x + width) * u,
             (y + height) * v,
-            z
+            0
           );
           this.vertices.push(p);
           this.uvs.push([u,v]);
@@ -701,26 +698,22 @@ p5.RendererGL.prototype.quad = function(){
   //
   var x1 = args[0],
     y1 = args[1],
-    z1 = args[2],
-    x2 = args[3],
-    y2 = args[4],
-    z2 = args[5],
-    x3 = args[6],
-    y3 = args[7],
-    z3 = args[8],
-    x4 = args[9],
-    y4 = args[10],
-    z4 = args[11];
-  var gId = 'quad|'+x1+'|'+y1+'|'+z1+'|'+
-  x2+'|'+y2+'|'+z2+
-  x3+'|'+y3+'|'+z3+
-  x4+'|'+y4+'|'+z4;
+    x2 = args[2],
+    y2 = args[3],
+    x3 = args[4],
+    y3 = args[5],
+    x4 = args[6],
+    y4 = args[7];
+  var gId = 'quad|'+x1+'|'+y1+'|'+
+  x2+'|'+y2+'|'+
+  x3+'|'+y3+'|'+
+  x4+'|'+y4;
   if(!this.geometryInHash(gId)){
     var _quad = function(){
-      this.vertices.push(new p5.Vector(x1,y1,z1));
-      this.vertices.push(new p5.Vector(x2,y2,z2));
-      this.vertices.push(new p5.Vector(x3,y3,z3));
-      this.vertices.push(new p5.Vector(x4,y4,z4));
+      this.vertices.push(new p5.Vector(x1,y1,0));
+      this.vertices.push(new p5.Vector(x2,y2,0));
+      this.vertices.push(new p5.Vector(x3,y3,0));
+      this.vertices.push(new p5.Vector(x4,y4,0));
       this.uvs.push([0, 0], [1, 0], [1, 1], [0, 1]);
     };
     var quadGeom = new p5.Geometry(2,2,_quad);
