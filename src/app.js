2c5b06417f8aefb245c34b8232fe5edc83f8fdea
Atul Varma <varmaa@gmail.com>
2016-09-18 09:19:46 -0400

    Support and test on-demand global mode.

--- src/app.js
+++ src/app.js
@@ -74,9 +74,10 @@ var _globalInit = function() {
   if (!window.PHANTOMJS && !window.mocha) {
     // If there is a setup or draw function on the window
     // then instantiate p5 in "global" mode
-    if((window.setup && typeof window.setup === 'function') ||
-      (window.draw && typeof window.draw === 'function')) {
-      p5.instance = new p5();
+    if(((window.setup && typeof window.setup === 'function') ||
+       (window.draw && typeof window.draw === 'function')) &&
+       !p5.instance) {
+      new p5();
     }
   }
 };
