fd76b4989d7aef4bd455cd4dce8d41b5087bc83a
Kevin Lewis <phazonoverload@users.noreply.github.com>
2016-10-10 13:59:35 +0100

    Updated docs from print() to println()

--- src/core/environment.js
+++ src/core/environment.js
@@ -22,24 +22,24 @@ var _windowPrint = window.print;
 
 if (window.console && console.log) {
   /**
-   * The print() function writes to the console area of your browser.
+   * The println() function writes to the console area of your browser.
    * This function is often helpful for looking at the data a program is
    * producing. This function creates a new line of text for each call to
    * the function. Individual elements can be
    * separated with quotes ("") and joined with the addition operator (+).
    * <br><br>
-   * While print() is similar to console.log(), it does not directly map to
+   * While println() is similar to console.log(), it does not directly map to
    * it in order to simulate easier to understand behavior than
    * console.log(). Due to this, it is slower. For fastest results, use
    * console.log().
    *
-   * @method print
+   * @method println
    * @param {Any} contents any combination of Number, String, Object, Boolean,
    *                       Array to print
    * @example
    * <div><code class='norender'>
    * var x = 10;
-   * print("The value of x is " + x);
+   * println("The value of x is " + x);
    * // prints "The value of x is 10"
    * </code></div>
    * @alt
