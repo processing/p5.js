cf06437213f735ca6f20d3d8e9012a0d634e1c35
Benoit <dabe@users.noreply.github.com>
2017-01-28 20:44:06 +1100

    Update mouse.js
    The property "touches" for a TouchEnd event is empty (tested with chrome). In this case the function getMousePos raises an exception. So this modification test if touches contains elements.

--- src/events/mouse.js
+++ src/events/mouse.js
@@ -353,7 +353,7 @@ p5.prototype.mouseIsPressed = false;
 p5.prototype.isMousePressed = false; // both are supported
 
 p5.prototype._updateNextMouseCoords = function(e) {
-  if(this._curElement !== null) {
+  if(this._curElement !== null && (!e.touches || e.touches.length>0)) {
     var mousePos = getMousePos(this._curElement.elt, this.width, this.height, e);
     this._setProperty('mouseX', mousePos.x);
     this._setProperty('mouseY', mousePos.y);
