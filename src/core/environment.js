98fcd76ebd5c7e74855412259c041505e109d404
feedzh <reijovosu@gmail.com>
2017-01-31 17:48:40 +0000

    fixing css cursors not appearing issue #1773

--- src/core/environment.js
+++ src/core/environment.js
@@ -169,8 +169,8 @@ p5.prototype.cursor = function(type, x, y) {
       // Note that x and y values must be unit-less positive integers < 32
       // https://developer.mozilla.org/en-US/docs/Web/CSS/cursor
       coords = x + ' ' + y;
-    }
-    if (type.substring(0, 6) !== 'http://') {
+    }        
+    if ((type.substring(0, 7) === 'http://') || (type.substring(0, 8) === 'https://')) {
       // Image (absolute url)
       cursor = 'url(' + type + ') ' + coords + ', auto';
     } else if (/\.(cur|jpg|jpeg|gif|png|CUR|JPG|JPEG|GIF|PNG)$/.test(type)) {
