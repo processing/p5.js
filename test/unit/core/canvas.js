7cdfda11291d632e1a10049caecfc1e746255944
Joe Cridge <joe.cridge@me.com>
2017-02-04 18:56:48 +0000

    Remove erroneous ‘done’s

--- test/unit/core/canvas.js
+++ test/unit/core/canvas.js
@@ -14,22 +14,22 @@ suite('Canvas', function() {
       assert.typeOf(modeAdjust, 'function');
     });
 
-    test('should convert numeric strings when rectMode is CORNER', function(done) {
+    test('should convert numeric strings when rectMode is CORNER', function() {
       result = modeAdjust('10', '24', '32', '50', CORNER);
       assert.deepEqual(result, { x: 10, y: 24, w: 32, h: 50 });
     });
 
-    test('should convert numeric strings when rectMode is CORNERS', function(done) {
+    test('should convert numeric strings when rectMode is CORNERS', function() {
       result = modeAdjust('10', '24', '32', '50', CORNERS);
       assert.deepEqual(result, { x: 10, y: 24, w: 22, h: 26 });
     });
 
-    test('should convert numeric strings when rectMode is RADIUS', function(done) {
+    test('should convert numeric strings when rectMode is RADIUS', function() {
       result = modeAdjust('10', '24', '32', '50', RADIUS);
       assert.deepEqual(result, { x: -22, y: -26, w: 64, h: 100 });
     });
 
-    test('should convert numeric strings when rectMode is CENTER', function(done) {
+    test('should convert numeric strings when rectMode is CENTER', function() {
       result = modeAdjust('10', '24', '32', '50', CENTER);
       assert.deepEqual(result, { x: -6, y: -1, w: 32, h: 50 });
     });
@@ -43,22 +43,22 @@ suite('Canvas', function() {
       assert.typeOf(arcModeAdjust, 'function');
     });
 
-    test('should convert numeric strings when ellipseMode is CORNER', function(done) {
+    test('should convert numeric strings when ellipseMode is CORNER', function() {
       result = arcModeAdjust('10', '24', '32', '50', CORNER);
       assert.deepEqual(result, { x: 26, y: 49, w: 32, h: 50 });
     });
 
-    test('should convert numeric strings when ellipseMode is CORNERS', function(done) {
+    test('should convert numeric strings when ellipseMode is CORNERS', function() {
       result = arcModeAdjust('10', '24', '32', '50', CORNERS);
       assert.deepEqual(result, { x: 10, y: 24, w: 42, h: 74 });
     });
 
-    test('should convert numeric strings when ellipseMode is RADIUS', function(done) {
+    test('should convert numeric strings when ellipseMode is RADIUS', function() {
       result = arcModeAdjust('10', '24', '32', '50', RADIUS);
       assert.deepEqual(result, { x: 10, y: 24, w: 64, h: 100 });
     });
 
-    test('should convert numeric strings when ellipseMode is CENTER', function(done) {
+    test('should convert numeric strings when ellipseMode is CENTER', function() {
       result = arcModeAdjust('10', '24', '32', '50', CENTER);
       assert.deepEqual(result, { x: 10, y: 24, w: 32, h: 50 });
     });
