1b6c2f4f9e858f60a3de6347b5ba72e0d7febf3f
limzykenneth <limzy.kenneth@gmail.com>
2017-03-10 17:02:42 +0800

    Add basic loadXML tests

--- test/unit/io/files_input.js
+++ test/unit/io/files_input.js
@@ -2,6 +2,7 @@ suite('Files', function() {
 
   var loadJSON = p5.prototype.loadJSON;
   var loadStrings = p5.prototype.loadStrings;
+  var loadXML = p5.prototype.loadXML;
 
   //variable for preload
   var preload = p5.prototype.preload;
@@ -15,6 +16,7 @@ suite('Files', function() {
     assert.typeOf(preload, 'Boolean');
   });
 
+  // loadJSON()
   suite('loadJSON() in Preload', function () {
     test('should be a function', function() {
       assert.ok(loadJSON);
@@ -28,6 +30,7 @@ suite('Files', function() {
     });
   });
 
+  // loadStrings()
   suite('loadStrings() in Preload', function(){
     test('should be a function', function() {
       assert.ok(loadStrings);
@@ -41,9 +44,24 @@ suite('Files', function() {
     });
   });
 
+  // loadXML()
+  suite('loadXML() in Preload', function(){
+    test('should be a function', function(){
+      assert.ok(loadXML);
+      assert.typeOf(loadXML, 'function');
+    });
+
+    // test('should return an Object', function() {
+    //   result = loadXML('unit/assets/test.xml');
+    //   assert.ok(result);
+    //   assert.isObject(result, 'result is an object');
+    // });
+  });
+
   //tests while preload is false with callbacks
   preload = false;
 
+  // loadJSON()
   suite('p5.prototype.loadJSON', function() {
     test('should be a function', function() {
       assert.ok(loadJSON);
@@ -94,6 +112,7 @@ suite('Files', function() {
     });*/
   });
 
+  // loadStrings()
   suite('p5.prototype.loadStrings', function() {
     test('should be a function', function() {
       assert.ok(loadStrings);
@@ -130,87 +149,95 @@ suite('Files', function() {
       });
     });
   });
-  /*
-  var loadTable = p5.prototype.loadTable;
 
-
-  suite('p5.prototype.loadTable',function(){
-    var url = 'http://localhost:9001/unit/assets/csv.csv';
-
+  // loadXML()
+  suite('p5.prototype.loadXML', function(){
     test('should be a function', function(){
-      assert.isFunction(loadTable);
-    });
-
-    test('should load a file without options',function(done) {
-      var myCallback = function(resp){
-        assert.ok(resp);
-        done();
-      };
-      loadTable(url,myCallback);
-    });
-
-    test('the loaded file should be correct',function(done){
-      var myCallback = function(resp){
-        assert.equal(resp.getRowCount(),4);
-        assert.strictEqual(resp.getRow(1).getString(0),'David');
-        assert.strictEqual(resp.getRow(1).getNum(1),31);
-        done();
-      };
-      loadTable(url,myCallback);
-    });
-
-    test('using the csv option works', function(done){
-      var myCallback = function(resp){
-        assert.equal(resp.getRowCount(),4);
-        assert.strictEqual(resp.getRow(1).getString(0),'David');
-        assert.strictEqual(resp.getRow(1).getNum(1),31);
-        done();
-      };
-      loadTable(url,'csv',myCallback);
-    });
-    test('using the csv and tsv options fails', function(){
-      var fn = function(){loadTable(url,'csv','tsv');};
-      assert.throw(fn,'Cannot set multiple separator types.');
-    });
-
-    test('using the header option works', function(done){
-      var myCallback = function(resp){
-        assert.equal(resp.getRowCount(),3);
-        assert.strictEqual(resp.getRow(0).getString('name'),'David');
-        assert.strictEqual(resp.getRow(0).getNum('age'),31);
-        done();
-      };
-      loadTable(url,'header',myCallback);
-    });
-    test('using the header and csv options together works', function(done){
-      var myCallback = function(resp){
-        assert.equal(resp.getRowCount(),3);
-        assert.strictEqual(resp.getRow(0).getString('name'),'David');
-        assert.strictEqual(resp.getRow(0).getNum('age'),31);
-        done();
-      };
-      loadTable(url,'header', 'csv', myCallback);
-    });
-
-    test('CSV files should handle commas within quoted fields',function(done){
-      var myCallback = function(resp){
-        assert.equal(resp.getRowCount(),4);
-        assert.equal(resp.getRow(2).get(0),'David, Jr.');
-        assert.equal(resp.getRow(2).getString(0),'David, Jr.');
-        assert.equal(resp.getRow(2).get(1),'11');
-        assert.equal(resp.getRow(2).getString(1),11);
-        done();
-      };
-      loadTable(url,myCallback);
-    });
-    test('CSV files should handle escaped quotes and returns within quoted fields',function(done){
-      var myCallback = function(resp){
-        assert.equal(resp.getRowCount(),4);
-        assert.equal(resp.getRow(3).get(0),'David,\nSr. "the boss"');
-        done();
-      };
-      loadTable(url,myCallback);
+      assert.ok(loadXML);
+      assert.typeOf(loadXML, 'function');
     });
   });
-  */
+
+
+  // var loadTable = p5.prototype.loadTable;
+
+  // suite('p5.prototype.loadTable',function(){
+  //   var url = 'unit/assets/csv.csv';
+
+  //   test('should be a function', function(){
+  //     assert.isFunction(loadTable);
+  //   });
+
+  //   test('should load a file without options',function(done) {
+  //     var myCallback = function(resp){
+  //       assert.ok(resp);
+  //       done();
+  //     };
+  //     loadTable(url,myCallback);
+  //   });
+
+  //   test('the loaded file should be correct',function(done){
+  //     var myCallback = function(resp){
+  //       assert.equal(resp.getRowCount(),4);
+  //       assert.strictEqual(resp.getRow(1).getString(0),'David');
+  //       assert.strictEqual(resp.getRow(1).getNum(1),31);
+  //       done();
+  //     };
+  //     loadTable(url,myCallback);
+  //   });
+
+  //   test('using the csv option works', function(done){
+  //     var myCallback = function(resp){
+  //       assert.equal(resp.getRowCount(),4);
+  //       assert.strictEqual(resp.getRow(1).getString(0),'David');
+  //       assert.strictEqual(resp.getRow(1).getNum(1),31);
+  //       done();
+  //     };
+  //     loadTable(url,'csv',myCallback);
+  //   });
+  //   test('using the csv and tsv options fails', function(){
+  //     var fn = function(){loadTable(url,'csv','tsv');};
+  //     assert.throw(fn,'Cannot set multiple separator types.');
+  //   });
+
+  //   test('using the header option works', function(done){
+  //     var myCallback = function(resp){
+  //       assert.equal(resp.getRowCount(),3);
+  //       assert.strictEqual(resp.getRow(0).getString('name'),'David');
+  //       assert.strictEqual(resp.getRow(0).getNum('age'),31);
+  //       done();
+  //     };
+  //     loadTable(url,'header',myCallback);
+  //   });
+  //   test('using the header and csv options together works', function(done){
+  //     var myCallback = function(resp){
+  //       assert.equal(resp.getRowCount(),3);
+  //       assert.strictEqual(resp.getRow(0).getString('name'),'David');
+  //       assert.strictEqual(resp.getRow(0).getNum('age'),31);
+  //       done();
+  //     };
+  //     loadTable(url,'header', 'csv', myCallback);
+  //   });
+
+  //   test('CSV files should handle commas within quoted fields',function(done){
+  //     var myCallback = function(resp){
+  //       assert.equal(resp.getRowCount(),4);
+  //       assert.equal(resp.getRow(2).get(0),'David, Jr.');
+  //       assert.equal(resp.getRow(2).getString(0),'David, Jr.');
+  //       assert.equal(resp.getRow(2).get(1),'11');
+  //       assert.equal(resp.getRow(2).getString(1),11);
+  //       done();
+  //     };
+  //     loadTable(url,myCallback);
+  //   });
+  //   test('CSV files should handle escaped quotes and returns within quoted fields',function(done){
+  //     var myCallback = function(resp){
+  //       assert.equal(resp.getRowCount(),4);
+  //       assert.equal(resp.getRow(3).get(0),'David,\nSr. "the boss"');
+  //       done();
+  //     };
+  //     loadTable(url,myCallback);
+  //   });
+  // });
+
 });
