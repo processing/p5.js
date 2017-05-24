ffcc0c422ecbc79a059ca3fb0769afb855f5f92e
mlarghydracept <mark.grigg@purchase.edu>
2017-05-24 12:42:27 -0700

    second

--- src/data/p5.StringDict.js
+++ src/data/p5.StringDict.js
@@ -3,10 +3,8 @@
  * @submodule Dictionary
  * @for p5
  */
-
 var p5 = require('./core');
-var constants = require('./constants');
-
+//var constants = require('./constants');
 /**
  *
  * A simple table class to use a String as a lookup for another String value.
@@ -19,18 +17,27 @@ var constants = require('./constants');
  * @param {Object} [pInst] pointer to p5 instance
  * @param {Boolean} whether we're using it as main canvas
  */
+
 p5.StringDict = function(keys, values) {
-this.count = keys.length;
-this.data = {};
-  if(count >= 1) {
-    for(var i=0; i<count; i++) {
-      this.data.push({
-        key: keys[i];
-        value: values[i];
-      });
+
+  this.data = {};
+  this.count = 0;
+
+  if(arguments.length === 1) {
+    if(keys instanceof Object) {
+      this.data = keys;
+      this.count = keys.length;
     }
-  }
-  
+  } /*else if(arguments.length > 1) {
+    this.count = keys.length;
+    if(count >= 1) {
+      for(var i=0; i<count; i++) {
+        this.data.push({
+          keys[i]: values[i];
+        });
+      }
+    }
+  }*/
 
   return this;
 };
@@ -39,27 +46,46 @@ p5.StringDict.prototype.size = function(){
   return this.count;
 };
 
-p5.StringDict.prototype.get = function(key) {
+p5.StringDict.prototype.getValue = function(key) {
   if(key in this.data){
-      return this.data[key];
-  } else {
-      throw key + ' does not exist in this Dictionary';
-   }
+    return this.data[key];
+  }else{
+    throw key + ' does not exist in this Dictionary';
+  }
+};
+
+p5.StringDict.prototype.add = function(key, value) {
+  if(arguments.length === 1) {
+    if(key instanceof Object) {
+      this.data.push(key);
+    }
+  } else if (arguments.length === 2) {
+    this.data.push({
+      key: value
+    });
+  }
 };
 
 p5.StringDict.prototype.clear = function(){
   this.data = {};
   this.count = 0;
-}
-
-p5.Graphics.prototype.remove = function(key) {
-    var index = this.data.indexOf(key);
-    if(index > -1) {
-      this.data.splice(index, 1);
-      this.count--;
-    } else {
-      throw key + ' does not exist in this Dictionary';
-    }
 };
+
+p5.StringDict.prototype.remove = function(key) {
+  var index = this.data.indexOf(key);
+  if(index > -1) {
+    this.data.splice(index, 1);
+    this.count--;
+  } else {
+    throw key + ' does not exist in this Dictionary';
+  }
+};
+
+p5.StringDict.prototype.print = function() {
+  for(var i=0; i<this.count; i++) {
+    console.log(this.data[i] + '\n');
+  }
+};
+
 
 module.exports = p5.StringDict;
