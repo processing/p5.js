962af5a3370da2055540b6e4ee6cbd8fc7da519b
David Rodrigues <david@sixhat.net>
2017-06-20 14:34:36 +0100

    corrected randomPoisson for numerical stability of large lambdas

--- src/math/random.js
+++ src/math/random.js
@@ -243,15 +243,14 @@ p5.prototype.randomGaussian = function(mean, sd)  {
 };
 
 /**
- *
  * Returns a random number (or an array of numbers) fitting a Poisson
- * distribution using Donald Knuth algorithm.
+ * distribution using Donald Knuth algorithm adapted by Junhao.
  * <br><br>
  * Takes 1 or 2 arguments, the mean and the number of elments to return.<br>
  * @method randomPoisson
  * @param  {Number} mean of distribution
  * @param  {Number} elements to return
- * @return {Number} the random number
+ * @return {Number} the random number or an array of random numbers
  * @example
  * <div>
  * <code>
@@ -283,23 +282,40 @@ p5.prototype.randomPoisson = function (mean, elements) {
       'argument, the mean';
   }
   elements = 'number' === typeof elements ? elements : 1;
-
-  var L = Math.exp(-mean);
   var out = new Array(elements);
 
   for (var i = out.length - 1; i >= 0; i--) {
-    var p = 1.0;
-    var k = 0;
-
-    do {
-      k++;
-      p *= Math.random();
-    } while (p > L);
-
-    out[i] = k - 1;
+    out[i] = this._randomPoissonJunhaoKnuth(mean);
   }
 
   return 1 === elements ? out[0] : out;
+};
+/**
+ * Auxiliary function to calculate poisson numbers by the Junhao Knuth method
+ * @param  {Number} mu mean of distribution
+ * @return {Number}    the Random Number
+ */
+p5.prototype._randomPoissonJunhaoKnuth = function(mu){
+  var lLeft = mu;
+  var k = 0;
+  var p = 1;
+  var e = Math.exp(1);
+  var STEP = 500;
+  var eSTEP = Math.exp(STEP);
+  do {
+    k++;
+    p *= Math.random();
+    if (p<e && lLeft>0){
+      if (lLeft>STEP){
+        p*= eSTEP;
+        lLeft -= STEP;
+      } else {
+        p*=Math.exp(lLeft);
+        lLeft = -1;
+      }
+    }
+  } while (p>1);
+  return k - 1;
 };
 
 module.exports = p5;
