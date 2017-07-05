e5612c25e4abbc3c999b5720a6d85733db7cf661
mlarghydracept <mark.grigg@purchase.edu>
2017-07-04 17:36:33 -0700

    handles creating buffers when shader changed (slow!)

--- src/webgl/p5.RendererGL.js
+++ src/webgl/p5.RendererGL.js
@@ -249,6 +249,9 @@ p5.RendererGL.prototype._getShader = function(vertId, fragId, isImmediateMode) {
     var shaderProgram = this._initShaders(vertId, fragId, isImmediateMode);
     this.mHash[mId] = shaderProgram;
   }
+  if(this.curShaderId !== mId) {
+    this.changedShader = true;
+  }
   this.curShaderId = mId;
 
   return this.mHash[this.curShaderId];
@@ -356,7 +359,6 @@ p5.RendererGL.prototype.noFill = function() {
       this.curStrokeColor[2],
       this.curStrokeColor[3]);
   }
-
 
   return this;
 };
