342faee963ebf7303b624256ec7a35c2256fffe8
Pierre Krafft <kpierre@outlook.com>
2017-10-15 01:20:54 +0200

    Add example for p5.Image
    Closes #1988

--- src/image/p5.Image.js
+++ src/image/p5.Image.js
@@ -35,6 +35,52 @@ var Filters = require('./filters');
  * <br><br>
  * Before using the pixels[] array, be sure to use the loadPixels() method on
  * the image to make sure that the pixel data is properly loaded.
+ * @example
+ * <div><code>
+ *
+ * function setup() {
+ *   var img = createImage(100, 100); // same as new p5.Image(100, 100);
+ *   img.loadPixels();
+ *   createCanvas(100, 100);
+ *   background(0);
+ *
+ *   // helper for writing color to array
+ *   function writeColor(image, x, y, red, green, blue, alpha) {
+ *     var index = (x + (y * width)) * 4;
+ *     image.pixels[index]   = red;
+ *     image.pixels[index+1] = green;
+ *     image.pixels[index+2] = blue;
+ *     image.pixels[index+3] = alpha;
+ *   }
+ *
+ *   // fill with random colors
+ *   for (var y = 0; y < img.height; y++) {
+ *     for (var x = 0; x < img.width; x++) {
+ *       var red = random(255);
+ *       var green = random(255);
+ *       var blue = random(255);
+ *       var alpha = 255;
+ *       writeColor(img, x, y, red, green, blue, alpha);
+ *     }
+ *   }
+ *
+ *   // draw a red line
+ *   for (var x = 0; x < img.width; x++) {
+ *     var y = 0;
+ *     writeColor(img, x, y, 255, 0, 0, 255);
+ *   }
+ *
+ *   // draw a green line
+ *   for (var x = 0; x < img.width; x++) {
+ *     var y = img.height - 1;
+ *     writeColor(img, x, y, 0, 255, 0, 255);
+ *   }
+ *
+ *   img.updatePixels();
+ *   image(img, 0, 0);
+ * }
+ * </code></div>
+ *
  *
  * @class p5.Image
  * @constructor
@@ -118,7 +164,7 @@ p5.Image = function(width, height){
    * values of the pixel at (1, 0). More generally, to set values for a pixel
    * at (x, y):
    * ```javascript
-   * var d = pixelDensity;
+   * var d = pixelDensity();
    * for (var i = 0; i < d; i++) {
    *   for (var j = 0; j < d; j++) {
    *     // loop over
