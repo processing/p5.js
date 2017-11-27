3efb629758497498c70cdd11fcdb41a905c473aa
dhowe <daniel@rednoise.org>
2017-11-27 10:22:20 +0900

    fix to #2378

--- src/core/p5.Renderer.js
+++ src/core/p5.Renderer.js
@@ -86,7 +86,7 @@ p5.Renderer.prototype.resize = function(w, h) {
 
 p5.Renderer.prototype.textLeading = function(l) {
 
-  if (arguments.length && arguments[0]) {
+  if (arguments.length && typeof arguments[0] === 'number') { // #2378
 
     this._setProperty('_textLeading', l);
     return this;
@@ -97,7 +97,7 @@ p5.Renderer.prototype.textLeading = function(l) {
 
 p5.Renderer.prototype.textSize = function(s) {
 
-  if (arguments.length && arguments[0]) {
+  if (arguments.length && typeof arguments[0] === 'number') { // #2378
 
     this._setProperty('_textSize', s);
     this._setProperty('_textLeading', s * constants._DEFAULT_LEADMULT);
