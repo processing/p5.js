d2a78fb6eeac7d507b6d225d91d95ff9295c06e6
piersh <piersh@hotmail.com>
2017-12-05 22:54:18 -0800

    cache viewport in renderer

--- src/webgl/p5.RendererGL.js
+++ src/webgl/p5.RendererGL.js
@@ -143,6 +143,9 @@ p5.RendererGL.prototype._initContext = function() {
       gl.enable(gl.DEPTH_TEST);
       gl.depthFunc(gl.LEQUAL);
       gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+      this._viewport = this.drawingContext.getParameter(
+        this.drawingContext.VIEWPORT
+      );
     }
   } catch (er) {
     throw new Error(er);
@@ -652,6 +655,7 @@ p5.RendererGL.prototype.resize = function(w, h) {
     this.GL.drawingBufferWidth,
     this.GL.drawingBufferHeight
   );
+  this._viewport = this.GL.getParameter(this.GL.VIEWPORT);
   // If we're using the default camera, update the aspect ratio
   if (this._curCamera === null || this._curCamera === 'default') {
     this._curCamera = null;
