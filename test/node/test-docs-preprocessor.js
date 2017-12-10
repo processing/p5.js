2ed02a336b836e92af9d2f4b75c94b4f24ee01f5
piersh <piersh@hotmail.com>
2017-12-09 22:57:46 -0800

    fix mocha test, and prettify

--- test/node/test-docs-preprocessor.js
+++ test/node/test-docs-preprocessor.js
@@ -14,20 +14,23 @@ describe('docs preprocessor', function() {
 
     it('should merge methods with the same name', function() {
       var data = {
-        classitems: [{
-          file: 'foo.js',
-          line: 1,
-          description: 'Does foo.',
-          itemtype: 'method',
-          name: 'foo',
-          params: [{name: 'bar', type: 'String'}]
-        }, {
-          file: 'foo.js',
-          line: 5,
-          itemtype: 'method',
-          name: 'foo',
-          params: [{name: 'baz', type: 'Number'}]
-        }],
+        classitems: [
+          {
+            file: 'foo.js',
+            line: 1,
+            description: 'Does foo.',
+            itemtype: 'method',
+            name: 'foo',
+            params: [{ name: 'bar', type: 'String' }]
+          },
+          {
+            file: 'foo.js',
+            line: 5,
+            itemtype: 'method',
+            name: 'foo',
+            params: [{ name: 'baz', type: 'Number' }]
+          }
+        ],
         consts: {}
       };
 
@@ -38,12 +41,21 @@ describe('docs preprocessor', function() {
           {
             file: 'foo.js',
             line: 1,
-            params: [{name: 'bar', type: 'String'}]
-          }, {
-            line: 5,
-            params: [{name: 'baz', type: 'Number'}]
-          }]
-        }],
+            description: 'Does foo.',
+            itemtype: 'method',
+            name: 'foo',
+            overloads: [
+              {
+                line: 1,
+                params: [{ name: 'bar', type: 'String' }]
+              },
+              {
+                line: 5,
+                params: [{ name: 'baz', type: 'Number' }]
+              }
+            ]
+          }
+        ],
         consts: {}
       });
     });
@@ -51,8 +63,8 @@ describe('docs preprocessor', function() {
     it('should not merge methods from different classes', function() {
       ensureMergeDoesNothing({
         classitems: [
-          {itemtype: 'method', class: 'Bar', name: 'foo'},
-          {itemtype: 'method', class: 'Baz', name: 'foo'}
+          { itemtype: 'method', class: 'Bar', name: 'foo' },
+          { itemtype: 'method', class: 'Baz', name: 'foo' }
         ],
         consts: {}
       });
@@ -61,8 +73,8 @@ describe('docs preprocessor', function() {
     it('should not merge properties', function() {
       ensureMergeDoesNothing({
         classitems: [
-          {itemtype: 'property', name: 'foo'},
-          {itemtype: 'property', name: 'foo'}
+          { itemtype: 'property', name: 'foo' },
+          { itemtype: 'property', name: 'foo' }
         ],
         consts: {}
       });
