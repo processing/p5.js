aa677f2fa1b95d78f05ecb2f16532abcfbe8ab17
piersh <piersh@hotmail.com>
2017-12-31 22:37:56 -0800

    prettify

--- src/webgl/primitives.js
+++ src/webgl/primitives.js
@@ -677,7 +677,7 @@ p5.prototype.icosphere = function(radiusX, radiusY, radiusZ, detail) {
   if (!this._renderer.geometryInHash(gId)) {
     var g = new p5.Geometry(detail);
 
-    function addVertex(p) {
+    var addVertex = function(p) {
       p.normalize();
       g.vertices.push(p);
       g.vertexNormals.push(p);
@@ -686,12 +686,12 @@ p5.prototype.icosphere = function(radiusX, radiusY, radiusZ, detail) {
         ((Math.atan2(p.x, p.z) / Math.PI + 1) / 2) % 1,
         Math.acos(-p.y) / Math.PI
       ]);
-    }
+    };
 
     var midPointIndexCache = {};
 
     // return index of point in the middle of p1 and p2
-    function midPoint(p1, p2) {
+    var midPoint = function(p1, p2) {
       var key = p1 < p2 ? p1 + ',' + p2 : p2 + ',' + p1;
       var ret = midPointIndexCache[key];
       if (typeof ret === 'undefined') {
@@ -699,7 +699,7 @@ p5.prototype.icosphere = function(radiusX, radiusY, radiusZ, detail) {
         addVertex(p5.Vector.add(g.vertices[p1], g.vertices[p2]));
       }
       return ret;
-    }
+    };
 
     /*
     addVertex(new p5.Vector(0.0, -1.0, 0.0)); //
@@ -723,18 +723,18 @@ p5.prototype.icosphere = function(radiusX, radiusY, radiusZ, detail) {
     // golden ratio FTW!
     var phi = (1 + Math.sqrt(5)) / 2;
 
-    addVertex(new p5.Vector(-1,  phi,  0));
-    addVertex(new p5.Vector( 1,  phi,  0));
-    addVertex(new p5.Vector(-1, -phi,  0));
-    addVertex(new p5.Vector( 1, -phi,  0));
-    addVertex(new p5.Vector( 0, -1,  phi));
-    addVertex(new p5.Vector( 0,  1,  phi));
-    addVertex(new p5.Vector( 0, -1, -phi));
-    addVertex(new p5.Vector( 0,  1, -phi));
-    addVertex(new p5.Vector( phi,  0, -1));
-    addVertex(new p5.Vector( phi,  0,  1));
-    addVertex(new p5.Vector(-phi,  0, -1));
-    addVertex(new p5.Vector(-phi,  0,  1));
+    addVertex(new p5.Vector(-1, phi, 0));
+    addVertex(new p5.Vector(1, phi, 0));
+    addVertex(new p5.Vector(-1, -phi, 0));
+    addVertex(new p5.Vector(1, -phi, 0));
+    addVertex(new p5.Vector(0, -1, phi));
+    addVertex(new p5.Vector(0, 1, phi));
+    addVertex(new p5.Vector(0, -1, -phi));
+    addVertex(new p5.Vector(0, 1, -phi));
+    addVertex(new p5.Vector(phi, 0, -1));
+    addVertex(new p5.Vector(phi, 0, 1));
+    addVertex(new p5.Vector(-phi, 0, -1));
+    addVertex(new p5.Vector(-phi, 0, 1));
 
     var faces = [
       [0, 5, 11],
@@ -759,7 +759,6 @@ p5.prototype.icosphere = function(radiusX, radiusY, radiusZ, detail) {
       [9, 1, 8]
     ];
 
-
     // refine triangles
     for (var i = 0; i < detail; i++) {
       var faces2 = [];
@@ -775,23 +774,18 @@ p5.prototype.icosphere = function(radiusX, radiusY, radiusZ, detail) {
         var bc = midPoint(b, c);
         var ca = midPoint(c, a);
 
-        faces2.push(
-          [a, ab, ca],
-          [b, bc, ab],
-          [c, ca, bc],
-          [ab, bc, ca]
-        );
+        faces2.push([a, ab, ca], [b, bc, ab], [c, ca, bc], [ab, bc, ca]);
       }
       faces = faces2;
     }
 
-    function dupe(iv, u, v) {
+    var dupe = function(iv, u, v) {
       var ret = g.vertices.length;
       g.vertices.push(g.vertices[iv]);
       g.vertexNormals.push(g.vertices[iv]);
       g.uvs.push([u, v]);
       return ret;
-    }
+    };
 
     var q, iv;
     for (var fi = 0; fi < faces.length; fi++) {
@@ -803,7 +797,10 @@ p5.prototype.icosphere = function(radiusX, radiusY, radiusZ, detail) {
       var u2 = uv2[0];
       var u3 = uv3[0];
 
-      if ((u1 < 0.25 || u2 < 0.25 || u3 < 0.25) && (u1 > 0.75 || u2 > 0.75 || u3 > 0.75)) {
+      if (
+        (u1 < 0.25 || u2 < 0.25 || u3 < 0.25) &&
+        (u1 > 0.75 || u2 > 0.75 || u3 > 0.75)
+      ) {
         for (q = 0; q < 3; q++) {
           iv = face[q];
           if (g.uvs[iv][0] === 0) {
@@ -817,9 +814,7 @@ p5.prototype.icosphere = function(radiusX, radiusY, radiusZ, detail) {
         var v = g.uvs[face[q]][1];
         if (v % 1 === 0) {
           var u =
-            (g.uvs[face[(q + 1) % 3]][0] +
-              g.uvs[face[(q + 2) % 3]][0]) /
-            2;
+            (g.uvs[face[(q + 1) % 3]][0] + g.uvs[face[(q + 2) % 3]][0]) / 2;
           face[q] = dupe(iv, u, v);
         }
       }
