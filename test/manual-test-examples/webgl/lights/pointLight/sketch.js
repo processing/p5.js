36efd95efd48b6711a75b734b8587fa529e6ac63
Stalgia Grigg <stalgia.grigg@gmail.com>
2018-01-07 14:37:26 -0800

    fix light references and z coord for manual test

--- test/manual-test-examples/webgl/lights/pointLight/sketch.js
+++ test/manual-test-examples/webgl/lights/pointLight/sketch.js
@@ -6,7 +6,7 @@ function draw(){
   background(0);
 
   ambientLight(50);
-  pointLight(250, 250, 250, mouseX - width / 2, mouseY - height / 2, 0);
+  pointLight(250, 250, 250, mouseX - width / 2, mouseY - height / 2, 100);
   
   ambientMaterial(250);
   sphere(50, 64);
