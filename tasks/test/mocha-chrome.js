4c087e0fda8765f62d86b3caa2d03a9bf9fbf524
piersh <piersh@hotmail.com>
2018-05-07 15:43:16 -0700

    make mochaChrome tests run sequentially

--- tasks/test/mocha-chrome.js
+++ tasks/test/mocha-chrome.js
@@ -3,18 +3,20 @@
 const MochaCrome = require('mocha-chrome');
 
 module.exports = function(grunt) {
-  grunt.registerMultiTask('mochaChrome', function() {
+  grunt.registerMultiTask('mochaChrome', async function() {
     var done = this.async();
-    var options = this.data.options;
-    Promise.all(
-      options.urls.map(function(testURL) {
-        return new Promise(function(resolve, reject) {
-          var my_opts = Object.assign({}, options);
-          my_opts.urls = undefined;
-          my_opts.url = testURL;
-          my_opts.chromeFlags = ['--no-sandbox', '--mute-audio'];
-          my_opts.logLevel = 'trace';
-          var runner = new MochaCrome(my_opts);
+
+    try {
+      var options = this.data.options;
+
+      for (var testURL of options.urls) {
+        var my_opts = Object.assign({}, options);
+        my_opts.urls = undefined;
+        my_opts.url = testURL;
+        my_opts.chromeFlags = ['--no-sandbox', '--mute-audio'];
+        my_opts.logLevel = 'trace';
+        var runner = new MochaCrome(my_opts);
+        var result = new Promise((resolve, reject) => {
           runner.on('ended', function(stats) {
             if (stats.failures) {
               reject(stats);
@@ -23,17 +25,23 @@ module.exports = function(grunt) {
             }
           });
           runner.on('failure', reject);
-          runner.connect().then(() => runner.run());
         });
-      })
-    )
-      .then(done)
-      .catch(function(e) {
-        if (e instanceof Error) {
-          done(e);
-        } else {
-          done(new Error(e));
-        }
-      });
+
+        (async function() {
+          await runner.connect();
+          await runner.run();
+        })();
+
+        await result;
+      }
+
+      done();
+    } catch (e) {
+      if (e instanceof Error) {
+        done(e);
+      } else {
+        done(new Error(e));
+      }
+    }
   });
 };
