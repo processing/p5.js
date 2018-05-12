9f3ab6705b6fb2cc2b5ed83315c4db8b550bfd63
Spongman <piersh@hotmail.com>
2018-05-11 22:28:21 -0700

    Update vertex.js
    fix `vertex` 3d overload:
    - make it `@chainable`
    - enforce `z` parameter (to disambiguate it from the 2d overload)

--- src/core/vertex.js
+++ src/core/vertex.js
@@ -733,9 +733,10 @@ p5.prototype.quadraticVertex = function(cx, cy, x3, y3) {
  * @method vertex
  * @param  {Number} x
  * @param  {Number} y
- * @param  {Number} [z] z-coordinate of the vertex
+ * @param  {Number} z   z-coordinate of the vertex
  * @param  {Number} [u] the vertex's texture u-coordinate
  * @param  {Number} [v] the vertex's texture v-coordinate
+ * @chainable
  */
 p5.prototype.vertex = function(x, y, moveTo, u, v) {
   if (this._renderer.isP3D) {
