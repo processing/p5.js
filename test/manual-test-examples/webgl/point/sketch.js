9cdda47a09b2b50547acac1a20787a4b477a21bc
Adil Rabbani <adil_rabbani@ymail.com>
2018-06-16 04:05:13 +0500

    changed example

--- test/manual-test-examples/webgl/point/sketch.js
+++ test/manual-test-examples/webgl/point/sketch.js
@@ -1,16 +1,27 @@
-var randNum_1;
-var randNum_2;
+var randX;
+var randY;
+
+var randStroke_r;
+var randStroke_g;
+var randStroke_b;
+
+var randStrokeWeight;
 
 function setup() {
   createCanvas(800, 600, WEBGL);
   background(50);
-  stroke(255);
-  strokeWeight(1);
 }
+
 function draw() {
   for (var i = 0; i < 10; i++) {
-    randNum_1 = Math.floor(Math.random() * (-1366 - 1366 + 1)) + 1366;
-    randNum_2 = Math.floor(Math.random() * (-760 - 760 + 1)) + 760;
-    point(randNum_1, randNum_2);
+    randStroke_r = Math.floor(Math.random() * 255 + 1);
+    randStroke_g = Math.floor(Math.random() * 255 + 1);
+    randStroke_b = Math.floor(Math.random() * 255 + 1);
+    randStrokeWeight = Math.floor(Math.random() * 10 + 1);
+    stroke(randStroke_r, randStroke_g, randStroke_b);
+    strokeWeight(randStrokeWeight);
+    randX = Math.floor(Math.random() * (-800 - 800 + 1)) + 800;
+    randY = Math.floor(Math.random() * (-600 - 600 + 1)) + 600;
+    point(randX, randY);
   }
 }
