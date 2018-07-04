d245d5a943dd9a5c978439d138bae0d2bebaac3b
TanviKumar <tanvikumar777@gmail.com>
2018-07-04 13:10:56 +0530

    Add second example to loadModel

--- src/webgl/loading.js
+++ src/webgl/loading.js
@@ -57,6 +57,35 @@ require('./p5.Geometry');
  *
  * @alt
  * Vertically rotating 3-d teapot octahedron.
+ *
+ * @example
+ * <div>
+ * <code>
+ * //draw a spinning teapot
+ * var teapot;
+ *
+ * function preload() {
+ *   // Load model with normalise parameter set to true
+ *   teapot = loadModel('assets/teapot.obj', true);
+ * }
+ *
+ * function setup() {
+ *   createCanvas(100, 100, WEBGL);
+ * }
+ *
+ * function draw() {
+ *   background(200);
+ *   scale(0.4); // Scaled to make model fit into canvas
+ *   rotateX(frameCount * 0.01);
+ *   rotateY(frameCount * 0.01);
+ *   normalMaterial(); // For effect
+ *   model(teapot);
+ * }
+ * </code>
+ * </div>
+ *
+ * @alt
+ * Vertically rotating 3-d teapot with red, green and blue gradient.
  */
 /**
  * @method loadModel
