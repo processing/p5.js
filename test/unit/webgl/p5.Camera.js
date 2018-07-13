de4d82efd515ebe582e26c93e127b7d70e7ac6ca
AidanNelson <AidanNelson@users.noreply.github.com>
2018-07-13 12:34:24 -0400

    create delta variable and add tests

--- test/unit/webgl/p5.Camera.js
+++ test/unit/webgl/p5.Camera.js
@@ -1,6 +1,7 @@
 suite('p5.Camera', function() {
   var myp5;
   var myCam;
+  var delta = 0.001;
 
   if (!window.Modernizr.webgl) {
     return;
@@ -42,9 +43,9 @@ suite('p5.Camera', function() {
         myCam.pan(1);
         myCam.pan(-1);
       }
-      assert.closeTo(myCam.centerX, originalCenterXYZ.x, 0.001);
-      assert.closeTo(myCam.centerY, originalCenterXYZ.y, 0.001);
-      assert.closeTo(myCam.centerZ, originalCenterXYZ.z, 0.001);
+      assert.closeTo(myCam.centerX, originalCenterXYZ.x, delta);
+      assert.closeTo(myCam.centerY, originalCenterXYZ.y, delta);
+      assert.closeTo(myCam.centerZ, originalCenterXYZ.z, delta);
     });
 
     test('Tilt() back and forth should result in similar centerXYZ', function() {
@@ -57,31 +58,32 @@ suite('p5.Camera', function() {
         myCam.tilt(1);
         myCam.tilt(-1);
       }
-      assert.closeTo(myCam.centerX, originalCenterXYZ.x, 0.001);
-      assert.closeTo(myCam.centerY, originalCenterXYZ.y, 0.001);
-      assert.closeTo(myCam.centerZ, originalCenterXYZ.z, 0.001);
+
+      assert.closeTo(myCam.centerX, originalCenterXYZ.x, delta);
+      assert.closeTo(myCam.centerY, originalCenterXYZ.y, delta);
+      assert.closeTo(myCam.centerZ, originalCenterXYZ.z, delta);
     });
   });
 
   suite('Position / Orientation', function() {
     test('Camera() sets eye, center, and up XYZ properties', function() {
-      myCam.camera(100, 100, 100, 200, 200, 200, 0, 0, 1);
-      assert.equal(myCam.eyeX, 100);
-      assert.equal(myCam.eyeY, 100);
-      assert.equal(myCam.eyeZ, 100);
-      assert.equal(myCam.centerX, 200);
-      assert.equal(myCam.centerY, 200);
-      assert.equal(myCam.centerZ, 200);
-      assert.equal(myCam.upX, 0);
-      assert.equal(myCam.upY, 0);
-      assert.equal(myCam.upZ, 1);
+      myCam.camera(1, 1, 1, 2, 2, 2, 0, 0, 1);
+      assert.strictEqual(myCam.eyeX, 1);
+      assert.strictEqual(myCam.eyeY, 1);
+      assert.strictEqual(myCam.eyeZ, 1);
+      assert.strictEqual(myCam.centerX, 2);
+      assert.strictEqual(myCam.centerY, 2);
+      assert.strictEqual(myCam.centerZ, 2);
+      assert.strictEqual(myCam.upX, 0);
+      assert.strictEqual(myCam.upY, 0);
+      assert.strictEqual(myCam.upZ, 1);
     });
 
     test('LookAt() should set centerXYZ', function() {
       myCam.lookAt(23, 23, 23);
-      assert.equal(myCam.centerX, 23);
-      assert.equal(myCam.centerY, 23);
-      assert.equal(myCam.centerZ, 23);
+      assert.strictEqual(myCam.centerX, 23);
+      assert.strictEqual(myCam.centerY, 23);
+      assert.strictEqual(myCam.centerZ, 23);
     });
 
     test('Move() back and forth should result in similar eyeXYZ', function() {
@@ -100,9 +102,9 @@ suite('p5.Camera', function() {
         myCam.move(myVec.x, myVec.y, myVec.z);
         myCam.move(-1 * myVec.x, -1 * myVec.y, -1 * myVec.z);
       }
-      assert.closeTo(myCam.eyeX, originalEyeXYZ.x, 0.001);
-      assert.closeTo(myCam.eyeY, originalEyeXYZ.y, 0.001);
-      assert.closeTo(myCam.eyeZ, originalEyeXYZ.z, 0.001);
+      assert.closeTo(myCam.eyeX, originalEyeXYZ.x, delta);
+      assert.closeTo(myCam.eyeY, originalEyeXYZ.y, delta);
+      assert.closeTo(myCam.eyeZ, originalEyeXYZ.z, delta);
     });
 
     test('SetPosition() should set eyeXYZ', function() {
@@ -110,9 +112,9 @@ suite('p5.Camera', function() {
 
       myCam.setPosition(pos, pos, pos);
 
-      assert.equal(myCam.eyeX, pos);
-      assert.equal(myCam.eyeY, pos);
-      assert.equal(myCam.eyeZ, pos);
+      assert.strictEqual(myCam.eyeX, pos);
+      assert.strictEqual(myCam.eyeY, pos);
+      assert.strictEqual(myCam.eyeZ, pos);
     });
   });
 
@@ -121,7 +123,10 @@ suite('p5.Camera', function() {
       myCam.ortho(-10, 10, -10, 10, 0, 100);
 
       for (let i = 0; i < myCam.projMatrix.mat4.length; i++) {
-        assert.equal(myCam.projMatrix.mat4[i], myp5._renderer.uPMatrix.mat4[i]);
+        assert.strictEqual(
+          myCam.projMatrix.mat4[i],
+          myp5._renderer.uPMatrix.mat4[i]
+        );
       }
     });
 
@@ -129,7 +134,29 @@ suite('p5.Camera', function() {
       myCam.perspective(Math.PI / 3.0, 1, 1, 100);
 
       for (let i = 0; i < myCam.projMatrix.mat4.length; i++) {
-        assert.equal(myCam.projMatrix.mat4[i], myp5._renderer.uPMatrix.mat4[i]);
+        assert.strictEqual(
+          myCam.projMatrix.mat4[i],
+          myp5._renderer.uPMatrix.mat4[i]
+        );
+      }
+    });
+  });
+
+  suite('Helper Functions', function() {
+    // copy
+    test('copy() returns a new p5.Camera object', function() {
+      var newCam = myCam.copy();
+      assert.instanceOf(newCam, p5.Camera);
+      assert.notDeepEqual(newCam, myCam);
+    });
+
+    // _getLocalAxes
+    test('_getLocalAxes() returns three vectors', function() {
+      var local = myCam._getLocalAxes();
+      for (let j = 0; j < 3; j++) {
+        assert.typeOf(local.x[j], 'number');
+        assert.typeOf(local.y[j], 'number');
+        assert.typeOf(local.z[j], 'number');
       }
     });
   });
