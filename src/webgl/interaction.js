d988e788ee71b984c8f018794ff6ca19fd7f41e9
AidanNelson <AidanNelson@users.noreply.github.com>
2018-07-25 19:30:27 -0400

    add zoom behavior / refactor / update docs

--- src/webgl/interaction.js
+++ src/webgl/interaction.js
@@ -1,18 +1,26 @@
+/**
+ * @module Lights, Camera
+ * @submodule Interaction
+ */
+
 'use strict';
 
 var p5 = require('../core/core');
 
 /**
- * Allows rotation of a 3D sketch by dragging the mouse. As the mouse is dragged
- * away from the center of the canvas in the X or Y direction, the sketch is
- * rotated about the Y or X axis respectively. Note that this rotation only
- * affects objects drawn after orbitControl() has been called in the draw() loop.
- * To reverse movement in either axis, enter a negative number for sensitivity.
- * Calling this function without arguments is equivalent to calling orbitControl(1,1).
+ * Allows movement around a 3D sketch using a mouse or trackpad.  Left-clicking
+ * and dragging will rotate the camera position about the center of the sketch,
+ * right-clicking and dragging will pan the camera position without rotation,
+ * and using the mouse wheel (scrolling) will move the camera closer or further
+ * from the center of the sketch. This function can be called with parameters
+ * dictating sensitivity to mouse movement along the X and Y axes.  Calling
+ * this function without parameters is equivalent to calling orbitControl(1,1).
+ * To reverse direction of movement in either axis, enter a negative number
+ * for sensitivity.
  * @method orbitControl
  * @for p5
- * @param  {Number} [sensitivityX]        sensitivity to mouse movement along X axis
- * @param  {Number} [sensitivityY]        sensitivity to mouse movement along Y axis
+ * @param  {Number} [sensitivityX] sensitivity to mouse movement along X axis
+ * @param  {Number} [sensitivityY] sensitivity to mouse movement along Y axis
  * @chainable
  * @example
  * <div>
@@ -24,35 +32,42 @@ var p5 = require('../core/core');
  * function draw() {
  *   background(200);
  *   orbitControl();
- *   translate(-20, 15, -20);
- *   torus(20, 5);
- *   translate(15, 0, 15);
- *   torus(20, 5);
- *   translate(15, 0, 15);
- *   torus(20, 5);
- *   translate(15, 0, 15);
- *   torus(20, 5);
- *   translate(15, 0, 15);
- *   torus(20, 5);
- *   translate(15, 0, 15);
- *   torus(20, 5);
+ *   rotateY(0.5);
+ *   box(30, 50);
  * }
  * </code>
  * </div>
  *
  * @alt
- * Camera orbits around a series of floating shapes when mouse is
- * hold-clicked & then moved.
+ * Camera orbits around a box when mouse is hold-clicked & then moved.
  */
-//@TODO: implement full orbit controls including
-//pan, zoom, quaternion rotation, etc.
+
 // implementation based on three.js 'orbitControls':
 // https://github.com/mrdoob/three.js/blob/dev/examples/js/controls/OrbitControls.js
 p5.prototype.orbitControl = function(sensitivityX, sensitivityY) {
   this._assert3d('orbitControl');
   p5._validateParameters('orbitControl', arguments);
 
+  // If the mouse is not in bounds of the canvas, disable all behaviors:
+  var mouseInCanvas =
+    this.mouseX < this.width &&
+    this.mouseX > 0 &&
+    this.mouseY < this.height &&
+    this.mouseY > 0;
+  if (!mouseInCanvas) return;
+
   var cam = this._renderer._curCamera;
+
+  if (typeof sensitivityX === 'undefined') {
+    sensitivityX = 1;
+  }
+  if (typeof sensitivityY === 'undefined') {
+    sensitivityY = sensitivityX;
+  }
+
+  // default right-mouse and mouse-wheel behaviors (context menu and scrolling,
+  // respectively) are disabled here to allow use of those events for panning and
+  // zooming
 
   // disable context menu for canvas element and add 'contextMenuDisabled'
   // flag to p5 instance
@@ -63,61 +78,36 @@ p5.prototype.orbitControl = function(sensitivityX, sensitivityY) {
     this._setProperty('contextMenuDisabled', true);
   }
 
-  if (typeof sensitivityX === 'undefined') {
-    sensitivityX = 1;
+  // disable default scrolling behavior on the canvas element and add
+  // 'wheelDefaultDisabled' flag to p5 instance
+  if (this.wheelDefaultDisabled !== true) {
+    this.canvas.onwheel = function() {
+      return false;
+    };
+    this._setProperty('wheelDefaultDisabled', true);
   }
-  if (typeof sensitivityY === 'undefined') {
-    sensitivityY = sensitivityX;
+
+  var scaleFactor = this.height < this.width ? this.height : this.width;
+
+  // ZOOM if there is a change in mouseWheelDelta
+  if (this._mouseWheelDeltaY !== this._pmouseWheelDeltaY) {
+    // zoom according to direction of mouseWheelDeltaY rather than value
+    if (this._mouseWheelDeltaY > 0) {
+      this._renderer._curCamera._orbit(0, 0, 0.5 * scaleFactor);
+    } else {
+      this._renderer._curCamera._orbit(0, 0, -0.5 * scaleFactor);
+    }
   }
 
   if (this.mouseIsPressed) {
+    // ORBIT BEHAVIOR
     if (this.mouseButton === this.LEFT) {
-      var scaleFactor = this.height < this.width ? this.height : this.width;
       var deltaTheta =
         -sensitivityX * (this.mouseX - this.pmouseX) / scaleFactor;
       var deltaPhi = sensitivityY * (this.mouseY - this.pmouseY) / scaleFactor;
-
-      var diffX = cam.eyeX - cam.centerX;
-      var diffY = cam.eyeY - cam.centerY;
-      var diffZ = cam.eyeZ - cam.centerZ;
-
-      // get spherical coorinates for current camera position about origin
-      var camRadius = Math.sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ);
-      // from https://github.com/mrdoob/three.js/blob/dev/src/math/Spherical.js#L72-L73
-      var camTheta = Math.atan2(diffX, diffZ); // equatorial angle
-      var camPhi = Math.acos(Math.max(-1, Math.min(1, diffY / camRadius))); // polar angle
-
-      // add mouse movements
-      camTheta += deltaTheta;
-      camPhi += deltaPhi;
-
-      // prevent rotation over the zenith / under bottom
-      if (camPhi > Math.PI) {
-        camPhi = Math.PI;
-      } else if (camPhi <= 0) {
-        camPhi = 0.001;
-      }
-
-      // from https://github.com/mrdoob/three.js/blob/dev/src/math/Vector3.js#L628-L632
-      // var sinPhiRadius = Math.sin(camPhi) * camRadius;
-      var _x = Math.sin(camPhi) * camRadius * Math.sin(camTheta);
-      var _y = Math.cos(camPhi) * camRadius;
-      var _z = Math.sin(camPhi) * camRadius * Math.cos(camTheta);
-
-      this.camera(
-        _x + cam.centerX,
-        _y + cam.centerY,
-        _z + cam.centerZ,
-        cam.centerX,
-        cam.centerY,
-        cam.centerZ,
-        0,
-        1,
-        0
-      );
-    }
-    if (this.mouseButton === this.RIGHT) {
-      // panning behavior along X/Z camera axes and restricted to X/Z plane
+      this._renderer._curCamera._orbit(deltaTheta, deltaPhi, 0);
+    } else if (this.mouseButton === this.RIGHT) {
+      // PANNING BEHAVIOR along X/Z camera axes and restricted to X/Z plane
       // in world space
       var local = cam._getLocalAxes();
 
