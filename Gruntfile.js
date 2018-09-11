da9b77846f05c79145b105daeef5b958b6965a83
Spongman <piersh@hotmail.com>
2018-09-10 18:15:10 -0700

    use uglify-es

--- Gruntfile.js
+++ Gruntfile.js
@@ -461,7 +461,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-yuidoc');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-minjson');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-newer');
