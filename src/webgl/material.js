6ab47e38c937bdf887dab9247f9750ee411ce774
Mudigonda Sai Kaushik <m.saikaushik97@gmail.com>
2018-09-14 10:42:40 +0530

    lights defined on p5 prototype

--- src/webgl/material.js
+++ src/webgl/material.js
@@ -81,6 +81,21 @@ p5.prototype.loadShader = function(vertFilename, fragFilename) {
 };
 
 /**
+ * Sets the default ambient light, directional light, falloff, and
+ * specular values. The defaults are ambientLight(128, 128, 128) and
+ * directionalLight(128, 128, 128, 0, 0, -1), lightFalloff(1, 0, 0),
+ * and lightSpecular(0, 0, 0). Lights need to be included in the draw()
+ * to remain persistent in a looping program. Placing them in the setup()
+ * of a looping program will cause them to only have an effect the
+ * first time through the loop.
+ * @method lights
+ * @chainable
+ */
+p5.prototype.lights = function() {
+  this._renderer.lights();
+};
+
+/**
  * @method createShader
  * @param {String} vertSrc source code for the vertex shader
  * @param {String} fragSrc source code for the fragment shader
