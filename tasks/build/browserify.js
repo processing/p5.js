54e0de0b1058c652e4f8e2d14286628d3498f1cc
evelyn masso <outofambit@github.com>
2018-12-31 15:41:49 -0600

    rename variable

--- tasks/build/browserify.js
+++ tasks/build/browserify.js
@@ -8,13 +8,12 @@ module.exports = function(grunt) {
     param
   ) {
     const isMin = param === 'min';
-    const filename = isMin ? 'p5.min.js' : 'p5.js';
-
+    const outputFilename = isMin ? 'p5.min.js' : 'p5.js';
     const srcFilePath = path.join(__dirname, '../../src/app.js');
 
     const options = {
       outDir: './lib',
-      outFile: filename,
+      outFile: outputFilename,
       minify: isMin,
       global: 'p5',
       watch: false
