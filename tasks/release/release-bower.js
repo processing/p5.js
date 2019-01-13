9139572789419bc3ea992529093d999d4809f02c
Laksh Singla <lakshsingla@gmail.com>
2019-01-13 23:01:16 +0530

    Update release tasks to ES6

--- tasks/release/release-bower.js
+++ tasks/release/release-bower.js
@@ -9,21 +9,21 @@ module.exports = function(grunt) {
     'Publishes the new release of p5.js on Bower',
     function() {
       // Async Task
-      var done = this.async();
+      const done = this.async();
       // Keep the version handy
-      var version = require('../../package.json').version;
+      const version = require('../../package.json').version;
       // Keep the release-party ready
-      var releaseParty = grunt.config.get('bowerReleaser');
+      const releaseParty = grunt.config.get('bowerReleaser');
       // Avoiding Callback Hell and using Promises
-      new Promise(function(resolve, reject) {
+      new Promise((resolve, reject) => {
         // Clone the repo. NEEDS TO BE QUIET. Took 3 hours to realise this.
         // Otherwise the stdout screws up
         console.log('Cloning the Release repo ...');
         exec(
-          'rm -rf bower-repo/ && git clone -q https://github.com/' +
-            releaseParty +
-            '/p5.js-release.git bower-repo',
-          function(err, stdout, stderr) {
+          `rm -rf bower-repo/ && git clone -q \
+          https://github.com/${releaseParty}/p5.js-release.git \
+          bower-repo`,
+          (err, stdout, stderr) => {
             if (err) {
               reject(err);
             }
@@ -39,32 +39,29 @@ module.exports = function(grunt) {
           // NOTE : Uses 'cp' of UNIX. Make sure it is unaliased in your .bashrc,
           // otherwise it may prompt always for overwrite (not desirable)
           console.log('Copying new files ...');
-          return new Promise(function(resolve, reject) {
-            exec('cp -R lib/*.js lib/addons bower-repo/lib', function(
-              err,
-              stdout,
-              stderr
-            ) {
-              if (err) {
-                reject(err);
+          return new Promise((resolve, reject) => {
+            exec(
+              'cp -R lib/*.js lib/addons bower-repo/lib',
+              (err, stdout, stderr) => {
+                if (err) {
+                  reject(err);
+                }
+                if (stderr) {
+                  reject(stderr);
+                }
+                resolve();
               }
-              if (stderr) {
-                reject(stderr);
-              }
-              resolve();
-            });
+            );
           });
         })
-        .then(function(resolve, reject) {
+        .then((resolve, reject) => {
           // Git add, commit, push
           console.log('Pushing out changes ...');
           return new Promise(function(resolve, reject) {
             exec(
-              'git add --all && git commit -am "' +
-                version +
-                '" && git push -q',
+              `git add --all && git commit -am "${version}" && git push -q`,
               { cwd: './bower-repo' },
-              function(err, stdout, stderr) {
+              (err, stdout, stderr) => {
                 if (err) {
                   reject(err);
                 }
@@ -78,7 +75,7 @@ module.exports = function(grunt) {
             );
           });
         })
-        .catch(function(err) {
+        .catch(err => {
           console.log('Failed to Release on Bower!');
           throw new Error(err);
         });
