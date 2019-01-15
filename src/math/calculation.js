58a642b6ca45bd525f42e46de081528f6f704b7e
ThunbergOlle <olle.thunberg03@gmail.com>
2019-01-15 18:53:27 +0100

    Updated hypo calculation. (Calculation.js line.805)
    Easier way of calculating the hypotenuse.

--- src/math/calculation.js
+++ src/math/calculation.js
@@ -802,38 +802,13 @@ function hypot(x, y, z) {
   if (typeof Math.hypot === 'function') {
     return Math.hypot.apply(null, arguments);
   }
-
+  var a2 = Math.pow(x, 2);
+  var b2 = Math.pow(y, 2);
+  var c2 = a2 + b2;
+  var hypoten = Math.sqrt(c2);
   // Otherwise use the V8 implementation
   // https://github.com/v8/v8/blob/8cd3cf297287e581a49e487067f5cbd991b27123/src/js/math.js#L217
-  var length = arguments.length;
-  var args = [];
-  var max = 0;
-  for (var i = 0; i < length; i++) {
-    var n = arguments[i];
-    n = +n;
-    if (n === Infinity || n === -Infinity) {
-      return Infinity;
-    }
-    n = Math.abs(n);
-    if (n > max) {
-      max = n;
-    }
-    args[i] = n;
-  }
-
-  if (max === 0) {
-    max = 1;
-  }
-  var sum = 0;
-  var compensation = 0;
-  for (var j = 0; j < length; j++) {
-    var m = args[j] / max;
-    var summand = m * m - compensation;
-    var preliminary = sum + summand;
-    compensation = preliminary - sum - summand;
-    sum = preliminary;
-  }
-  return Math.sqrt(sum) * max;
+  return hypoten;
 }
 
 module.exports = p5;
