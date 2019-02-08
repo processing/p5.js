f04bb2000dc50a34ffd13f3b06514e0268d39dbb
Spongman <piersh@hotmail.com>
2019-02-08 15:22:42 -0800

    webgl loadPixels/_pixelsDirty

--- src/webgl/p5.RendererGL.js
+++ src/webgl/p5.RendererGL.js
@@ -451,6 +451,7 @@ p5.RendererGL.prototype.background = function() {
   this.GL.clearColor(_r, _g, _b, _a);
   this.GL.depthMask(true);
   this.GL.clear(this.GL.COLOR_BUFFER_BIT | this.GL.DEPTH_BUFFER_BIT);
+  this._pixelsState._pixelsDirty = true;
 };
 
 //////////////////////////////////////////////
@@ -591,14 +592,27 @@ p5.RendererGL.prototype.strokeWeight = function(w) {
 
 // x,y are canvas-relative (pre-scaled by _pixelDensity)
 p5.RendererGL.prototype._getPixel = function(x, y) {
-  var pixels = new Uint8Array(4);
-  // prettier-ignore
-  this.drawingContext.readPixels(
-    x, y, 1, 1,
-    this.drawingContext.RGBA, this.drawingContext.UNSIGNED_BYTE,
-    pixels
-  );
-  return [pixels[0], pixels[1], pixels[2], pixels[3]];
+  var pixelsState = this._pixelsState;
+  var imageData, index;
+  if (pixelsState._pixelsDirty) {
+    imageData = new Uint8Array(4);
+    // prettier-ignore
+    this.drawingContext.readPixels(
+      x, y, 1, 1,
+      this.drawingContext.RGBA, this.drawingContext.UNSIGNED_BYTE,
+      imageData
+    );
+    index = 0;
+  } else {
+    imageData = pixelsState.pixels;
+    index = (Math.floor(x) + Math.floor(y) * this.canvas.width) * 4;
+  }
+  return [
+    imageData[index + 0],
+    imageData[index + 1],
+    imageData[index + 2],
+    imageData[index + 3]
+  ];
 };
 
 /**
@@ -612,6 +626,10 @@ p5.RendererGL.prototype._getPixel = function(x, y) {
  */
 
 p5.RendererGL.prototype.loadPixels = function() {
+  var pixelsState = this._pixelsState;
+  if (!pixelsState._pixelsDirty) return;
+  pixelsState._pixelsDirty = false;
+
   //@todo_FES
   if (this._pInst._glAttributes.preserveDrawingBuffer !== true) {
     console.log(
@@ -619,30 +637,23 @@ p5.RendererGL.prototype.loadPixels = function() {
     );
     return;
   }
-  var pd = this._pInst._pixelDensity;
-  var x = 0;
-  var y = 0;
-  var w = this.width;
-  var h = this.height;
-  w *= pd;
-  h *= pd;
+
   //if there isn't a renderer-level temporary pixels buffer
   //make a new one
-  if (typeof this.pixels === 'undefined') {
-    this.pixels = new Uint8Array(
-      this.GL.drawingBufferWidth * this.GL.drawingBufferHeight * 4
-    );
+  var pixels = pixelsState.pixels;
+  var len = this.GL.drawingBufferWidth * this.GL.drawingBufferHeight * 4;
+  if (!(pixels instanceof Uint8Array) || pixels.length !== len) {
+    pixels = new Uint8Array(len);
+    this._pixelsState._setProperty('pixels', pixels);
   }
+
+  var pd = this._pInst._pixelDensity;
+  // prettier-ignore
   this.GL.readPixels(
-    x,
-    y,
-    w,
-    h,
-    this.GL.RGBA,
-    this.GL.UNSIGNED_BYTE,
-    this.pixels
+    0, 0, this.width * pd, this.height * pd,
+    this.GL.RGBA, this.GL.UNSIGNED_BYTE,
+    pixels
   );
-  this._pixelsState._setProperty('pixels', this.pixels);
 };
 
 //////////////////////////////////////////////
@@ -672,9 +683,14 @@ p5.RendererGL.prototype.resize = function(w, h) {
   this._curCamera._resize();
 
   //resize pixels buffer
-  if (typeof this.pixels !== 'undefined') {
-    this.pixels = new Uint8Array(
-      this.GL.drawingBufferWidth * this.GL.drawingBufferHeight * 4
+  var pixelsState = this._pixelsState;
+  pixelsState._pixelsDirty = true;
+  if (typeof pixelsState.pixels !== 'undefined') {
+    pixelsState._setProperty(
+      'pixels',
+      new Uint8Array(
+        this.GL.drawingBufferWidth * this.GL.drawingBufferHeight * 4
+      )
     );
   }
 };
@@ -695,6 +711,7 @@ p5.RendererGL.prototype.clear = function() {
   var _a = arguments[3] || 0;
   this.GL.clearColor(_r, _g, _b, _a);
   this.GL.clear(this.GL.COLOR_BUFFER_BIT | this.GL.DEPTH_BUFFER_BIT);
+  this._pixelsState._pixelsDirty = true;
 };
 
 p5.RendererGL.prototype.applyMatrix = function(a, b, c, d, e, f) {
