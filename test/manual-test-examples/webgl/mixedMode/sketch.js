e87b9dca5ffcb5529fea16cbf9900eae981c642a
Eliot Ostling <eostling@cnm.edu>
2019-03-07 17:16:30 -0700

    Removed args to normal material

--- test/manual-test-examples/webgl/mixedMode/sketch.js
+++ test/manual-test-examples/webgl/mixedMode/sketch.js
@@ -1,3 +1,7 @@
+
+//To do: fix the call to normalMaterial in the mixedMode manual test to follow the current proper usage (hint: no arguments)
+
+
 var theta = 0;
 
 function setup() {
@@ -13,8 +17,7 @@ function draw() {
   for (var i = 0; i < 500; i += 100) {
     push();
     translate(0, 0, i);
-    normalMaterial(i * 0.1, 100, 100);
-
+    normalMaterial();
     push();
     translate(0, cos(i + frameCount * 0.1) * 10, 0);
     box(20, 20, 20);
