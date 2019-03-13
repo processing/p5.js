f0b89e50c3997b9c1c30a07e145246b93e31c7a4
plxity <purutaneja.com@gmail.com>
2019-03-14 00:09:53 +0530

    changed require to import

--- src/app.js
+++ src/app.js
@@ -1,88 +1,88 @@
 'use strict';
 
 // core
-var p5 = require('./core/main');
-require('./core/constants');
-require('./core/environment');
-require('./core/error_helpers');
-require('./core/helpers');
-require('./core/legacy');
-require('./core/p5.Element');
-require('./core/p5.Graphics');
-require('./core/p5.Renderer');
-require('./core/p5.Renderer2D');
-require('./core/rendering');
-require('./core/shim');
-require('./core/structure');
-require('./core/transform');
-require('./core/shape/2d_primitives');
-require('./core/shape/attributes');
-require('./core/shape/curves');
-require('./core/shape/vertex');
+import p5 from './core/main';
+import './core/constants';
+import './core/environment';
+import './core/error_helpers';
+import './core/helpers';
+import './core/legacy';
+import './core/p5.Element';
+import './core/p5.Graphics';
+import './core/p5.Renderer';
+import './core/p5.Renderer2D';
+import './core/rendering';
+import './core/shim';
+import './core/structure';
+import './core/transform';
+import './core/shape/2d_primitives';
+import './core/shape/attributes';
+import './core/shape/curves';
+import './core/shape/vertex';
 
 // color
-require('./color/color_conversion');
-require('./color/creating_reading');
-require('./color/p5.Color');
-require('./color/setting');
+import './color/color_conversion';
+import './color/creating_reading';
+import './color/p5.Color';
+import './color/setting';
 
 // data
-require('./data/p5.TypedDict');
+import './data/p5.TypedDict';
 
 // events
-require('./events/acceleration');
-require('./events/keyboard');
-require('./events/mouse');
-require('./events/touch');
+import './events/acceleration';
+import './events/keyboard';
+import './events/mouse';
+import './events/touch';
 
 // image
-require('./image/filters');
-require('./image/image');
-require('./image/loading_displaying');
-require('./image/p5.Image');
-require('./image/pixels');
+import './image/filters';
+import './image/image';
+import './image/loading_displaying';
+import './image/p5.Image';
+import './image/pixels';
 
 // io
-require('./io/files');
-require('./io/p5.Table');
-require('./io/p5.TableRow');
-require('./io/p5.XML');
+import './io/files';
+import './io/p5.Table';
+import './io/p5.TableRow';
+import './io/p5.XML';
 
 // math
-require('./math/calculation');
-require('./math/math');
-require('./math/noise');
-require('./math/p5.Vector');
-require('./math/random');
-require('./math/trigonometry');
+import './math/calculation';
+import './math/math';
+import './math/noise';
+import './math/p5.Vector';
+import './math/random';
+import './math/trigonometry';
 
 // typography
-require('./typography/attributes');
-require('./typography/loading_displaying');
-require('./typography/p5.Font');
+import './typography/attributes';
+import './typography/loading_displaying';
+import './typography/p5.Font';
 
 // utilities
-require('./utilities/array_functions');
-require('./utilities/conversion');
-require('./utilities/string_functions');
-require('./utilities/time_date');
+import './utilities/array_functions';
+import './utilities/conversion';
+import './utilities/string_functions';
+import './utilities/time_date';
 
 // webgl
-require('./webgl/3d_primitives');
-require('./webgl/interaction');
-require('./webgl/light');
-require('./webgl/loading');
-require('./webgl/material');
-require('./webgl/p5.Camera');
-require('./webgl/p5.Geometry');
-require('./webgl/p5.Matrix');
-require('./webgl/p5.RendererGL.Immediate');
-require('./webgl/p5.RendererGL');
-require('./webgl/p5.RendererGL.Retained');
-require('./webgl/p5.Shader');
-require('./webgl/p5.Texture');
-require('./webgl/text');
+import './webgl/3d_primitives';
+import './webgl/interaction';
+import './webgl/light';
+import './webgl/loading';
+import './webgl/material';
+import './webgl/p5.Camera';
+import './webgl/p5.Geometry';
+import './webgl/p5.Matrix';
+import './webgl/p5.RendererGL.Immediate';
+import './webgl/p5.RendererGL';
+import './webgl/p5.RendererGL.Retained';
+import './webgl/p5.Shader';
+import './webgl/p5.Texture';
+import './webgl/text';
 
-require('./core/init');
+import './core/init';
 
-module.exports = p5;
+export default p5;
