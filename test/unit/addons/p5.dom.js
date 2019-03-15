7203401a58212083ccdf4d35258a6feed725e2c8
Oleksii_Bulba <Oleksii_Bulba@epam.com>
2019-03-15 14:00:49 +0300

    Added tests for methods addClass, removeClass, hasClass and toggleClass

--- test/unit/addons/p5.dom.js
+++ test/unit/addons/p5.dom.js
@@ -64,6 +64,66 @@ suite('DOM', function() {
       const elem = myp5.createDiv();
       assert.strictEqual(elem.elt.innerHTML, '');
     });
+
+    test('should add class to element', function() {
+      elt = document.createElement('div');
+      elt.setAttribute('id', 'testdiv');
+      document.body.appendChild(elt);
+
+      myp5.select('#testdiv').addClass('testclass');
+      assert.strictEqual(elt.getAttribute('class'), 'testclass');
+    });
+
+    test('should remove class from element with only one class', function() {
+      elt = document.createElement('div');
+      elt.setAttribute('id', 'testdiv');
+      elt.setAttribute('class', 'testclass');
+      document.body.appendChild(elt);
+
+      myp5.select('#testdiv').removeClass('testclass');
+      assert.strictEqual(elt.getAttribute('class'), '');
+    });
+
+    test('should remove class from element with several classes', function() {
+      elt = document.createElement('div');
+      elt.setAttribute('id', 'testdiv');
+      elt.setAttribute('class', 'testclass1 testclass2 testclass3');
+      document.body.appendChild(elt);
+
+      myp5.select('#testdiv').removeClass('testclass2');
+      assert.strictEqual(elt.getAttribute('class'), 'testclass1 testclass3');
+    });
+
+    test('should return true if element has specified class', function() {
+      elt = document.createElement('div');
+      elt.setAttribute('id', 'testdiv');
+      elt.setAttribute('class', 'testclass1 testclass2 testclass3');
+      document.body.appendChild(elt);
+
+      assert.strictEqual(myp5.select('#testdiv').hasClass('testclass2'), true);
+    });
+
+    test('should return false if element has not specified class', function() {
+      elt = document.createElement('div');
+      elt.setAttribute('id', 'testdiv');
+      elt.setAttribute('class', 'testclass1 testclass3');
+      document.body.appendChild(elt);
+
+      assert.strictEqual(myp5.select('#testdiv').hasClass('testclass2'), false);
+    });
+
+    test('should toggle specified class on element', function() {
+      elt = document.createElement('div');
+      elt.setAttribute('id', 'testdiv');
+      elt.setAttribute('class', 'testclass1 testclass2');
+      document.body.appendChild(elt);
+
+      myp5.select('#testdiv').toggleClass('testclass2');
+      assert.strictEqual(elt.getAttribute('class'), 'testclass1');
+
+      myp5.select('#testdiv').toggleClass('testclass2');
+      assert.strictEqual(elt.getAttribute('class'), 'testclass1 testclass2');
+    });
   });
 
   suite('p5.prototype.createButton', function() {
