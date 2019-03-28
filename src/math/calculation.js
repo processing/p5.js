d8a9b02f78ddb1d609e94e79c04f6f4bbef93cfe
Ajayneethikannan <ajaykarate001@gmail.com>
2019-03-28 07:52:35 +0530

    Improve norm function reference

--- src/math/calculation.js
+++ src/math/calculation.js
@@ -579,8 +579,7 @@ p5.prototype.min = function() {
  * Normalizes a number from another range into a value between 0 and 1.
  * Identical to map(value, low, high, 0, 1).
  * Numbers outside of the range are not clamped to 0 and 1, because
- * out-of-range values are often intentional and useful. (See the second
- * example above.)
+ * out-of-range values are often intentional and useful. (See the example above.)
  *
  * @method norm
  * @param  {Number} value incoming value to be normalized
@@ -596,6 +595,7 @@ p5.prototype.min = function() {
  *   let upperBound = width; //100;
  *   let normalized = norm(currentNum, lowerBound, upperBound);
  *   let lineY = 70;
+ *   stroke(3);
  *   line(0, lineY, width, lineY);
  *   //Draw an ellipse mapped to the non-normalized value.
  *   noStroke();
