c3fd5d9db9cf3202680d2e23ed6e9bf0f9c48813
Stalgia Grigg <stalgia.grigg@gmail.com>
2019-05-06 10:55:02 -0700

    modify comments for p5.Image properties

--- src/image/p5.Image.js
+++ src/image/p5.Image.js
@@ -147,9 +147,9 @@ p5.Image = function(width, height) {
   this.drawingContext = this.canvas.getContext('2d');
   this._pixelsState = this;
   this._pixelDensity = 1;
-  //used for webgl texturing only
-  this._modified = false;
   this._pixelsDirty = true;
+  //used for webgl texturing only determines whether to reupload texture to GPU
+  this._modified = false;
   /**
    * Array containing the values for all the pixels in the display window.
    * These values are numbers. This array is the size (include an appropriate
