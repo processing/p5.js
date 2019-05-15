d46fb4f53105bdaa5ed6feee3b301ab3f4f8830c
berfinA <ataman.berfin@gmail.com>
2019-05-14 17:53:24 -0700

    Update 2d_primitives.js

--- src/core/shape/2d_primitives.js
+++ src/core/shape/2d_primitives.js
@@ -106,7 +106,8 @@ p5.prototype._normalizeArcAngles = function(
  * origin may be changed with the <a href="#/p5/ellipseMode">ellipseMode()</a> function.<br><br>
  * The arc is always drawn clockwise from wherever start falls to wherever stop falls on the ellipse.
  * Adding or subtracting TWO_PI to either angle does not change where they fall.
- * If both start and stop fall at the same place, a full ellipse will be drawn.
+ * If both start and stop fall at the same place, a full ellipse will be drawn.Be ware that the the
+ * y-axis increases in the downward direction therefore the values of PI is counter clockwise.
  *
  * @method arc
  * @param  {Number} x      x-coordinate of the arc's ellipse
@@ -167,6 +168,7 @@ p5.prototype._normalizeArcAngles = function(
  */
 p5.prototype.arc = function(x, y, w, h, start, stop, mode, detail) {
   p5._validateParameters('arc', arguments);
+
 
   // if the current stroke and fill settings wouldn't result in something
   // visible, exit immediately
