785a8f2b155df8848b10717c560bbcca4e3d6683
Sanket Singh <sanketsingh24@gmail.com>
2019-06-19 11:23:06 +0530

    Modify docs example

--- src/webgl/light.js
+++ src/webgl/light.js
@@ -337,11 +337,16 @@ p5.prototype.lights = function() {
  * }
  * function draw() {
  *   background(0);
- *   lightFalloff(0.5, 0.01, 0);
  *   let locX = mouseX - width / 2;
  *   let locY = mouseY - height / 2;
+ *   translate(-25, 0, 0);
+ *   lightFalloff(1, 0, 0);
  *   pointLight(250, 250, 250, locX, locY, 50);
- *   sphere(40);
+ *   sphere(20);
+ *   translate(50, 0, 0);
+ *   lightFalloff(0.9, 0.01, 0);
+ *   pointLight(250, 250, 250, locX, locY, 50);
+ *   sphere(20);
  * }
  * </code>
  * </div>
