0614e6c929753bb4c8d923f1982de28da1769fce
Sanket Singh <24548786+sanketsingh24@users.noreply.github.com>
2019-06-20 00:04:28 +0530

    Loading for STL file 3D Models(#3675)
    * Added parser for ASCII STL file
    
    * added stl files
    
    * Completed Binary parser for STL
    
    * Added Documentation
    
    * Added Some Comments
    
    * added unit tests and organised files
    
    * Organised files
    
    * removed typo
    
    * Added an error for invalid filetype in loadModel
    
    * Combined unit tests for loadSTL
    
    * Added check for TextDecoder in loadModel
    
    * Added polyfill
    
    * Change error to warning

--- src/webgl/loading.js
+++ src/webgl/loading.js
@@ -12,14 +12,17 @@ var p5 = require('../core/main');
 require('./p5.Geometry');
 
 /**
- * Load a 3d model from an OBJ file.
+ * Load a 3d model from an OBJ or STL file.
  * <br><br>
- * One of the limitations of the OBJ format is that it doesn't have a built-in
+ * One of the limitations of the OBJ and STL format is that it doesn't have a built-in
  * sense of scale. This means that models exported from different programs might
  * be very different sizes. If your model isn't displaying, try calling
  * <a href="#/p5/loadModel">loadModel()</a> with the normalized parameter set to true. This will resize the
  * model to a scale appropriate for p5. You can also make additional changes to
  * the final size of your model with the <a href="#/p5/scale">scale()</a> function.
+ *
+ * Also, the support for colored STL files is not present. STL files with color will be
+ * rendered without color properties.
  *
  * @method loadModel
  * @param  {String} path              Path of the model to be loaded
@@ -109,26 +112,57 @@ p5.prototype.loadModel = function(path) {
     failureCallback = arguments[2];
   }
 
+  var fileType = path.slice(-4);
   var model = new p5.Geometry();
   model.gid = path + '|' + normalize;
   var self = this;
-  this.loadStrings(
-    path,
-    function(strings) {
-      parseObj(model, strings);
 
-      if (normalize) {
-        model.normalize();
-      }
+  if (fileType === '.stl') {
+    this.httpDo(
+      path,
+      'GET',
+      'arrayBuffer',
+      function(arrayBuffer) {
+        parseSTL(model, arrayBuffer);
 
-      self._decrementPreload();
-      if (typeof successCallback === 'function') {
-        successCallback(model);
-      }
-    }.bind(this),
-    failureCallback
-  );
+        if (normalize) {
+          model.normalize();
+        }
+        self._decrementPreload();
+        if (typeof successCallback === 'function') {
+          successCallback(model);
+        }
+      }.bind(this),
+      failureCallback
+    );
+  } else if (fileType === '.obj') {
+    this.loadStrings(
+      path,
+      function(strings) {
+        parseObj(model, strings);
 
+        if (normalize) {
+          model.normalize();
+        }
+
+        self._decrementPreload();
+        if (typeof successCallback === 'function') {
+          successCallback(model);
+        }
+      }.bind(this),
+      failureCallback
+    );
+  } else {
+    p5._friendlyFileLoadError(3, path);
+
+    if (failureCallback) {
+      failureCallback();
+    } else {
+      console.error(
+        'Sorry, the file type is invalid. Only OBJ and STL files are supported.'
+      );
+    }
+  }
   return model;
 };
 
@@ -238,6 +272,308 @@ function parseObj(model, lines) {
     model.computeNormals();
   }
 
+  return model;
+}
+
+/**
+ * STL files can be of two types, ASCII and Binary,
+ *
+ * We need to convert the arrayBuffer to an array of strings,
+ * to parse it as an ASCII file.
+ */
+function parseSTL(model, buffer) {
+  if (isBinary(buffer)) {
+    parseBinarySTL(model, buffer);
+  } else {
+    var reader = new DataView(buffer);
+
+    if (!('TextDecoder' in window)) {
+      console.warn(
+        'Sorry, ASCII STL loading only works in browsers that support TextDecoder (https://caniuse.com/#feat=textencoder)'
+      );
+      return model;
+    }
+
+    var decoder = new TextDecoder('utf-8');
+    var lines = decoder.decode(reader);
+    var lineArray = lines.split('\n');
+    parseASCIISTL(model, lineArray);
+  }
+  return model;
+}
+
+/**
+ * This function checks if the file is in ASCII format or in Binary format
+ *
+ * It is done by searching keyword `solid` at the start of the file.
+ *
+ * An ASCII STL data must begin with `solid` as the first six bytes.
+ * However, ASCII STLs lacking the SPACE after the `d` are known to be
+ * plentiful. So, check the first 5 bytes for `solid`.
+ *
+ * Several encodings, such as UTF-8, precede the text with up to 5 bytes:
+ * https://en.wikipedia.org/wiki/Byte_order_mark#Byte_order_marks_by_encoding
+ * Search for `solid` to start anywhere after those prefixes.
+ */
+function isBinary(data) {
+  var reader = new DataView(data);
+
+  // US-ASCII ordinal values for `s`, `o`, `l`, `i`, `d`
+  var solid = [115, 111, 108, 105, 100];
+  for (var off = 0; off < 5; off++) {
+    // If "solid" text is matched to the current offset, declare it to be an ASCII STL.
+    if (matchDataViewAt(solid, reader, off)) return false;
+  }
+
+  // Couldn't find "solid" text at the beginning; it is binary STL.
+  return true;
+}
+
+/**
+ * This function matches the `query` at the provided `offset`
+ */
+function matchDataViewAt(query, reader, offset) {
+  // Check if each byte in query matches the corresponding byte from the current offset
+  for (var i = 0, il = query.length; i < il; i++) {
+    if (query[i] !== reader.getUint8(offset + i, false)) return false;
+  }
+
+  return true;
+}
+
+/**
+ * This function parses the Binary STL files.
+ * https://en.wikipedia.org/wiki/STL_%28file_format%29#Binary_STL
+ *
+ * Currently there is no support for the colors provided in STL files.
+ */
+function parseBinarySTL(model, buffer) {
+  var reader = new DataView(buffer);
+
+  // Number of faces is present following the header
+  var faces = reader.getUint32(80, true);
+  var r,
+    g,
+    b,
+    hasColors = false,
+    colors;
+  var defaultR, defaultG, defaultB;
+
+  // Binary files contain 80-byte header, which is generally ignored.
+  for (var index = 0; index < 80 - 10; index++) {
+    // Check for `COLOR=`
+    if (
+      reader.getUint32(index, false) === 0x434f4c4f /*COLO*/ &&
+      reader.getUint8(index + 4) === 0x52 /*'R'*/ &&
+      reader.getUint8(index + 5) === 0x3d /*'='*/
+    ) {
+      hasColors = true;
+      colors = [];
+
+      defaultR = reader.getUint8(index + 6) / 255;
+      defaultG = reader.getUint8(index + 7) / 255;
+      defaultB = reader.getUint8(index + 8) / 255;
+      // To be used when color support is added
+      // alpha = reader.getUint8(index + 9) / 255;
+    }
+  }
+  var dataOffset = 84;
+  var faceLength = 12 * 4 + 2;
+
+  // Iterate the faces
+  for (var face = 0; face < faces; face++) {
+    var start = dataOffset + face * faceLength;
+    var normalX = reader.getFloat32(start, true);
+    var normalY = reader.getFloat32(start + 4, true);
+    var normalZ = reader.getFloat32(start + 8, true);
+
+    if (hasColors) {
+      var packedColor = reader.getUint16(start + 48, true);
+
+      if ((packedColor & 0x8000) === 0) {
+        // facet has its own unique color
+        r = (packedColor & 0x1f) / 31;
+        g = ((packedColor >> 5) & 0x1f) / 31;
+        b = ((packedColor >> 10) & 0x1f) / 31;
+      } else {
+        r = defaultR;
+        g = defaultG;
+        b = defaultB;
+      }
+    }
+
+    for (var i = 1; i <= 3; i++) {
+      var vertexstart = start + i * 12;
+
+      var newVertex = new p5.Vector(
+        reader.getFloat32(vertexstart, true),
+        reader.getFloat32(vertexstart + 8, true),
+        reader.getFloat32(vertexstart + 4, true)
+      );
+
+      model.vertices.push(newVertex);
+
+      if (hasColors) {
+        colors.push(r, g, b);
+      }
+    }
+
+    var newNormal = new p5.Vector(normalX, normalY, normalZ);
+
+    model.vertexNormals.push(newNormal, newNormal, newNormal);
+
+    model.faces.push([3 * face, 3 * face + 1, 3 * face + 2]);
+  }
+  if (hasColors) {
+    // add support for colors here.
+  }
+  return model;
+}
+
+/**
+ * ASCII STL file starts with `solid 'nameOfFile'`
+ * Then contain the normal of the face, starting with `facet normal`
+ * Next contain a keyword indicating the start of face vertex, `outer loop`
+ * Next comes the three vertex, starting with `vertex x y z`
+ * Vertices ends with `endloop`
+ * Face ends with `endfacet`
+ * Next face starts with `facet normal`
+ * The end of the file is indicated by `endsolid`
+ */
+function parseASCIISTL(model, lines) {
+  var state = '';
+  var curVertexIndex = [];
+  var newNormal, newVertex;
+
+  for (var iterator = 0; iterator < lines.length; ++iterator) {
+    var line = lines[iterator].trim();
+    var parts = line.split(' ');
+
+    for (var partsiterator = 0; partsiterator < parts.length; ++partsiterator) {
+      if (parts[partsiterator] === '') {
+        // Ignoring multiple whitespaces
+        parts.splice(partsiterator, 1);
+      }
+    }
+
+    if (parts.length === 0) {
+      // Remove newline
+      continue;
+    }
+
+    switch (state) {
+      case '': // First run
+        if (parts[0] !== 'solid') {
+          // Invalid state
+          console.error(line);
+          console.error('Invalid state "' + parts[0] + '", should be "solid"');
+          return;
+        } else {
+          state = 'solid';
+        }
+        break;
+
+      case 'solid': // First face
+        if (parts[0] !== 'facet' || parts[1] !== 'normal') {
+          // Invalid state
+          console.error(line);
+          console.error(
+            'Invalid state "' + parts[0] + '", should be "facet normal"'
+          );
+          return;
+        } else {
+          // Push normal for first face
+          newNormal = new p5.Vector(
+            parseFloat(parts[2]),
+            parseFloat(parts[3]),
+            parseFloat(parts[4])
+          );
+          model.vertexNormals.push(newNormal, newNormal, newNormal);
+          state = 'facet normal';
+        }
+        break;
+
+      case 'facet normal': // After normal is defined
+        if (parts[0] !== 'outer' || parts[1] !== 'loop') {
+          // Invalid State
+          console.error(line);
+          console.error(
+            'Invalid state "' + parts[0] + '", should be "outer loop"'
+          );
+          return;
+        } else {
+          // Next should be vertices
+          state = 'vertex';
+        }
+        break;
+
+      case 'vertex':
+        if (parts[0] === 'vertex') {
+          //Vertex of triangle
+          newVertex = new p5.Vector(
+            parseFloat(parts[1]),
+            parseFloat(parts[2]),
+            parseFloat(parts[3])
+          );
+          model.vertices.push(newVertex);
+          curVertexIndex.push(model.vertices.indexOf(newVertex));
+        } else if (parts[0] === 'endloop') {
+          // End of vertices
+          model.faces.push(curVertexIndex);
+          curVertexIndex = [];
+          state = 'endloop';
+        } else {
+          // Invalid State
+          console.error(line);
+          console.error(
+            'Invalid state "' + parts[0] + '", should be "vertex" or "endloop"'
+          );
+          return;
+        }
+        break;
+
+      case 'endloop':
+        if (parts[0] !== 'endfacet') {
+          // End of face
+          console.error(line);
+          console.error(
+            'Invalid state "' + parts[0] + '", should be "endfacet"'
+          );
+          return;
+        } else {
+          state = 'endfacet';
+        }
+        break;
+
+      case 'endfacet':
+        if (parts[0] === 'endsolid') {
+          // End of solid
+        } else if (parts[0] === 'facet' && parts[1] === 'normal') {
+          // Next face
+          newNormal = new p5.Vector(
+            parseFloat(parts[2]),
+            parseFloat(parts[3]),
+            parseFloat(parts[4])
+          );
+          model.vertexNormals.push(newNormal, newNormal, newNormal);
+          state = 'facet normal';
+        } else {
+          // Invalid State
+          console.error(line);
+          console.error(
+            'Invalid state "' +
+              parts[0] +
+              '", should be "endsolid" or "facet normal"'
+          );
+          return;
+        }
+        break;
+
+      default:
+        console.error('Invalid state "' + state + '"');
+        break;
+    }
+  }
   return model;
 }
 
