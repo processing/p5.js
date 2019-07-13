b76424955b875b9e61118057a76d3c90ab8f6d75
Abhinav Sagar <40603139+abhinavsagar@users.noreply.github.com>
2019-07-13 17:08:14 +0530

    Update sample-linter.js

--- utils/sample-linter.js
+++ utils/sample-linter.js
@@ -1,13 +1,13 @@
 'use strict';
-const eslint = require('eslint');
+import eslint from 'eslint';
 
 // envs: ['eslint-samples/p5'],
 
-var itemtypes = ['method', 'property'];
-var classes = ['p5', 'p5.dom'];
+const itemtypes = ['method', 'property'];
+const classes = ['p5', 'p5.dom'];
 
-var dataDoc = require('../docs/reference/data.min.json');
-var globals = {};
+import dataDoc from '../docs/reference/data.min.json';
+const globals = {};
 dataDoc.classitems
   .filter(
     ci => classes.indexOf(ci.class) >= 0 && itemtypes.indexOf(ci.itemtype) >= 0
@@ -29,19 +29,19 @@ dataDoc.classitems
   });
 
 function splitLines(text) {
-  var lines = [];
+  const lines = [];
 
   lines.lineFromIndex = function(index) {
-    var lines = this;
-    var lineCount = lines.length;
-    for (var i = 0; i < lineCount; i++) {
+    const lines = this;
+    const lineCount = lines.length;
+    for (let i = 0; i < lineCount; i++) {
       if (index < lines[i].index) return i - 1;
     }
     return lineCount - 1;
   };
 
-  var m;
-  var reSplit = /(( *\* ?)?.*)(?:\r\n|\r|\n)/g;
+  let m;
+  const reSplit = /(( *\* ?)?.*)(?:\r\n|\r|\n)/g;
   while ((m = reSplit.exec(text)) != null) {
     if (m.index === reSplit.lastIndex) {
       reSplit.lastIndex++;
@@ -57,9 +57,9 @@ function splitLines(text) {
   return lines;
 }
 
-var EOL = require('os').EOL;
+const EOL = require('os').EOL;
 
-var userFunctions = [
+const userFunctions = [
   'draw',
   'setup',
   'preload',
@@ -81,7 +81,7 @@ var userFunctions = [
   'keyReleased',
   'keyTyped'
 ];
-var userFunctionTrailer =
+const userFunctionTrailer =
   EOL + userFunctions.map(s => 'typeof ' + s + ';').join(EOL) + EOL;
 
 module.exports = {
@@ -96,27 +96,27 @@ module.exports = {
       preprocess: function(text) {
         this.lines = splitLines(text);
 
-        var m;
-        var comments = [];
+        let m;
+        const comments = [];
 
-        var reComment = /\/\*\*(?:.|\r|\n)*?\*\//g;
+        const reComment = /\/\*\*(?:.|\r|\n)*?\*\//g;
         while ((m = reComment.exec(text)) != null) {
-          var value = m[0];
+          const value = m[0];
           comments.push({
             value: value,
             range: [m.index, m.index + value.length]
           });
         }
 
-        var samples = (this.samples = []);
+        const samples = (this.samples = []);
 
-        for (var i = 0; i < comments.length; i++) {
-          var comment = comments[i];
-          var commentText = comment.value;
+        for (let i = 0; i < comments.length; i++) {
+          const comment = comments[i];
+          const commentText = comment.value;
 
-          var re = /(<code[^>]*>\s*(?:\r\n|\r|\n))((?:.|\r|\n)*?)<\/code>/gm;
+          const re = /(<code[^>]*>\s*(?:\r\n|\r|\n))((?:.|\r|\n)*?)<\/code>/gm;
           while ((m = re.exec(commentText)) != null) {
-            var code = m[2];
+            const code = m[2];
             if (!code) continue;
             code = code.replace(/^ *\* ?/gm, '');
 
@@ -132,50 +132,50 @@ module.exports = {
       },
 
       postprocess: function(sampleMessages, filename) {
-        var problems = [];
+        const problems = [];
 
-        for (var i = 0; i < sampleMessages.length; i++) {
-          var messages = sampleMessages[i];
-          var sample = this.samples[i];
+        for (let i = 0; i < sampleMessages.length; i++) {
+          const messages = sampleMessages[i];
+          const sample = this.samples[i];
           if (!messages.length) continue;
 
           var sampleLines;
 
-          var sampleIndex = sample.comment.range[0] + sample.index;
-          var sampleLine = this.lines.lineFromIndex(sampleIndex);
+          const sampleIndex = sample.comment.range[0] + sample.index;
+          const sampleLine = this.lines.lineFromIndex(sampleIndex);
 
-          for (var j = 0; j < messages.length; j++) {
-            var msg = messages[j];
+          for (let j = 0; j < messages.length; j++) {
+            const msg = messages[j];
 
-            var fix = msg.fix;
+            const fix = msg.fix;
             if (fix) {
               if (!sampleLines) {
                 sampleLines = splitLines(sample.code);
               }
 
-              var fixLine1 = sampleLines.lineFromIndex(fix.range[0]);
-              var fixLine2 = sampleLines.lineFromIndex(fix.range[1] - 1);
+              const fixLine1 = sampleLines.lineFromIndex(fix.range[0]);
+              const fixLine2 = sampleLines.lineFromIndex(fix.range[1] - 1);
               if (fixLine1 !== fixLine2) {
                 // TODO: handle multi-line fixes
                 fix.range = [0, 0];
                 fix.text = '';
               } else {
-                var line = this.lines[sampleLine + fixLine1];
+                const line = this.lines[sampleLine + fixLine1];
 
-                var fixColumn1 = fix.range[0] - sampleLines[fixLine1].index;
-                var fixColumn2 = fix.range[1] - sampleLines[fixLine1].index;
+                const fixColumn1 = fix.range[0] - sampleLines[fixLine1].index;
+                const fixColumn2 = fix.range[1] - sampleLines[fixLine1].index;
 
                 fix.range[0] = line.index + line.prefixLength + fixColumn1;
                 fix.range[1] = line.index + line.prefixLength + fixColumn2;
               }
             }
 
-            var startLine = msg.line + sampleLine;
+            const startLine = msg.line + sampleLine;
             msg.column += this.lines[startLine].prefixLength;
             msg.line = startLine;
 
             if (msg.endLine) {
-              var endLine = msg.endLine + sampleLine;
+              const endLine = msg.endLine + sampleLine;
               msg.endColumn += this.lines[endLine].prefixLength;
               msg.endLine = endLine;
             }
@@ -216,13 +216,13 @@ function eslintFiles(opts, filesSrc) {
   const engine = new eslint.CLIEngine(opts);
   engine.addPlugin('eslint-samples', module.exports);
 
-  let report = engine.executeOnFiles(filesSrc);
+  const report = engine.executeOnFiles(filesSrc);
 
   if (opts.fix) {
     eslint.CLIEngine.outputFixes(report);
   }
 
-  let results = report.results;
+  const results = report.results;
   if (opts.quiet) {
     results = eslint.CLIEngine.getErrorResults(results);
   }
@@ -233,7 +233,7 @@ function eslintFiles(opts, filesSrc) {
 module.exports.eslintFiles = eslintFiles;
 
 if (!module.parent) {
-  var result = eslintFiles(null, process.argv.slice(2));
+  const result = eslintFiles(null, process.argv.slice(2));
   console.log(result.output);
   process.exit(result.report.errorCount === 0 ? 0 : 1);
 }
