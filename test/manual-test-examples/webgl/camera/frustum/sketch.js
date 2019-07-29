b6c1a17105863adcfd8cc67e79447e0631b35fa6
Sanket Singh <sanketsingh24@gmail.com>
2019-07-30 00:04:52 +0530

    Update manual test

--- test/manual-test-examples/webgl/camera/frustum/sketch.js
+++ test/manual-test-examples/webgl/camera/frustum/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  frustum(-width / 2, width / 2, height / 2, -height / 2, -500, 500);
+  frustum(-width / 2, width / 2, -height / 2, height / 2, -500, 500);
 }
 
 function draw() {
-  background(0);
+  background(200);
   rotateX(map(mouseY, 0, height, 0, TWO_PI));
   rotateY(map(mouseX, 0, width, 0, TWO_PI));
   normalMaterial();
