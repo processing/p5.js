d3fa7ba85a402931eb0a3677109d1ea170f3fdbf
Stalgia Grigg <stalgia.grigg@gmail.com>
2019-08-06 14:05:50 -0700

    Optional zoom sensitivity argument for orbitControl

--- src/webgl/interaction.js
+++ src/webgl/interaction.js
@@ -22,6 +22,7 @@ import * as constants from '../core/constants';
  * @for p5
  * @param  {Number} [sensitivityX] sensitivity to mouse movement along X axis
  * @param  {Number} [sensitivityY] sensitivity to mouse movement along Y axis
+ * @param  {Number} [sensitivityZoom] sensitivity to scroll movement along Z axis
  * @chainable
  * @example
  * <div>
@@ -45,7 +46,11 @@ import * as constants from '../core/constants';
 
 // implementation based on three.js 'orbitControls':
 // https://github.com/mrdoob/three.js/blob/dev/examples/js/controls/OrbitControls.js
-p5.prototype.orbitControl = function(sensitivityX, sensitivityY) {
+p5.prototype.orbitControl = function(
+  sensitivityX,
+  sensitivityY,
+  sensitivityZoom
+) {
   this._assert3d('orbitControl');
   p5._validateParameters('orbitControl', arguments);
 
@@ -64,6 +69,9 @@ p5.prototype.orbitControl = function(sensitivityX, sensitivityY) {
   }
   if (typeof sensitivityY === 'undefined') {
     sensitivityY = sensitivityX;
+  }
+  if (typeof sensitivityZoom === 'undefined') {
+    sensitivityZoom = 0.5;
   }
 
   // default right-mouse and mouse-wheel behaviors (context menu and scrolling,
@@ -90,9 +98,9 @@ p5.prototype.orbitControl = function(sensitivityX, sensitivityY) {
   if (this._mouseWheelDeltaY !== this._pmouseWheelDeltaY) {
     // zoom according to direction of mouseWheelDeltaY rather than value
     if (this._mouseWheelDeltaY > 0) {
-      this._renderer._curCamera._orbit(0, 0, 0.5 * scaleFactor);
+      this._renderer._curCamera._orbit(0, 0, sensitivityZoom * scaleFactor);
     } else {
-      this._renderer._curCamera._orbit(0, 0, -0.5 * scaleFactor);
+      this._renderer._curCamera._orbit(0, 0, -sensitivityZoom * scaleFactor);
     }
   }
 
