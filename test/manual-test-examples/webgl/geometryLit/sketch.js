c2c319349d8c8dfb8361a17f89479a74a706c4e1
Stalgia Grigg <stalgia.grigg@gmail.com>
2019-08-26 17:46:20 +0300

    finish up the lit and unlit test, add noLights and spotLight

--- test/manual-test-examples/webgl/geometryLit/sketch.js
+++ test/manual-test-examples/webgl/geometryLit/sketch.js
@@ -1,10 +1,16 @@
-const columnHeight = 100;
-const numberOfColumns = 8;
-let rowWidth;
 const margins = 200;
-let img;
+const rowHeight = 100;
+// To add a test, raise this number by the number of rows that you wish to add
+// then add your test at the bottom of draw()
+const numberOfRows = 10;
+const uniqueGeometry = 6;
+let columnWidth;
 
-let x = 0;
+let currentX,
+  currentY,
+  theta = 0;
+
+let img;
 
 function preload() {
   img = loadImage('../assets/cat.jpg');
@@ -12,88 +18,95 @@ function preload() {
 
 function setup() {
   setAttributes('perPixelLighting', true);
-  createCanvas(windowWidth, columnHeight * numberOfColumns, WEBGL);
-  rowWidth = (width - margins) / 6;
+  createCanvas(windowWidth, rowHeight * numberOfRows, WEBGL);
+  columnWidth = (width - margins) / uniqueGeometry;
 }
-
-let theta = 0;
 
 function draw() {
   background(150, 150, 150, 255);
-  let locY = mouseY - height / 2;
-  let locX = mouseX - width / 2;
 
-  // ambientMaterial + ambientLight
-  translate(0, -columnHeight * (numberOfColumns / 2), 0);
+  // ambientMaterial() + ambientLight()
+  currentY = -rowHeight * (numberOfRows / 2);
+  translate(0, currentY, 0);
   push();
   noStroke();
   ambientMaterial(200, 100, 100);
   ambientLight(255);
-  drawGeometry();
+  drawGeometryRow();
   pop();
 
-  //  similar effect achieved with emissiveMaterial
-  translate(0, columnHeight, 0);
+  // ambientMaterial() + ambientLight()
+  translate(0, rowHeight, 0);
+  currentY += rowHeight;
   push();
   noStroke();
   emissiveMaterial(200, 100, 100);
-  drawGeometry();
+  drawGeometryRow();
   pop();
 
-  // ambientLight + pointLight + specularMaterial with low shininess
-  translate(0, columnHeight, 0);
+  // ambientLight() + pointLight() + specularMaterial with low shininess()
+  translate(0, rowHeight, 0);
+  currentY += rowHeight;
   push();
   ambientLight(0, 0, 150);
   pointLight(200, 0, 0, 0, 0, 0);
+  directionalLight(0, 200, 0, 0.5, 0, -1);
   noStroke();
   specularMaterial(250);
-  shininess(1);
-  drawGeometry();
+  shininess(0.1);
+  drawGeometryRow();
   pop();
 
-  // ambientLight + directionalLight + specularMaterial with high shininess
-  translate(0, columnHeight, 0);
+  // ambientLight() + pointLight() + specularMaterial with low shininess()
+  translate(0, rowHeight, 0);
+  currentY += rowHeight;
   push();
   ambientLight(0, 0, 150);
-  directionalLight(200, 0, 0, 0.5, 0, -1);
+  pointLight(200, 0, 0, 0, 0, 0);
+  directionalLight(0, 200, 0, 0.5, 0, -1);
   noStroke();
   specularMaterial(250);
   shininess(10);
-  drawGeometry();
+  drawGeometryRow();
   pop();
 
-  // ambientLight + pointLight + fill with high lightFalloff
-  translate(0, columnHeight, 0);
+  // ambientLight() + pointLight() + fill with high lightFalloff()
+  translate(0, rowHeight, 0);
+  currentY += rowHeight;
   push();
   ambientLight(100, 200, 100);
   pointLight(0, 0, 200, 0, 0, 0);
   noStroke();
   lightFalloff(1, 0, 0);
-  drawGeometry();
+  drawGeometryRow();
   pop();
 
-  // ambientLight + pointLight + fill with high lightFalloff
-  translate(0, columnHeight, 0);
+  // ambientLight() + pointLight() + fill with high lightFalloff()
+  translate(0, rowHeight, 0);
+  currentY += rowHeight;
   push();
   ambientLight(100, 200, 100);
   pointLight(0, 0, 200, 0, 0, 0);
   noStroke();
   lightFalloff(0.1, 0, 0);
-  drawGeometry();
+  drawGeometryRow();
   pop();
 
-  translate(0, columnHeight, 0);
+  // texture() + directionalLight()
+  translate(0, rowHeight, 0);
+  currentY += rowHeight;
   push();
   ambientLight(255);
   directionalLight(200, 0, 0, 0.5, 0, -1);
   noStroke();
   texture(img);
-  drawGeometry();
+  drawGeometryRow();
   pop();
 
-  //test specular highlight
+  //specularColor()
   noStroke();
-  translate(0, columnHeight, 0);
+  translate(0, rowHeight, 0);
+  currentY += rowHeight;
   push();
   shininess(10);
   ambientLight(50);
@@ -102,60 +115,95 @@ function draw() {
   specularColor(0, 250, 0);
   pointLight(0, 255, 0, 0, 100, 100);
   specularMaterial(255);
-  drawGeometry();
+  drawGeometryRow();
   pop();
+
+  // spotLight()
+  translate(0, rowHeight, 0);
+  currentY += rowHeight;
+  push();
+  ambientLight(50);
+  spotLight(0, 250, 0, 0, currentY + 100, 200, 0, 0, -1, 10, 3);
+  spotLight(0, 0, 250, -width / 2, currentY, 100, 1, 0, 0, 100, 25);
+  ambientMaterial(250);
+  drawGeometryRow();
+  pop();
+
+  translate(0, rowHeight, 0);
+  currentY += rowHeight;
+  push();
+  fill(255);
+  ambientLight(150);
+  pointLight(200, 200, 200, 0, 0, 0);
+  drawGeometryRow(() => {
+    noLights();
+    fill(255);
+  });
+  pop();
+
+  // to add test, copy-paste the following lines and uncomment
+  // translate(0, rowHeight, 0);
+  // currentY += rowHeight;
+  // push();
+  // Add your lighting and material conditions here :-)
+  // drawGeometryRow(); // you can change condition halfway through a row by adding a function argument
+  // pop();
 
   theta += 0.05;
 }
 
-function drawGeometry() {
-  translate(-rowWidth * 3, 0, 0);
-  x = 0;
-  push();
-  _rotate();
-  plane(25);
-  pop();
+function drawGeometryRow(callInMiddle) {
+  currentX = -columnWidth * (uniqueGeometry / 2);
+  translate(currentX, 0, 0);
+  drawObject(() => {
+    plane(25);
+  });
 
-  translate(rowWidth, 0, 0);
-  x++;
-  push();
-  _rotate();
-  box(25, 25, 25);
-  pop();
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    box(25, 25, 25);
+  });
 
-  translate(rowWidth, 0, 0);
-  x++;
-  push();
-  _rotate();
-  cylinder(25, 25);
-  pop();
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    cylinder(25, 25);
+  });
 
-  translate(rowWidth, 0, 0);
-  x++;
-  push();
-  _rotate();
-  cone(25, 25);
-  pop();
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    cone(25, 25);
+  });
 
-  translate(rowWidth, 0, 0);
-  x++;
-  push();
-  _rotate();
-  torus(25, 10);
-  pop();
+  if (typeof callInMiddle === 'function') {
+    callInMiddle();
+  }
 
-  translate(rowWidth, 0, 0);
-  x++;
-  push();
-  _rotate();
-  sphere(25);
-  pop();
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    torus(25, 10);
+  });
 
-  translate(rowWidth, 0, 0);
-  x++;
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    sphere(25);
+  });
+
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    rect(0, 0, 25, 25);
+  });
+}
+
+function drawObject(geom) {
   push();
   _rotate();
-  rect(0, 0, 25, 25);
+  geom();
   pop();
 }
 
--- test/manual-test-examples/webgl/geometryUnlit/sketch.js
+++ test/manual-test-examples/webgl/geometryUnlit/sketch.js
@@ -1,6 +1,10 @@
-let columnHeight = 100;
-let rowWidth;
+let rowHeight = 100;
+// To add a test, raise this number by the number of rows that you wish to add
+// then add your test at the bottom of draw()
+const numberOfRows = 5;
+let columnWidth;
 let margins = 200;
+const uniqueGeometry = 6;
 let img;
 
 function preload() {
@@ -8,8 +12,8 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, 600, WEBGL);
-  rowWidth = (width - margins) / 6;
+  createCanvas(windowWidth, rowHeight * numberOfRows, WEBGL);
+  columnWidth = (width - margins) / 6;
 }
 
 var theta = 0;
@@ -17,95 +21,111 @@ var theta = 0;
 function draw() {
   background(150, 150, 150, 255);
 
-  translate(0, -columnHeight * 2, 0);
+  //default fill and stroke
+  translate(0, -rowHeight * 2, 0);
   push();
-  drawGeometry();
+  drawGeometryRow();
   pop();
 
-  translate(0, columnHeight, 0);
+  // normalMaterial
+  translate(0, rowHeight, 0);
   push();
   normalMaterial();
-  drawGeometry();
+  drawGeometryRow();
   pop();
 
-  translate(0, columnHeight, 0);
+  //stroke + noFill
+  translate(0, rowHeight, 0);
   push();
   stroke(255, 0, 255);
   strokeWeight(0.5);
   noFill();
-  drawGeometry();
+  drawGeometryRow();
   pop();
 
-  translate(0, columnHeight, 0);
+  // noStroke + fill
+  translate(0, rowHeight, 0);
   push();
   fill(100, 255, 100);
   noStroke();
-  drawGeometry();
+  drawGeometryRow();
   pop();
 
-  translate(0, columnHeight, 0);
+  // texture
+  translate(0, rowHeight, 0);
   push();
   texture(img);
   noStroke();
-  drawGeometry();
+  drawGeometryRow();
   pop();
+
+  // To add test, copy-paste the following lines and uncomment
+  // translate(0, rowHeight, 0);
+  // push();
+  // Add your lighting and material conditions here :-)
+  // drawGeometryRow();
+  // pop();
+
   theta += 0.05;
 }
 
-function drawGeometry() {
-  translate(-rowWidth * 3, 0, 0);
-  push();
-  rotateZ(theta * mouseX * 0.001);
-  rotateX(theta * mouseX * 0.001);
-  rotateY(theta * mouseX * 0.001);
-  plane(25);
-  pop();
+function drawGeometryRow(callInMiddle) {
+  currentX = -columnWidth * (uniqueGeometry / 2);
+  translate(currentX, 0, 0);
+  drawObject(() => {
+    plane(25);
+  });
 
-  translate(rowWidth, 0, 0);
-  push();
-  rotateZ(theta * mouseX * 0.001);
-  rotateX(theta * mouseX * 0.001);
-  rotateY(theta * mouseX * 0.001);
-  box(25, 25, 25);
-  pop();
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    box(25, 25, 25);
+  });
 
-  translate(rowWidth, 0, 0);
-  push();
-  rotateZ(theta * mouseX * 0.001);
-  rotateX(theta * mouseX * 0.001);
-  rotateY(theta * mouseX * 0.001);
-  cylinder(25, 25);
-  pop();
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    cylinder(25, 25);
+  });
 
-  translate(rowWidth, 0, 0);
-  push();
-  rotateZ(theta * mouseX * 0.001);
-  rotateX(theta * mouseX * 0.001);
-  rotateY(theta * mouseX * 0.001);
-  cone(25, 25);
-  pop();
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    cone(25, 25);
+  });
 
-  translate(rowWidth, 0, 0);
-  push();
-  rotateZ(theta * mouseX * 0.001);
-  rotateX(theta * mouseX * 0.001);
-  rotateY(theta * mouseX * 0.001);
-  torus(25, 10);
-  pop();
+  if (typeof callInMiddle === 'function') {
+    callInMiddle();
+  }
 
-  translate(rowWidth, 0, 0);
-  push();
-  rotateZ(theta * mouseX * 0.001);
-  rotateX(theta * mouseX * 0.001);
-  rotateY(theta * mouseX * 0.001);
-  sphere(25);
-  pop();
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    torus(25, 10);
+  });
 
-  translate(rowWidth, 0, 0);
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    sphere(25);
+  });
+
+  translate(columnWidth, 0, 0);
+  currentX += columnWidth;
+  drawObject(() => {
+    rect(0, 0, 25, 25);
+  });
+}
+
+function drawObject(geom) {
   push();
+  _rotate();
+  geom();
+  pop();
+}
+
+function _rotate() {
   rotateZ(theta * mouseX * 0.001);
   rotateX(theta * mouseX * 0.001);
   rotateY(theta * mouseX * 0.001);
-  rect(0, 0, 25, 25);
-  pop();
 }
