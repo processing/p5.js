8b942d29a051bb9f5ac858d958defdc837708f38
vishal <vishalsingh600700@gmail.com>
2019-08-29 23:19:55 +0530

    Modification in doc file

--- src/core/reference.js
+++ src/core/reference.js
@@ -321,9 +321,11 @@
  * JSON.stringify(value, replacer, space)
  *
  * [value]: The value to convert to a JSON string.
+ * 
  * [replacer]  //Optional  :A function that alters the behavior of the stringification process, or an array of String and Number objects 
  * that serve as a whitelist for selecting/filtering the properties of the value object to be included in the JSON string.
  * If this value is null or not provided, all properties of the object are included in the resulting JSON string.
+ * 
  * [space]  //optional :A String or Number object that's used to insert white space into the output JSON string for readability purposes.
  * If this is a Number, it indicates the number of space characters to use as white space; this number is capped at 10 (if it is greater, the value is just 10).
  * Values less than 1 indicate that no space should be used.
