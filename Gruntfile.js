c651a8204290228386957ee50d771381c191fb55
Evelyn Masso <outofambit@github.com>
2020-01-05 15:22:56 -0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -393,7 +393,7 @@ module.exports = grunt => {
       dev: {
         sort: true,
         src: 'src/**/*.js',
-        dest: 'assets',
+        dest: 'internationalization',
         options: {
           lngs: ['en'],
           resource: {
