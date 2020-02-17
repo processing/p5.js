1643935c46bdf32ef168c1649262f2311643f718
evelyn masso <coding@outofambit.com>
2020-02-17 18:48:32 -0500

    rework i18next init

--- src/core/internationalization.js
+++ src/core/internationalization.js
@@ -2,15 +2,35 @@ import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import resources from '../../translations';
 
-i18next
-  .use(LanguageDetector)
-  .init({
-    fallbackLng: 'en',
-    nestingPrefix: '$tr(',
-    nestingSuffix: ')',
-    defaultNS: 'translation',
-    resources
-  })
-  .catch(e => `Translations failed to load (${e})`);
+/**
+ * This is our translation function. Give it a key and
+ * it will retreive the appropriate string
+ * (within supported languages) according to the
+ * user's browser's language settings.
+ *
+ * (We'll set this to a real value in the init function below!)
+ */
+export let translator;
 
-export default i18next.t;
+/**
+ * Set up our translation function, with loaded languages
+ */
+export const initialize = () =>
+  new Promise((resolve, reject) => {
+    i18next
+      .use(LanguageDetector)
+      .init({
+        fallbackLng: 'en',
+        nestingPrefix: '$tr(',
+        nestingSuffix: ')',
+        defaultNS: 'translation',
+        resources
+      })
+      .then(
+        translateFn => {
+          translator = translateFn;
+          resolve();
+        },
+        e => reject(`Translations failed to load (${e})`)
+      );
+  });
