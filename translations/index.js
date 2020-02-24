68049570d09a1cf88960974f2836fe6992404fa9
evelyn masso <coding@outofambit.com>
2020-02-24 17:06:50 -0500

    more inline documentation

--- translations/index.js
+++ translations/index.js
@@ -1,6 +1,13 @@
 import en from './en/translation';
 import es from './es/translation';
 
+/**
+ * Maps our translations to their language key
+ * (`en` is english, `es` es español)
+ *
+ * `translation` is the namespace we're using for
+ * our initial set of translation strings.
+ */
 export default {
   en: {
     translation: en
