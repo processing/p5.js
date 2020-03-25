ab0a120bb2934aed7ef830422aa1bdd38f1ecafc
Akshay Padte <akshaypadte99@gmail.com>
2020-03-26 04:20:32 +0530

    A way to verify downloads

--- test/unit/image/downloading.js
+++ test/unit/image/downloading.js
@@ -40,5 +40,25 @@ suite('downloading animated gifs', function() {
     test('should not throw an error', function() {
       myp5.saveGif(myGif);
     });
+    test('should download a gif', function() {
+      // create a backup of createObjectURL
+      let couBackup = window.URL.createObjectURL;
+      let gifBlob;
+
+      // file-saver uses createObjectURL as an intermediate step. If we
+      // modify the definition a just a little bit we can capture whenever
+      // it is called and also peek in the data that was passed to it
+      window.URL.createObjectURL = blob => {
+        // now use this gifBlob to verify the downloaded output
+        gifBlob = blob;
+        return couBackup(blob);
+      };
+
+      myp5.saveGif(myGif);
+      console.log(gifBlob);
+      // restore createObjectURL to the original one
+      window.URL.createObjectURL = couBackup;
+      assert.strictEqual(gifBlob.type, 'image/gif');
+    });
   });
 });
