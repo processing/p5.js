e7abd2cc84c50e331f368eb6818bb13ae1b02b89
limzykenneth <limzy.kenneth@gmail.com>
2020-04-04 00:20:04 +0100

    Centralize grunt configs

--- tasks/release/release-p5.js
+++ tasks/release/release-p5.js
@@ -1,100 +1,15 @@
-/* Grunt Task to create and publish a p5.js release */
-/*
-MUST HAVES BEFOREHAND :
-* Logged in NPM CLI : Check if you are logged in by "npm whoami"
-* High Bandwidth : Lots of things to download and pull and push (~190 MB)
-* An environment variable named GITHUB_TOKEN with the value of your Access Token : Make one by going to your Settings->Personal Access Tokens-> New Token. Once you have it, in your shell, run "export GITHUB_TOKEN=<your token here>".
-*
-* Run with 'grunt release-p5:[increment]' where '[increment]' can be 'patch',
-* 'minor', or 'major', corresponding to semver.
-*/
+// Grunt Task to create release archive. Also handles releasing
+// for Bower and release of reference on the website.
 
 const open = require('open');
 
 module.exports = function(grunt) {
-  // Options for this custom task
-  const opts = {
-    clean: {
-      release: {
-        src: ['release/']
-      },
-      reference: grunt.config.get('clean').reference,
-      bower: {
-        src: ['bower-repo/']
-      },
-      website: {
-        src: ['p5-website/']
-      }
-    },
-    compress: {
-      main: {
-        options: {
-          archive: 'release/p5.zip'
-        },
-        files: [{ cwd: 'lib/', src: ['**/*'], expand: true }]
-      }
-    },
-    copy: {
-      release: {
-        expand: true,
-        src: [
-          'lib/p5.js',
-          'lib/p5.min.js',
-          'lib/addons/p5.sound.js',
-          'lib/addons/p5.sound.min.js'
-        ],
-        dest: 'release/',
-        flatten: true
-      },
-      bower: {
-        files: [
-          {
-            expand: true,
-            src: ['lib/p5.js', 'lib/p5.min.js'],
-            dest: 'bower-repo/lib/'
-          },
-          {
-            expand: true,
-            src: 'lib/addons/*',
-            dest: 'bower-repo/lib/addons/'
-          }
-        ]
-      },
-      docs: {
-        files: [
-          {
-            expand: true,
-            src: ['docs/reference/data.json', 'docs/reference/data.min.json'],
-            dest: 'p5-website/src/templates/pages/reference/',
-            flatten: true
-          },
-          {
-            expand: true,
-            cwd: 'docs/reference/assets/',
-            src: '**',
-            dest: 'p5-website/src/templates/pages/reference/assets'
-          },
-          {
-            expand: true,
-            src: ['lib/p5.min.js', 'lib/addons/p5.sound.min.js'],
-            dest: 'p5-website/src/assets/js/',
-            flatten: true
-          }
-        ]
-      }
-    }
-  };
-
   // Register the Release Task
   grunt.registerTask(
     'release-p5',
     'Drafts and Publishes a fresh release of p5.js',
     function(args) {
       // 0. Setup Config
-      grunt.config.set('clean', opts.clean);
-      grunt.config.set('compress', opts.compress);
-      grunt.config.set('copy', opts.copy);
-
       // Keeping URLs as config vars, so that anyone can change
       // them to add their own, to test if release works or not.
       grunt.config.set('bowerReleaser', 'lmccart');
