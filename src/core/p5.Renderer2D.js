b3a905936d95e52157dbb80ef053b31ad8eae5b5
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-06-05 19:09:01 -0700

    remove closePath call in ellipse fix stroke bug closes #4598

--- src/core/p5.Renderer2D.js
+++ src/core/p5.Renderer2D.js
@@ -521,7 +521,6 @@ p5.Renderer2D.prototype.ellipse = function(args) {
   ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
   ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
   ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
-  ctx.closePath();
   if (doFill) {
     ctx.fill();
   }
