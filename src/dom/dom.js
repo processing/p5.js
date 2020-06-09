2a5009e02ec78a53f9bfaa42a7998ae555b6f958
Peiling Jiang <jiangpeiling98@outlook.com>
2020-06-09 15:23:04 -0400

    fix lint

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1309,7 +1309,7 @@ if (navigator.mediaDevices.getUserMedia === undefined) {
  * }
  *
  * function draw() {
- *   image(capture, 0, 0, width, (width * capture.height) / capture.width);
+ *   image(capture, 0, 0, width, width * capture.height / capture.width);
  *   filter(INVERT);
  * }
  * </code>
@@ -2107,9 +2107,9 @@ p5.Element.prototype.size = function(w, h) {
     var AUTO = p5.prototype.AUTO;
     if (aW !== AUTO || aH !== AUTO) {
       if (aW === AUTO) {
-        aW = (h * this.width) / this.height;
+        aW = h * this.width / this.height;
       } else if (aH === AUTO) {
-        aH = (w * this.height) / this.width;
+        aH = w * this.height / this.width;
       }
       // set diff for cnv vs normal div
       if (this.elt instanceof HTMLCanvasElement) {
