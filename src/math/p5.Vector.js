3d34b2e92c6a46ca5bf27c626e8c507cb2612acc
weslord <weslord@gmail.com>
2020-10-13 04:13:52 -0700

    Add static version of Vector.normalize()

--- src/math/p5.Vector.js
+++ src/math/p5.Vector.js
@@ -1213,6 +1213,20 @@ p5.Vector.prototype.dist = function dist(v) {
  * // [0.4454354, 0.8908708, 0.089087084]
  * </code>
  * </div>
+ *
+ * <div class="norender">
+ * <code>
+ * // Static method
+ * let v_initial = createVector(10, 20, 2);
+ * // v_initial has components [10.0, 20.0, 2.0]
+ * let v_normalized = p5.Vector.normalize(v_initial);
+ * print(v_normalized);
+ * // returns a new vector with components set to
+ * // [0.4454354, 0.8908708, 0.089087084]
+ * // v_initial remains unchanged
+ * </code>
+ * </div>
+ *
  * <div>
  * <code>
  * function draw() {
@@ -2319,6 +2333,31 @@ p5.Vector.mag = function mag(vecT) {
     z = vecT.z;
   const magSq = x * x + y * y + z * z;
   return Math.sqrt(magSq);
+};
+
+/**
+ * Normalize the vector to length 1 (make it a unit vector).
+ */
+/**
+ * @method normalize
+ * @static
+ * @param {p5.Vector} v  the vector to normalize
+ * @param {p5.Vector} [target] the vector to receive the result (Optional)
+ * @return {p5.Vector}   v normalized to a length of 1
+ */
+p5.Vector.normalize = function normalize(v, target) {
+  if (arguments.length < 2) {
+    target = v.copy();
+  } else {
+    if (!(target instanceof p5.Vector)) {
+      p5._friendlyError(
+        'The target parameter should be of type p5.Vector',
+        'p5.Vector.normalize'
+      );
+    }
+    target.set(v);
+  }
+  return target.normalize();
 };
 
 export default p5.Vector;
