362e6b6de2f17f7ff1fe1a926397e566bfd81a8e
JetStarBlues <JetStarBlues@users.noreply.github.com>
2021-06-16 18:44:34 -0600

    Document `vertex()` variant

--- src/core/shape/vertex.js
+++ src/core/shape/vertex.js
@@ -958,9 +958,17 @@ p5.prototype.quadraticVertex = function(...args) {
  * @method vertex
  * @param  {Number} x
  * @param  {Number} y
- * @param  {Number} z   z-coordinate of the vertex
- * @param  {Number} [u] the vertex's texture u-coordinate
- * @param  {Number} [v] the vertex's texture v-coordinate
+ * @param  {Number} z   z-coordinate of the vertex.
+ *                       Defaults to 0 if not specified.
+ * @chainable
+ */
+/**
+ * @method vertex
+ * @param  {Number} x
+ * @param  {Number} y
+ * @param  {Number} [z]
+ * @param  {Number} u   the vertex's texture u-coordinate
+ * @param  {Number} v   the vertex's texture v-coordinate
  * @chainable
  */
 p5.prototype.vertex = function(x, y, moveTo, u, v) {
