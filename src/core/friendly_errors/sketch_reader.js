4beecefaa1b4413bf7aad1eaed9ba9028061b730
Shantanu Kaushik <shantanukaushik12@gmail.com>
2021-07-16 12:19:40 +0530

    use promises instead of async await

--- src/core/friendly_errors/sketch_reader.js
+++ src/core/friendly_errors/sketch_reader.js
@@ -28,15 +28,11 @@ const _extractVariables = arr => {
     '%cFrom _extractVariables',
     'color: black; font-weight: bold; font-size: 16px; background: yellow;'
   );
-  //we can extract variable names from here and check
-  //there are 2 regex and 2 ways of declaring the variables, we can use them both and do our stuff
-
-  //check if the line contains a , => either let a, b=2,d; or let a = [12,321,342]
+  //extract variable names from the user's code
   let matches = [];
   arr.forEach(ele => {
+    //extract a, b, c from let/const a=10, b=20, c;
     if (ele.includes(',')) {
-      //do the regex 1
-
       matches.push(
         ...ele.split(',').flatMap(s => {
           let match;
@@ -49,7 +45,7 @@ const _extractVariables = arr => {
         })
       );
     } else {
-      //do the regex 2
+      //extract a from let/const a=10;
       const reg = /(?:(?:let|const)\s+)([\w$]+)/g;
       const found = ele.matchAll(reg);
       for (const match of found) {
@@ -57,13 +53,13 @@ const _extractVariables = arr => {
       }
     }
   });
-  _checkForConsts(matches);
-  //now we have to check if the obtained variables are a part of p5.js or not
+  //check if the obtained variables are a part of p5.js or not
+  _checkForConsts(matches); //check if the obtained variables are a p5.js constants
 };
 
 const _extractFuncVariables = arr => {
   let matches = [];
-  //we can extract function names from here and check
+  //extract function names
   console.log(
     '%cFrom _extractFuncVariables',
     'color: black; font-weight: bold; font-size: 16px; background: yellow;'
@@ -72,13 +68,15 @@ const _extractFuncVariables = arr => {
   arr.forEach(ele => {
     matches.push(ele.match(reg)[1]);
   });
+  //matches array contains the names of the functions
   console.log(matches);
 };
 
 const _codeToLines = code => {
+  //convert code to array of code and filter out
+  //unnecessary lines
   let arr = code.split('\n');
-
-  //filter out variable names from all
+  //filter out lines containing variable names
   let arrVars = arr
     .map(line => line.trim())
     .filter(
@@ -89,7 +87,7 @@ const _codeToLines = code => {
         (!line.includes('=>') && !line.includes('function'))
     );
 
-  //filter out function names from all
+  //filter out lines containing function names
   let arrFunc = arr
     .map(line => line.trim())
     .filter(
@@ -100,17 +98,16 @@ const _codeToLines = code => {
         (line.includes('=>') || line.includes('function'))
     );
 
-  //now pass the relevant array to a regexer function which will list all the variables/functions
+  //pass the relevant array to a function which will extract all the variables/functions names
   _extractVariables(arrVars);
   _extractFuncVariables(arrFunc);
 };
 
 const _removeMultilineComments = str => {
-  //get the start index of open multiline comment
   let start = str.indexOf('/*');
   let end = str.indexOf('*/');
 
-  //create a new string which don't have things in comments
+  //create a new string which don't have multiline comments
   while (start !== -1 && end !== -1) {
     if (start === 0) {
       str = str.substr(end + 2);
@@ -123,41 +120,49 @@ const _removeMultilineComments = str => {
   return str;
 };
 
-const _fesCodeReader = async () => {
-  let codeNode = document.querySelector('[data-tag="@fs-sketch.js"]'),
-    text = '';
-  if (codeNode) {
-    //if web editor
-    text = codeNode.innerHTML;
-  } else {
-    //obtain the name of the file in script tag
-    const scripts = [...document.querySelectorAll('script')]
-      .map(file => file.getAttribute('src'))
-      .filter(
-        attr =>
-          attr !== null &&
-          attr !== undefined &&
-          !attr.includes('p5.js') &&
-          !attr.includes('p5.min.js') &&
-          !attr.includes('p5.sounds.min.js') &&
-          !attr.includes('p5.sounds.js') &&
-          attr !== ''
+const _fesCodeReader = () => {
+  return new Promise((resolve, reject) => {
+    let codeNode = document.querySelector('[data-tag="@fs-sketch.js"]'),
+      text = '';
+    if (codeNode) {
+      //if web editor
+      text = codeNode.innerHTML;
+      resolve(text);
+    } else {
+      //obtain the name of the file in script tag
+      //ignore p5.js, p5.min.js, p5.sounds.js, p5.sounds.min.js
+      const scripts = [...document.querySelectorAll('script')]
+        .map(file => file.getAttribute('src'))
+        .filter(
+          attr =>
+            attr !== null &&
+            attr !== undefined &&
+            !attr.includes('p5.js') &&
+            !attr.includes('p5.min.js') &&
+            !attr.includes('p5.sounds.min.js') &&
+            !attr.includes('p5.sounds.js') &&
+            attr !== ''
+        );
+      //obtain the user's code form the JS file
+      fetch(`${scripts[0]}`).then(res =>
+        res.text().then(txt => {
+          text = txt;
+          resolve(text);
+        })
       );
-    //scripts will have all the files which donsen't contains the files in the array: [null, undefined, '', 'p5.js','p5.min.js','p5.sound.min.js', 'p5.sound.js']
-    //there can be multiple files but we will check only 1 file because we assume that user will only use 1 file and not multiple
-    //this can of course be extended to multiple files
-
-    await fetch(`${scripts[0]}`).then(res =>
-      res.text().then(txt => {
-        text = txt;
-      })
-    );
-  }
-  //convert the code to array of lines of code
-  text = _removeMultilineComments(text);
-  _codeToLines(text);
+    }
+  });
 };
 
-window.addEventListener('load', _fesCodeReader);
+window.addEventListener('load', () => {
+  _fesCodeReader()
+    .then(code => {
+      //remove multiline comments
+      code = _removeMultilineComments(code);
+      //convert the code to array of lines of code
+      _codeToLines(code);
+    })
+    .catch(err => console.log(err));
+});
 
 export default p5;
