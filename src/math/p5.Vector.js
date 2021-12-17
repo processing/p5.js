25a1b1c432ea002ced4bde0d2a7f3490fea5d8bb
A Welles <awelles@users.noreply.github.com>
2021-12-17 18:31:35 -0500

    Revert "fix #5497"
    This reverts commit fbe4ed6ac877348753e90c222b765ef8fe0301ee.

--- src/math/p5.Vector.js
+++ src/math/p5.Vector.js
@@ -1489,11 +1489,9 @@ p5.Vector.prototype.heading = function heading() {
  */
 
 p5.Vector.prototype.setHeading = function setHeading(a) {
-  let newHeading = a;
-  if (this.isPInst) newHeading = this._toRadians(newHeading);
   let m = this.mag();
-  this.x = m * Math.cos(newHeading);
-  this.y = m * Math.sin(newHeading);
+  this.x = m * Math.cos(a);
+  this.y = m * Math.sin(a);
   return this;
 };
 
