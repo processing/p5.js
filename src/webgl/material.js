3a0adcd3e9d8bcb6b64f3099b8330ea98a853615
Adam Ferriss <aferriss@fb.com>
2021-12-26 18:09:22 -0800

    Allow texture() to accept p5.Texture without throwing a warning.

--- src/webgl/material.js
+++ src/webgl/material.js
@@ -380,7 +380,7 @@ p5.prototype.resetShader = function() {
  * <a href="https://p5js.org/examples/3d-materials.html">example</a>.
  *
  * @method texture
- * @param {p5.Image|p5.MediaElement|p5.Graphics} tex  image to use as texture
+ * @param {p5.Image|p5.MediaElement|p5.Graphics|p5.Texture} tex  image to use as texture
  * @chainable
  * @example
  * <div>
