9a8069114624cfeb8ea42725c416dd8288b2035a
u7310220 <u7310220@anu.edu.au>
2022-10-25 21:39:49 +1100

    Removed unrelated changes

--- src/core/main.js
+++ src/core/main.js
@@ -387,7 +387,7 @@ class p5 {
         this._frameRate = 1000.0 / (now - this._lastFrameTime);
         this.deltaTime = now - this._lastFrameTime;
         this._setProperty('deltaTime', this.deltaTime);
-        this._lastFrameTime = this._lastFrameTime+target_time_between_frames;
+        this._lastFrameTime = now;
 
         // If the user is actually using mouse module, then update
         // coordinates, otherwise skip. We can test this by simply
@@ -688,9 +688,6 @@ class p5 {
         globalObject[prop] = value;
       }
     };
-  }
-  isWebGL() {
-    return Graphics.isWebGL;
   }
 }
 
