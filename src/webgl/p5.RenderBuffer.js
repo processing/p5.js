cbc1677dff9d328f6e11dc35ab51a5467f70726a
INARI_DARKFOX <39549290+inaridarkfox4231@users.noreply.github.com>
2023-01-23 18:12:54 +0900

    Disable registers not used for drawing
    If the registers corresponding to the attributes that are not used for drawing are left open, the data still stored there will cause problems.
    So it asks the renderer if the register corresponding to it is open, and if it is, it closes it and notifies the renderer and shader of that state.

--- src/webgl/p5.RenderBuffer.js
+++ src/webgl/p5.RenderBuffer.js
@@ -58,6 +58,14 @@ p5.RenderBuffer.prototype._prepareBuffer = function(geometry, shader) {
     }
     // enable the attribute
     shader.enableAttrib(attr, this.size);
+  } else {
+    const loc = attr.location;
+    if (loc == -1 || !this._renderer.registerEnabled[loc]) { return; }
+    // Disable register corresponding to unused attribute
+    gl.disableVertexAttribArray(loc);
+    attr.enabled = false;
+    // Record register availability
+    this._renderer.registerEnabled[loc] = false;
   }
 };
 
