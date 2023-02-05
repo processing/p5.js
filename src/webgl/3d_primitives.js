1f70f3481806891d8adb54205acb98774e5e4a33
Leslie Yip <leslieyip02@gmail.com>
2023-02-05 09:06:15 +0800

    Fix: use this._pInst.noStroke() instead

--- src/webgl/3d_primitives.js
+++ src/webgl/3d_primitives.js
@@ -1994,8 +1994,8 @@ p5.RendererGL.prototype.image = function(
   this._pInst.push();
 
   this._pInst.noLights();
+  this._pInst.noStroke();
 
-  noStroke();
   this._pInst.texture(img);
   this._pInst.textureMode(constants.NORMAL);
 
