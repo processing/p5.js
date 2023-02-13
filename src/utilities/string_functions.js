79c49bdf1aeda06cc71921431e484abb7095c8a3
adityashrivastav1 <aditya.shri9981@gmail.com>
2023-02-13 22:10:16 +0530

    rearraged the code

--- src/utilities/string_functions.js
+++ src/utilities/string_functions.js
@@ -200,23 +200,6 @@ p5.prototype.nf = function(nums, left, right) {
   }
 };
 
-function convertToDecimal(n) {
-  var sign = +n < 0 ? '-' : '',
-    toStr = n.toString();
-  if (!/e/i.test(toStr)) {
-    return n;
-  }
-  var [lead, decimal, pow] = n.toString()
-    .replace(/^-/, '')
-    .replace(/^([0-9]+)(e.*)/, '$1.$2')
-    .split(/e|\./);
-  return +pow < 0 ?
-    (sign + '0.' + '0'.repeat(Math.max(Math.abs(pow) - 1 || 0, 0)) + lead + decimal
-    ) : (sign + lead + (+pow >= decimal.length) ?
-      (decimal + '0'.repeat(Math.max(+pow - decimal.length || 0, 0))
-      ) : (decimal.slice(0, +pow) + '.' + decimal.slice(+pow)));
-}
-
 function doNf(num, left, right) {
   let numStr = num.toString();
   if (numStr.includes('e')) {
@@ -232,6 +215,23 @@ function doNf(num, left, right) {
     leftPart = leftPart.padStart(left, '0');
     return leftPart + '.' + rightPart;
   }
+}
+
+function convertToDecimal(n) {
+  var sign = +n < 0 ? '-' : '',
+    toStr = n.toString();
+  if (!/e/i.test(toStr)) {
+    return n;
+  }
+  var [lead, decimal, pow] = n.toString()
+    .replace(/^-/, '')
+    .replace(/^([0-9]+)(e.*)/, '$1.$2')
+    .split(/e|\./);
+  return +pow < 0 ?
+    (sign + '0.' + '0'.repeat(Math.max(Math.abs(pow) - 1 || 0, 0)) + lead + decimal
+    ) : (sign + lead + (+pow >= decimal.length) ?
+      (decimal + '0'.repeat(Math.max(+pow - decimal.length || 0, 0))
+      ) : (decimal.slice(0, +pow) + '.' + decimal.slice(+pow)));
 }
 
 /**
