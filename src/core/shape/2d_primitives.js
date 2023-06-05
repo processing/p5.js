f1a00c66fe5c2fc32995450064ab3be994c5844b
wong-justin <jkwongfl@yahoo.com>
2023-06-05 11:31:33 -0400

    add line() WEBGL example

--- src/core/shape/2d_primitives.js
+++ src/core/shape/2d_primitives.js
@@ -375,6 +375,17 @@ p5.prototype._renderEllipse = function(x, y, w, h, detailX) {
  * </code>
  * </div>
  *
+ * <div>
+ * <code>
+ * createCanvas(100, 100, WEBGL);
+ * strokeWeight(7);
+ * line(0, 0, 0, 30, 0, 0);
+ * line(0, 0, 0, 0, 30, 0);
+ * line(-30, -30, -30, 30, 30, 30);
+ * describe('two lines mark the flat x and y axes while a third line spans diagonally through the third dimension');
+ * </code>
+ * </div>
+ *
  */
 
 /**
