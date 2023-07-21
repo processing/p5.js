8b1e19ce2341bcae4b68e5296c953b952ad34f37
AryanKoundal <aryankoundal474@gmail.com>
2023-07-21 13:13:24 +0530

    added definition of map function in lightingglsl

--- src/webgl/shaders/lighting.glsl
+++ src/webgl/shaders/lighting.glsl
@@ -70,6 +70,10 @@ LightResult _light(vec3 viewDirection, vec3 normal, vec3 lightVector) {
   return lr;
 }
 
+float map(float value, float min1, float max1, float min2, float max2) {
+  return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+}
+
 vec2 mapTextureToNormal( vec3 normal ){
   // x = r sin(phi) cos(theta)
   // y = r cos(phi)
