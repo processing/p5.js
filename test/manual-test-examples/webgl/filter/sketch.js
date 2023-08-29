547578b5c2dc58488af080876abaef80ea81f50f
Adam Ferriss <aferriss@fb.com>
2023-08-29 00:02:24 -0700

    testing different manual examples

--- test/manual-test-examples/webgl/filter/sketch.js
+++ test/manual-test-examples/webgl/filter/sketch.js
@@ -1,15 +1,29 @@
+
 let img;
 
 function preload() {
-  img = loadImage('../../../../docs/reference/assets/bricks.jpg');
+  img = loadImage('../../../../docs/reference/assets/moonwalk.jpg');
 }
+
+let pg;
 
 function setup() {
-  createCanvas(200, 200, WEBGL);
-  img.resize(200, 200);
+  // img.resize(600, 600);
+  createCanvas(img.width, img.height);
+  pg = createGraphics(img.width, img.height, WEBGL);
 }
 
+
 function draw() {
-  image(img, -width/2, -height/2);
-  filter(POSTERIZE, 4);
+  if(pg.webglVersion === P2D){
+    pg.image(img, 0, 0, width, height);
+  } else {
+    pg.image(img, -width / 2, -height / 2, width, height);
+  }
+
+  if(mouseIsPressed){
+    pg.filter(BLUR, 10);
+  }
+
+  image(pg, 0, 0, width, height);
 }
