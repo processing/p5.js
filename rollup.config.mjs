5948219ef025ffca26114e638436d2cee33fa877
limzykenneth <limzy.kenneth@gmail.com>
2023-09-18 20:26:52 +0100

    Fix shaders string import

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
+import { string } from 'rollup-plugin-string';
 
 export default {
   input: 'src/app.js',
@@ -8,5 +9,11 @@ export default {
     format: 'iife',
     name: 'p5'
   },
-  plugins: [nodeResolve(), json()]
+  plugins: [
+    nodeResolve(),
+    json(),
+    string({
+      include: 'src/webgl/shaders/**/*'
+    })
+  ]
 };
