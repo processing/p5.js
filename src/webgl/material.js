112496d46fcfa4ed09f16adffcc53fb649452fa1
Deveshi Dwivedi <120312681+deveshidwivedi@users.noreply.github.com>
2023-10-28 19:48:20 +0530

    Update material.js

--- src/webgl/material.js
+++ src/webgl/material.js
@@ -152,23 +152,23 @@ p5.prototype.loadShader = function(
  *
  *
  * // the fragment shader is called for each pixel
- * let fs =
- *   varying +
- *   'uniform vec2 p;' +
- *   'uniform float r;' +
- *   'const int I = 500;' +
- *   'void main() {' +
- *   '  vec2 c = p + vPos * r, z = c;' +
- *   '  float n = 0.0;' +
- *   '  for (int i = I; i > 0; i --) {' +
- *   '    if(z.x*z.x+z.y*z.y > 4.0) {' +
- *   '      n = float(i)/float(I);' +
- *   '      break;' +
- *   '    }' +
- *   '    z = vec2(z.x*z.x-z.y*z.y, 2.0*z.x*z.y) + c;' +
- *   '  }' +
- *   '  gl_FragColor = vec4(0.5-cos(n*17.0)/2.0,0.5-cos(n*13.0)/2.0,0.5-cos(n*23.0)/2.0,1.0);' +
- *   '}';
+ * let fs = `
+ *    precision highp float;
+ *    uniform vec2 p; 
+ *    uniform float r; 
+ *    const int I = 500; 
+ *    void main() { 
+ *      vec2 c = p + gl_FragCoord.xy * r, z = c; 
+ *      float n = 0.0; 
+ *      for (int i = I; i > 0; i --) { 
+ *        if(z.x*z.x+z.y*z.y > 4.0) { 
+ *          n = float(i)/float(I); 
+ *          break; 
+ *        } 
+ *        z = vec2(z.x*z.x-z.y*z.y, 2.0*z.x*z.y) + c; 
+ *      } 
+ *      gl_FragColor = vec4(0.5-cos(n*17.0)/2.0,0.5-cos(n*13.0)/2.0,0.5-cos(n*23.0)/2.0,1.0); 
+ *    }`;
  *
  * let mandel;
  * function setup() {
