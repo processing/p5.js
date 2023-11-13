25ce601cdebf2d6426329046bf3fa5f2267e7507
Jaivignesh-afk <23f1001347@ds.study.iitm.ac.in>
2023-11-13 18:28:48 +0530

    Refactored argument name for createGraphics

--- src/core/rendering.js
+++ src/core/rendering.js
@@ -291,11 +291,11 @@ p5.prototype.noCanvas = function() {
  * @return {p5.Graphics} offscreen graphics buffer
  */
 p5.prototype.createGraphics = function(w, h, renderer, canvas) {
-  if (arguments[2] instanceof HTMLCanvasElement) {
+  if (args[2] instanceof HTMLCanvasElement) {
     renderer = constants.P2D;
-    canvas = arguments[2];
+    canvas = args[2];
   }
-  p5._validateParameters('createGraphics', arguments);
+  p5._validateParameters('createGraphics', ...args);
   return new p5.Graphics(w, h, renderer, this, canvas);
 };
 
