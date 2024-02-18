31fdc5f6ad67ed66d17de9cd86975958ed9c6a60
Garima <garima19802003@gmail.com>
2024-02-18 22:17:09 +0530

    Added unit test for calculateBoundingBox()

--- test/unit/webgl/p5.Geometry.js
+++ test/unit/webgl/p5.Geometry.js
@@ -92,6 +92,20 @@ suite('p5.Geometry', function() {
       assert.equal(geom._addJoin.callCount, 4);
     });
 
+    test('calculateBoundingBox()', function() {
+      geom.vertices.push(
+        myp5.createVector(0, 0, 0),
+        myp5.createVector(10, 20, 30),
+        myp5.createVector(-5, 15, 25)
+      );
+      geom.calculateBoundingBox();
+      assert.deepEqual(geom.boundingBox.min.array(), [-5, 0, 0]);
+      assert.deepEqual(geom.boundingBox.max.array(), [10, 20, 30]);
+      assert.deepEqual(geom.boundingBox.size.array(), [15, 20, 30]);
+      assert.deepEqual(geom.boundingBox.offset.array(), [2.5, 10, 15]);
+    });
+
+
     test('degenerate edge in the middle', function() {
       geom.vertices.push(
         myp5.createVector(0, 0),
