1e08d4ec96362bbf7f5d49f49a8b4244f12f6492
dhowe <dhowe@cims.nyu.edu>
2024-10-12 18:58:55 +0100

    reorg for registerAddon

---
+++ src/type/text2d.js
@@ -0,0 +1,484 @@
+import * as constants from '../core/constants';
+
+/**
+ * @module Type
+ * @submodule Rendering
+ * @for p5
+ * @requires core
+ */
+
+function text2d(p5, fn) {
+
+  p5.Renderer2D.textFunctions = [
+    'text',
+    'textAlign',
+    'textAscent',
+    'textBounds',
+    'textDescent',
+    'textLeading',
+    'textFont',
+    'textSize',
+    'textStyle',
+    'textWidth',
+    'textWrap',
+  ];
+
+  // textWeight: 400, // ADDED DCH
+  // textVariant: 'normal', // ADDED DCH
+  //  // textStretch, lineHeight ?
+
+  //////////////////////// start API ////////////////////////
+
+  p5.Renderer2D.prototype.textAlign = function (h, v) {
+    if (typeof h !== 'undefined') {
+      this.states.textAlign = h;
+      if (typeof v !== 'undefined') {
+        this.states.textBaseline = v;
+      }
+      return this._applyTextProperties();
+    } else {
+      return {
+        horizontal: this.states.textAlign,
+        vertical: this.states.textBaseline
+      };
+    }
+  };
+
+  p5.Renderer2D.prototype.textAscent = function () {
+    const ctx = this.drawingContext;
+    // do we need to update this.states.textAscent here? prob no
+    return ctx.measureText('').fontBoundingBoxAscent;
+  };
+
+  p5.Renderer2D.prototype.textBounds = function (s, x = 0, y = 0) {
+    let metrics = this.drawingContext.measureText(s);
+    let w = metrics.actualBoundingBoxLeft
+      + metrics.actualBoundingBoxRight;
+    let h = metrics.actualBoundingBoxAscent
+      + Math.abs(metrics.actualBoundingBoxDescent);
+    return { x, y: y + metrics.actualBoundingBoxDescent, w, h };
+  };
+
+  p5.Renderer2D.prototype.textDescent = function () {
+    const ctx = this.drawingContext;
+    return Math.abs(ctx.measureText('').fontBoundingBoxDescent);
+  };
+
+  p5.Renderer2D.prototype.textFont = function (theFont, theSize, theWeight, theStyle, theVariant) {
+
+    if (arguments.length) {
+
+      if (theFont instanceof FontFace) {
+        theFont = theFont.family;
+      }
+      if (typeof theFont !== 'string') {
+        throw new Error('null font passed to textFont');
+      }
+      if (typeof theSize === 'string') { // defaults to px
+        // TODO: handle other units and possibly convert to px
+        theSize = Number.parseFloat(theSize);
+      }
+
+      // font props to be added to P5.Renderer.states (also textStretch, lineHeight?)
+      if (typeof this.states.textWeight === 'undefined') {
+        this.states.textWeight = 400;
+      }
+      if (typeof this.states.textWeight === 'undefined') {
+        this.states.textVariant = 'normal';
+      }
+
+      this.states.textFont = theFont;
+
+      if (typeof theSize !== 'undefined') {
+        this.states.textSize = theSize;
+      }
+      if (typeof theWeight !== 'undefined') {
+        this.states.textWeight = theWeight;
+      }
+      if (typeof theStyle !== 'undefined') {
+        this.states.textStyle = theStyle;
+      }
+      if (typeof theVariant !== 'undefined') {
+        this.states.textVariant = theVariant;
+      }
+
+      return this._applyTextProperties();
+    }
+
+    return this._buildFontString(states);
+  }
+
+  p5.Renderer2D.prototype.textLeading = function (leading) {
+    if (typeof leading === 'number') {
+      this.states.leadingSet = true;
+      this.states.textLeading = leading;
+      return this._pInst;
+    }
+    return this.states.textLeading;
+  }
+
+  p5.Renderer2D.prototype.textSize = function (theSize) {
+    if (typeof theSize !== 'undefined') {
+      this.states.textSize = Number.parseFloat(theSize);
+
+      // handle leading here, if not set otherwise 
+      if (!this.states.leadingSet) {
+        this.states.textLeading = this.states.textSize * 1.275;
+      }
+
+      return this._applyTextProperties();
+    }
+    return this.states.textSize;
+  };
+
+  p5.Renderer2D.prototype.textStyle = function (s) {
+    if (s) {
+      if (
+        s === constants.NORMAL ||
+        s === constants.ITALIC ||
+        s === constants.BOLD ||
+        s === constants.BOLDITALIC
+      ) {
+        this.states.textStyle = s;
+      }
+      return this._applyTextProperties();
+    }
+
+    return this.states.textStyle;
+  }
+
+  p5.Renderer2D.prototype.textWidth = function (theText) {
+    if (theText.length === 0) return 0;
+
+    // Only use the line with the longest width, and replace tabs with double-space
+    const lines = theText.replace(tabsRE, '  ').split(linebreakRE);
+
+    // Get the textWidth for every line
+    const newArr = lines.map(this._lineWidth);
+
+    // Return the largest textWidth
+    return Math.max(...newArr);
+  };
+
+
+  p5.Renderer2D.prototype.textWrap = function (wrapStyle) {
+    this.states.textWrap = wrapStyle;
+    return this.states.textWrap;
+  };
+
+  // this is a mess
+  p5.Renderer2D.prototype.text = function (str, x, y, maxWidth, maxHeight) {
+    let baselineHacked;
+
+    // baselineHacked: (HACK)
+    // A temporary fix to conform to Processing's implementation
+    // of BASELINE vertical alignment in a bounding box
+
+    if (typeof maxWidth !== 'undefined') {
+      if (this.drawingContext.textBaseline === constants.BASELINE) {
+        baselineHacked = true;
+        this.drawingContext.textBaseline = constants.TOP;
+      }
+    }
+
+    //const p = Renderer.prototype.text.apply(this, arguments);
+    let lines, line, testLine, testWidth;
+    let words, chars, shiftedY;
+    let finalMaxHeight = Number.MAX_VALUE;
+
+    // fix for #5785 (top of bounding box)
+    let finalMinHeight = y;
+
+    if (!(this.states.doFill || this.states.doStroke)) {
+      return;
+    }
+
+    if (typeof str === 'undefined') {
+      return;
+    } else if (typeof str !== 'string') {
+      str = str.toString();
+    }
+
+    // Replaces tabs with double-spaces and splits string on any line
+    // breaks present in the original string
+    str = str.replace(/(\t)/g, '  ');
+    lines = str.split('\n');
+
+    if (typeof maxWidth !== 'undefined') {
+      if (this.states.rectMode === constants.CENTER) {
+        x -= maxWidth / 2;
+      }
+
+      switch (this.states.textAlign) {
+        case constants.CENTER:
+          x += maxWidth / 2;
+          break;
+        case constants.RIGHT:
+          x += maxWidth;
+          break;
+      }
+
+      if (typeof maxHeight !== 'undefined') {
+        if (this.states.rectMode === constants.CENTER) {
+          y -= maxHeight / 2;
+          finalMinHeight -= maxHeight / 2;
+        }
+
+        let originalY = y;
+        let ascent = p.textAscent();
+
+        switch (this.states.textBaseline) {
+          case constants.BOTTOM:
+            shiftedY = y + maxHeight;
+            y = Math.max(shiftedY, y);
+            // fix for #5785 (top of bounding box)
+            finalMinHeight += ascent;
+            break;
+          case constants.CENTER:
+            shiftedY = y + maxHeight / 2;
+            y = Math.max(shiftedY, y);
+            // fix for #5785 (top of bounding box)
+            finalMinHeight += ascent / 2;
+            break;
+        }
+
+        // remember the max-allowed y-position for any line (fix to #928)
+        finalMaxHeight = y + maxHeight - ascent;
+
+        // fix for #5785 (bottom of bounding box)
+        if (this.states.textBaseline === constants.CENTER) {
+          finalMaxHeight = originalY + maxHeight - ascent / 2;
+        }
+      } else {
+        // no text-height specified, show warning for BOTTOM / CENTER
+        if (this.states.textBaseline === constants.BOTTOM ||
+          this.states.textBaseline === constants.CENTER) {
+          // use rectHeight as an approximation for text height
+          let rectHeight = p.textSize() * this.states.textLeading;
+          finalMinHeight = y - rectHeight / 2;
+          finalMaxHeight = y + rectHeight / 2;
+        }
+      }
+
+      // Render lines of text according to settings of textWrap
+      // Splits lines at spaces, for loop adds one word + space
+      // at a time and tests length with next word added
+      if (this.states.textWrap === constants.WORD) {
+        let nlines = [];
+        for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+          line = '';
+          words = lines[lineIndex].split(' ');
+          for (let wordIndex = 0; wordIndex < words.length; wordIndex++) {
+            testLine = `${line + words[wordIndex]}` + ' ';
+            testWidth = this.textWidth(testLine);
+            if (testWidth > maxWidth && line.length > 0) {
+              nlines.push(line);
+              line = `${words[wordIndex]}` + ' ';
+            } else {
+              line = testLine;
+            }
+          }
+          nlines.push(line);
+        }
+
+        let offset = 0;
+        if (this.states.textBaseline === constants.CENTER) {
+          offset = (nlines.length - 1) * p.textLeading() / 2;
+        } else if (this.states.textBaseline === constants.BOTTOM) {
+          offset = (nlines.length - 1) * p.textLeading();
+        }
+
+        for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+          line = '';
+          words = lines[lineIndex].split(' ');
+          for (let wordIndex = 0; wordIndex < words.length; wordIndex++) {
+            testLine = `${line + words[wordIndex]}` + ' ';
+            testWidth = this.textWidth(testLine);
+            if (testWidth > maxWidth && line.length > 0) {
+              this._renderText(
+                p,
+                line.trim(),
+                x,
+                y - offset,
+                finalMaxHeight,
+                finalMinHeight
+              );
+              line = `${words[wordIndex]}` + ' ';
+              y += p.textLeading();
+            } else {
+              line = testLine;
+            }
+          }
+          this._renderText(
+            //p,
+            line.trim(),
+            x,
+            y - offset,
+            finalMaxHeight,
+            finalMinHeight
+          );
+          y += p.textLeading();
+        }
+      } else {
+        let nlines = [];
+        for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+          line = '';
+          chars = lines[lineIndex].split('');
+          for (let charIndex = 0; charIndex < chars.length; charIndex++) {
+            testLine = `${line + chars[charIndex]}`;
+            testWidth = this.textWidth(testLine);
+            if (testWidth <= maxWidth) {
+              line += chars[charIndex];
+            } else if (testWidth > maxWidth && line.length > 0) {
+              nlines.push(line);
+              line = `${chars[charIndex]}`;
+            }
+          }
+        }
+
+        nlines.push(line);
+        let offset = 0;
+        if (this.states.textBaseline === constants.CENTER) {
+          offset = (nlines.length - 1) * p.textLeading() / 2;
+        } else if (this.states.textBaseline === constants.BOTTOM) {
+          offset = (nlines.length - 1) * p.textLeading();
+        }
+
+        // Splits lines at characters, for loop adds one char at a time
+        // and tests length with next char added
+        for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+          line = '';
+          chars = lines[lineIndex].split('');
+          for (let charIndex = 0; charIndex < chars.length; charIndex++) {
+            testLine = `${line + chars[charIndex]}`;
+            testWidth = this.textWidth(testLine);
+            if (testWidth <= maxWidth) {
+              line += chars[charIndex];
+            } else if (testWidth > maxWidth && line.length > 0) {
+              this._renderText(
+                //p,
+                line.trim(),
+                x,
+                y - offset,
+                finalMaxHeight,
+                finalMinHeight
+              );
+              y += p.textLeading();
+              line = `${chars[charIndex]}`;
+            }
+          }
+        }
+        this._renderText(
+          //p,
+          line.trim(),
+          x,
+          y - offset,
+          finalMaxHeight,
+          finalMinHeight
+        );
+        y += p.textLeading();
+      }
+    } else {
+      // Offset to account for vertically centering multiple lines of text - no
+      // need to adjust anything for vertical align top or baseline
+      let offset = 0;
+      if (this.states.textBaseline === constants.CENTER) {
+        offset = (lines.length - 1) * p.textLeading() / 2;
+      } else if (this.states.textBaseline === constants.BOTTOM) {
+        offset = (lines.length - 1) * p.textLeading();
+      }
+
+      // Renders lines of text at any line breaks present in the original string
+      for (let i = 0; i < lines.length; i++) {
+        this._renderText(
+          p,
+          lines[i],
+          x,
+          y - offset,
+          finalMaxHeight,
+          finalMinHeight - offset
+        );
+        y += p.textLeading();
+      }
+    }
+
+    // end HACK
+    if (baselineHacked) {
+      this.drawingContext.textBaseline = constants.BASELINE;
+    }
+
+    return this._pInst;
+  };
+  //////////////////////// end API ////////////////////////
+
+  p5.Renderer2D.prototype._buildFontString =function (states) {
+    let { textStyle, textVariant, textWeight, textSize, textFont } = this.states;
+    let style = (textStyle === 'normal') ? '' : textStyle;
+    let weight = (textWeight === 'normal' || textWeight === 400) ? '' : textWeight;
+    let variant = (textVariant === 'normal') ? '' : textVariant;
+    let css = `${style} ${weight} ${variant} ${textSize}px ${textFont}`;
+    return css;
+  }
+
+  p5.Renderer2D.prototype._applyTextProperties = function () {
+    let { drawingContext, states } = this;
+    drawingContext.font = this._buildFontString(states);
+    drawingContext.textAlign = states.textAlign;
+    if (states.textBaseline === constants.CENTER) {
+      drawingContext.textBaseline = constants._CTX_MIDDLE;
+    } else {
+      drawingContext.textBaseline = states.textBaseline;
+    }
+    return this._pInst;
+  };
+
+  p5.Renderer2D.prototype._lineWidth = function (s) {
+    let metrics = this.drawingContext.measureText(s);
+    // this should be more accurate than width
+    return Math.abs(metrics.actualBoundingBoxLeft)
+      + Math.abs(metrics.actualBoundingBoxRight);
+  };
+
+
+  // text() calls this method to render text
+  p5.Renderer2D.prototype._renderText = function (p, line, x, y, maxY, minY) {
+    // TODO: replace p arg with this._pInst
+    let { drawingContext, states } = this;
+
+    if (y < minY || y >= maxY) {
+      return; // don't render lines beyond minY/maxY
+    }
+
+    p.push(); // fix to #803
+
+    // no stroke unless specified by user
+    if (states.doStroke && states.strokeSet) {
+      drawingContext.strokeText(line, x, y);
+    }
+
+    if (!this._clipping && states.doFill) {
+
+      // if fill hasn't been set by user, use default text fill
+      if (!states.fillSet) {
+        this._setFill(constants._DEFAULT_TEXT_FILL);
+      }
+
+      drawingContext.fillText(line, x, y);
+    }
+    p.pop();
+
+    return p;
+  };
+
+  rendererFuncs.forEach(func => { // tmp-check
+    if (typeof p5.Renderer2D.prototype[func] !== 'function') {
+      throw Error(`p5.Renderer2D.prototype.${func} is not a function`);
+    }
+  });
+}
+
+export default text2d;
+
+// if (typeof p5 !== 'undefined') {
+//   text2d(p5, p5.prototype);
+// }
---
+++ src/type/textApi.js
@@ -0,0 +1,106 @@
+
+/**
+ * @module Type
+ * @submodule p5Api
+ * @for p5
+ * @requires core
+ */
+
+function textApi(p5, fn) {
+
+  const rendererFuncs = [
+    'text',
+    'textAlign',
+    'textAscent',
+    'textBounds',
+    'textDescent',
+    'textLeading',
+    'textFont',
+    'textSize',
+    'textStyle',
+    'textWidth',
+    'textWrap',
+  ];
+
+  const validFontTypes = ['ttf', 'otf', 'woff', 'woff2'];
+  const validFontTypesRE = new RegExp(`\.(${validFontTypes.join('|')})$`, 'i');
+  const invalidFontError = 'Sorry, a string path is invalid. Only TTF, OTF, WOFF and WOFF2 font files are supported.';
+
+  //////////////////////////// API ////////////////////////////
+
+  // attach text functions to p5 prototype, 
+  // each delegating to the current renderer
+  p5.Renderer2D.textFunctions.forEach(func => {
+    fn[func] = function (...args) {
+      p5._validateParameters(func, args);
+      return this._renderer[func](...args);
+    };
+  });
+
+  fn.loadFont = async function (...args/*path, name, onSuccess, onError*/) {  // no renderer
+
+    let name, path = args.shift();
+    if (typeof path !== 'string' || path.length === 0) {
+      p5._friendlyError(invalidFontError, 'p5.loadFont'); // ?
+    }
+    if (typeof args[1] === 'string') {
+      name = args.shift();
+    }
+    else if (validFontTypesRE.test(path)) {
+      name = path.split('/').pop().split('.').shift();
+    }
+    else {
+      p5._friendlyError(invalidFontError, 'p5.loadFont'); // ?
+    }
+
+    let callback, errorCallback, options;
+
+    // check for callbacks
+    for (let i = 0; i < args.length; i++) {
+      const arg = args[i];
+      if (typeof arg === 'function') {
+        if (!callback) {
+          callback = arg;
+        } else {
+          errorCallback = arg;
+        }
+      }
+      else if (typeof arg === 'object') {
+        options = arg;
+      }
+    }
+
+    const fontFile = new FontFace(name, `url(${path})`, options);
+    document.fonts.add(fontFile);
+
+    return await new Promise(resolve =>
+      fontFile.load().then(() => {
+        if (typeof callback !== 'undefined') {
+          callback(fontFile);
+        }
+        resolve(fontFile)
+      },
+        err => {
+          p5._friendlyFileLoadError(4, path); // ?
+          if (errorCallback) {
+            errorCallback(err);
+          } else {
+            throw err;
+          }
+        }
+      ));
+  };
+
+  ['loadFont', ...rendererFuncs].forEach(f => { // tmp-check
+    if (typeof fn[f] !== 'function') {
+      throw new Error(`p5.prototype.${f} is not a function`);
+    }
+  });
+
+}
+
+export default textApi;
+
+// if (typeof p5 !== 'undefined') {
+//   textApi(p5, p5.prototype);
+// }
