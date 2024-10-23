6550134efb4141acd3876459e5f85b70f5180a9c
Silas Morgan <Victorsmorgan@gmail.com>
2024-10-23 17:58:30 -0400

    Update src/dom/index.js
    Fixed indentation on affected lines.
    
    Co-authored-by: Kenneth Lim <hello@limzykenneth.com>

--- src/dom/index.js
+++ src/dom/index.js
@@ -4,8 +4,8 @@ import media from './media_element';
 import file from './p5.File';
 
 export default function(p5){
-    p5.registerAddon(dom);
-    p5.registerAddon(element);
-    p5.registerAddon(media);
-    p5.registerAddon(file);
-  }
+  p5.registerAddon(dom);
+  p5.registerAddon(element);
+  p5.registerAddon(media);
+  p5.registerAddon(file);
+}
