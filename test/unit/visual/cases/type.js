633c9300baca85b430d8aebfca3521223d155c7a
dhowe <dhowe@cims.nyu.edu>
2024-11-04 21:00:50 +0000

    reorg visual test dirs

--- test/unit/visual/cases/type.js
+++ test/unit/visual/cases/type.js
@@ -1,22 +1,24 @@
 import { visualSuite, visualTest } from '../visualTest';
 
-visualSuite('type', function() {
-  visualSuite('textFont', function() {
-    visualTest('with the default font', function (p5, screenshot) {
-      p5.createCanvas(50, 50);
-      p5.textSize(20);
-      p5.textAlign(p5.LEFT, p5.TOP);
-      p5.text('test', 0, 0);
-      screenshot();
-    });
+visualSuite('type', function () {
+  visualSuite('v1', function () {
+    visualSuite('textFont', function () {
+      visualTest('with the default font', function (p5, screenshot) {
+        p5.createCanvas(50, 50);
+        p5.textSize(20);
+        p5.textAlign(p5.LEFT, p5.TOP);
+        p5.text('test', 0, 0);
+        screenshot();
+      });
 
-    visualTest('with the default monospace font', function (p5, screenshot) {
-      p5.createCanvas(50, 50);
-      p5.textSize(20);
-      p5.textFont('monospace');
-      p5.textAlign(p5.LEFT, p5.TOP);
-      p5.text('test', 0, 0);
-      screenshot();
+      visualTest('with the default monospace font', function (p5, screenshot) {
+        p5.createCanvas(50, 50);
+        p5.textSize(20);
+        p5.textFont('monospace');
+        p5.textAlign(p5.LEFT, p5.TOP);
+        p5.text('test', 0, 0);
+        screenshot();
+      });
     });
   });
 });
