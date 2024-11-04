6625640d90a3c6a10c24fbdc8cf6c5cb1c77039b
koundinya <k.dhulipalla@arts.ac.uk>
2024-11-04 19:24:11 +0000

    Revert "v1/v2 unit visual tests: on hold"
    This reverts commit 12118158ea96fbc920509d84bd87b20dff64f68e.

--- test/unit/spec.js
+++ test/unit/spec.js
@@ -1,61 +1,60 @@
 var spec = {
-  accessibility: ["describe", "outputs"],
-  color: ["color_conversion", "creating_reading", "p5.Color", "setting"],
+  accessibility: ['describe', 'outputs'],
+  color: ['color_conversion', 'creating_reading', 'p5.Color', 'setting'],
   core: [
-    "2d_primitives",
-    "attributes",
-    "curves",
-    "environment",
-    "error_helpers",
-    "main",
-    "p5.Element",
-    "p5.Graphics",
-    "param_errors",
-    "preload",
-    "rendering",
-    "structure",
-    "transform",
-    "version",
-    "vertex",
+    '2d_primitives',
+    'attributes',
+    'curves',
+    'environment',
+    'error_helpers',
+    'main',
+    'p5.Element',
+    'p5.Graphics',
+    'param_errors',
+    'preload',
+    'rendering',
+    'structure',
+    'transform',
+    'version',
+    'vertex'
   ],
-  data: ["p5.TypedDict", "local_storage"],
-  dom: ["dom"],
-  events: ["keyboard", "mouse", "touch", "acceleration"],
-  image: ["p5.Image", "loading", "pixels", "filters", "downloading"],
+  data: ['p5.TypedDict', 'local_storage'],
+  dom: ['dom'],
+  events: ['keyboard', 'mouse', 'touch', 'acceleration'],
+  image: ['p5.Image', 'loading', 'pixels', 'filters', 'downloading'],
   io: [
-    "files",
-    "loadBytes",
-    "loadStrings",
-    "loadXML",
-    "loadJSON",
-    "loadTable",
-    "saveTable",
-    "loadImage",
-    "loadModel",
-    "loadShader",
+    'files',
+    'loadBytes',
+    'loadStrings',
+    'loadXML',
+    'loadJSON',
+    'loadTable',
+    'saveTable',
+    'loadImage',
+    'loadModel',
+    'loadShader'
   ],
-  math: ["calculation", "noise", "p5.Vector", "random", "trigonometry"],
-  typography: ["attributes", "loadFont", "p5.Font"],
-  utilities: ["array_functions", "conversion", "string_functions", "time_date"],
+  math: ['calculation', 'noise', 'p5.Vector', 'random', 'trigonometry'],
+  typography: ['attributes', 'loadFont', 'p5.Font'],
+  utilities: ['array_functions', 'conversion', 'string_functions', 'time_date'],
   webgl: [
-    "3d_primitives",
-    "interaction",
-    "p5.Matrix",
-    "p5.Camera",
-    "p5.Framebuffer",
-    "p5.Geometry",
-    "p5.RendererGL",
-    "p5.Shader",
-    "p5.Texture",
-    "light",
+    '3d_primitives',
+    'interaction',
+    'p5.Matrix',
+    'p5.Camera',
+    'p5.Framebuffer',
+    'p5.Geometry',
+    'p5.RendererGL',
+    'p5.Shader',
+    'p5.Texture',
+    'light'
   ],
-  "visual/cases": [
+  'visual/cases': [
     // Add the visual tests that you want run as part of CI here. Feel free
     // to omit some for speed if they should only be run manually.
-    "webgl",
-    // "typography",
-    "typography.v1",
-  ],
+    'webgl',
+    'typography'
+  ]
 };
 document.write(
   '<script src="unit/visual/visualTest.js" type="text/javascript"></script>'
@@ -65,10 +64,10 @@ Object.keys(spec).map(function (folder) {
     var string = [
       '<script src="unit/',
       folder,
-      "/",
+      '/',
       file,
-      '.js" type="text/javascript" ></script>',
+      '.js" type="text/javascript" ></script>'
     ];
-    document.write(string.join(""));
+    document.write(string.join(''));
   });
 });
