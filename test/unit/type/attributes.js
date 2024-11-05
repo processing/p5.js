cd753a493af417c93bbb14875f37aa2a962e7cfc
dhowe <dhowe@cims.nyu.edu>
2024-11-05 17:37:44 +0000

    update unit tests

--- test/unit/type/attributes.js
+++ test/unit/type/attributes.js
@@ -1,91 +1,77 @@
 import p5 from '../../../src/app.js';
 
-suite('Typography Attributes', function () {
-  let myp5;
+suite('Typography Attributes', function() {
+  var myp5;
 
-  beforeAll(function () {
-    new p5(function (p) {
-      p.setup = function () {
-        myp5 = p;
-      };
+  beforeEach(function () {
+    myp5 = new p5(function (p) {
+      p.setup = function () { };
+      p.draw = function () { };
     });
   });
 
-  afterAll(function () {
+  afterEach(function () {
     myp5.remove();
   });
 
-  suite('p5.prototype.textFont', function () {
-    test('sets the current font as Georgia', function () {
-      myp5.textFont('Georgia');
-      assert.strictEqual(myp5.textFont(), 'Georgia');
-    });
-
-    test('sets the current font as Helvetica', function () {
-      myp5.textFont('Helvetica');
-      assert.strictEqual(myp5.textFont(), 'Helvetica');
-    });
-
-    test('sets the current font and text size', function () {
-      myp5.textFont('Courier New', 24);
-      assert.strictEqual(myp5.textFont(), 'Courier New');
-      assert.strictEqual(myp5.textSize(), 24);
-    });
-  });
-  
-  suite('p5.prototype.textLeading', function () {
-    test('sets and gets the spacing value', function () {
+  suite('textLeading', function() {
+    test('sets and gets the leading value', function() {
       myp5.textLeading(20);
       assert.strictEqual(myp5.textLeading(), 20);
     });
-    test('should work for negative spacing value', function () {
+    test('should work for negative leadings', function() {
       myp5.textLeading(-20);
       assert.strictEqual(myp5.textLeading(), -20);
     });
   });
 
-  suite('p5.prototype.textSize', function () {
-    test('sets and gets the font size', function () {
+  suite('textSize', function() {
+    test('sets and gets the text size', function() {
       myp5.textSize(24);
       assert.strictEqual(myp5.textSize(), 24);
     });
   });
 
-  suite('p5.prototype.textStyle', function () {
-    test('sets and gets the font style', function () {
+  suite('textStyle', function() {
+    test('sets and gets the text style', function() {
       myp5.textStyle(myp5.ITALIC);
       assert.strictEqual(myp5.textStyle(), myp5.ITALIC);
     });
   });
 
-  suite('p5.prototype.textWidth', function () {
-    test('should return a number for char input', function () {
+  suite('textWidth', function() {
+    test('should return a number for char input', function() {
       assert.isNumber(myp5.textWidth('P'));
     });
     test('should return a number for string input.', function () {
       assert.isNumber(myp5.textWidth('p5.js'));
     });
     // Either should not throw error
-    test('should return a number for number input', function () {
-      assert.isNumber(myp5.textWidth('p5.js'));
+    test('should return a number for number input', function() {
+      assert.isNumber(myp5.textWidth(100));
     });
   });
 
-  suite('p5.prototype.textAscent', function () {
-    test('should return a number', function () {
+  suite('textAscent', function() {
+    test('should return a number', function() {
       assert.isNumber(myp5.textAscent());
     });
   });
 
-  suite('p5.prototype.textDescent', function () {
-    test('should return a number', function () {
+  suite('textDescent', function() {
+    test('should return a number', function() {
       assert.isNumber(myp5.textDescent());
     });
   });
 
-  suite('p5.prototype.textWrap', function () {
-    test('returns textWrap text attribute', function () {
+  suite('textWrap', function() {
+    test('gets the default text wrap attribute', function() {
       assert.strictEqual(myp5.textWrap(), myp5.WORD);
     });
+    test('sets and gets the text wrap value', function() {
+      myp5.textWrap(myp5.CHAR);
+      assert.strictEqual(myp5.textWrap(), myp5.CHAR);
+    });
+
   });
 });
---
+++ test/unit/type/loading.js
@@ -0,0 +1,44 @@
+import p5 from '../../../src/app.js';
+
+suite('Loading Fonts', function () {
+  var myp5;
+
+  beforeEach(function () {
+    myp5 = new p5(function (p) {
+      p.setup = function () { };
+      p.draw = function () { };
+    });
+  });
+
+  afterEach(function () {
+    myp5.remove();
+  });
+
+  // tests ////////////////////////////////////////////////
+  const fontFile = '/unit/assets/acmesa.ttf';
+
+  test('loadFont.await', async () => {
+    const pFont = await myp5.loadFont(fontFile);
+    assert.ok(pFont, 'acmesa.ttf loaded');
+    assert.equal(pFont.name, 'acmesa');
+    assert.isTrue(pFont instanceof p5.Font);
+  });
+  
+  test('loadFont.then', () => {
+    myp5.loadFont(fontFile).then(pFont => {
+      assert.ok(pFont, 'acmesa.ttf loaded');
+      assert.equal(pFont.name, 'acmesa');
+      assert.isTrue(pFont instanceof p5.Font);
+    });
+  });
+
+  test('loadFont.callback', () => {
+    myp5.loadFont(fontFile, (pFont) => {
+      
+      assert.ok(pFont, 'acmesa.ttf loaded');
+      assert.equal(pFont.name, 'acmesa');
+      assert.isTrue(pFont instanceof p5.Font);
+    });
+  });
+
+});
