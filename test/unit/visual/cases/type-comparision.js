870b774429a7f425bfe0edf0108148249ee9f303
koundinya <k.dhulipalla@arts.ac.uk>
2024-11-11 08:39:48 +0000

    add type-comparison

---
+++ test/unit/visual/cases/type-comparision.js
@@ -0,0 +1,54 @@
+import { visualSuite, visualTest } from "../visualTest";
+import { server } from "@vitest/browser/context";
+const { readFile, readdir } = server.commands;
+
+const combinedTests = {
+  textFont: ["with the default font", "with the default monospace font"],
+  textAlign: [
+    "all alignments with single word",
+    "all alignments with single line",
+    "all alignments with multi-lines and wrap word",
+    "all alignments with multi-lines and wrap char",
+    "all alignments with multi-line manual text",
+  ],
+  textStyle: ["all text styles"],
+  textSize: ["text sizes comparison"],
+  textLeading: ["text leading with different values"],
+  textWidth: ["verify width of a string"],
+};
+
+visualSuite("Type Comparison", function () {
+  // Compare matching suites
+  Object.keys(combinedTests).forEach((suite) => {
+    visualSuite(suite, function () {
+      // Compare matching tests within suite
+      combinedTests[suite].forEach((testName) => {
+        visualTest(`compare ${testName}`, async function (p5, screenshot) {
+          try {
+            const v1Path = `/unit/visual/screenshots/Type.v1/${suite}/${testName}/000.png`;
+            const v2Path = `/unit/visual/screenshots/Type.v2/${suite}/${testName}/000.png`;
+
+            const v1Image = await p5.loadImage(v1Path);
+            const v2Image = await p5.loadImage(v2Path);
+
+            const totalWidth = v1Image.width + v2Image.width + 1;
+            const maxHeight = Math.max(v1Image.height, v2Image.height);
+
+            p5.createCanvas(totalWidth, maxHeight);
+            p5.background(255);
+
+            p5.image(v1Image, 0, 0);
+            p5.stroke("red");
+            p5.line(v1Image.width, 0, v1Image.width, maxHeight);
+            p5.image(v2Image, v1Image.width + 1, 0);
+
+            screenshot();
+          } catch (err) {
+            console.error(`Error comparing ${suite}/${testName}:`, err);
+            throw err;
+          }
+        });
+      });
+    });
+  });
+});
--- test/unit/visual/cases/type.v1.js
+++ test/unit/visual/cases/type.v1.js
@@ -15,7 +15,7 @@ visualSuite("Type.v1", function () {
       screenshot();
     });
 
-    visualTest("With the default monospace font", function (p5, screenshot) {
+    visualTest("with the default monospace font", function (p5, screenshot) {
       p5.createCanvas(50, 50);
       p5.textSize(20);
       p5.textFont("monospace");
@@ -25,235 +25,235 @@ visualSuite("Type.v1", function () {
     });
   });
 
-  visualSuite("textAlign", function () {
-    visualTest("all alignments with single word", function (p5, screenshot) {
-      const alignments = [
-        { alignX: p5.LEFT, alignY: p5.TOP },
-        { alignX: p5.CENTER, alignY: p5.TOP },
-        { alignX: p5.RIGHT, alignY: p5.TOP },
-        { alignX: p5.LEFT, alignY: p5.CENTER },
-        { alignX: p5.CENTER, alignY: p5.CENTER },
-        { alignX: p5.RIGHT, alignY: p5.CENTER },
-        { alignX: p5.LEFT, alignY: p5.BOTTOM },
-        { alignX: p5.CENTER, alignY: p5.BOTTOM },
-        { alignX: p5.RIGHT, alignY: p5.BOTTOM },
-      ];
+  //   visualSuite("textAlign", function () {
+  //     visualTest("all alignments with single word", function (p5, screenshot) {
+  //       const alignments = [
+  //         { alignX: p5.LEFT, alignY: p5.TOP },
+  //         { alignX: p5.CENTER, alignY: p5.TOP },
+  //         { alignX: p5.RIGHT, alignY: p5.TOP },
+  //         { alignX: p5.LEFT, alignY: p5.CENTER },
+  //         { alignX: p5.CENTER, alignY: p5.CENTER },
+  //         { alignX: p5.RIGHT, alignY: p5.CENTER },
+  //         { alignX: p5.LEFT, alignY: p5.BOTTOM },
+  //         { alignX: p5.CENTER, alignY: p5.BOTTOM },
+  //         { alignX: p5.RIGHT, alignY: p5.BOTTOM },
+  //       ];
 
-      p5.createCanvas(300, 200);
-      p5.textSize(20);
-      alignments.forEach((alignment) => {
-        p5.textAlign(alignment.alignX, alignment.alignY);
-        p5.text("Word", 0, 0);
-        const bb = p5.textBounds("Word", 0, 0);
-        p5.noFill();
-        p5.stroke("red");
-        p5.rect(bb.x, bb.y, bb.w, bb.h);
-      });
-      screenshot();
-    });
+  //       p5.createCanvas(300, 200);
+  //       p5.textSize(20);
+  //       alignments.forEach((alignment) => {
+  //         p5.textAlign(alignment.alignX, alignment.alignY);
+  //         p5.text("Word", 0, 0);
+  //         const bb = p5.textBounds("Word", 0, 0);
+  //         p5.noFill();
+  //         p5.stroke("red");
+  //         p5.rect(bb.x, bb.y, bb.w, bb.h);
+  //       });
+  //       screenshot();
+  //     });
 
-    visualTest("all alignments with single line", function (p5, screenshot) {
-      const alignments = [
-        { alignX: p5.LEFT, alignY: p5.TOP },
-        { alignX: p5.CENTER, alignY: p5.TOP },
-        { alignX: p5.RIGHT, alignY: p5.TOP },
-        { alignX: p5.LEFT, alignY: p5.CENTER },
-        { alignX: p5.CENTER, alignY: p5.CENTER },
-        { alignX: p5.RIGHT, alignY: p5.CENTER },
-        { alignX: p5.LEFT, alignY: p5.BOTTOM },
-        { alignX: p5.CENTER, alignY: p5.BOTTOM },
-        { alignX: p5.RIGHT, alignY: p5.BOTTOM },
-      ];
+  //     visualTest("all alignments with single line", function (p5, screenshot) {
+  //       const alignments = [
+  //         { alignX: p5.LEFT, alignY: p5.TOP },
+  //         { alignX: p5.CENTER, alignY: p5.TOP },
+  //         { alignX: p5.RIGHT, alignY: p5.TOP },
+  //         { alignX: p5.LEFT, alignY: p5.CENTER },
+  //         { alignX: p5.CENTER, alignY: p5.CENTER },
+  //         { alignX: p5.RIGHT, alignY: p5.CENTER },
+  //         { alignX: p5.LEFT, alignY: p5.BOTTOM },
+  //         { alignX: p5.CENTER, alignY: p5.BOTTOM },
+  //         { alignX: p5.RIGHT, alignY: p5.BOTTOM },
+  //       ];
 
-      p5.createCanvas(300, 200);
-      p5.textSize(20);
-      alignments.forEach((alignment) => {
-        p5.textAlign(alignment.alignX, alignment.alignY);
-        p5.text("Single Line", 0, 0);
-        const bb = p5.textBounds("Single Line", 0, 0);
-        p5.noFill();
-        p5.stroke("red");
-        p5.rect(bb.x, bb.y, bb.w, bb.h);
-      });
-      screenshot();
-    });
+  //       p5.createCanvas(300, 200);
+  //       p5.textSize(20);
+  //       alignments.forEach((alignment) => {
+  //         p5.textAlign(alignment.alignX, alignment.alignY);
+  //         p5.text("Single Line", 0, 0);
+  //         const bb = p5.textBounds("Single Line", 0, 0);
+  //         p5.noFill();
+  //         p5.stroke("red");
+  //         p5.rect(bb.x, bb.y, bb.w, bb.h);
+  //       });
+  //       screenshot();
+  //     });
 
-    visualTest(
-      "all alignments with multi-lines and wrap word",
-      function (p5, screenshot) {
-        const alignments = [
-          { alignX: p5.LEFT, alignY: p5.TOP },
-          { alignX: p5.CENTER, alignY: p5.TOP },
-          { alignX: p5.RIGHT, alignY: p5.TOP },
-          { alignX: p5.LEFT, alignY: p5.CENTER },
-          { alignX: p5.CENTER, alignY: p5.CENTER },
-          { alignX: p5.RIGHT, alignY: p5.CENTER },
-          { alignX: p5.LEFT, alignY: p5.BOTTOM },
-          { alignX: p5.CENTER, alignY: p5.BOTTOM },
-          { alignX: p5.RIGHT, alignY: p5.BOTTOM },
-        ];
+  //     visualTest(
+  //       "all alignments with multi-lines and wrap word",
+  //       function (p5, screenshot) {
+  //         const alignments = [
+  //           { alignX: p5.LEFT, alignY: p5.TOP },
+  //           { alignX: p5.CENTER, alignY: p5.TOP },
+  //           { alignX: p5.RIGHT, alignY: p5.TOP },
+  //           { alignX: p5.LEFT, alignY: p5.CENTER },
+  //           { alignX: p5.CENTER, alignY: p5.CENTER },
+  //           { alignX: p5.RIGHT, alignY: p5.CENTER },
+  //           { alignX: p5.LEFT, alignY: p5.BOTTOM },
+  //           { alignX: p5.CENTER, alignY: p5.BOTTOM },
+  //           { alignX: p5.RIGHT, alignY: p5.BOTTOM },
+  //         ];
 
-        p5.createCanvas(300, 200);
-        p5.textSize(20);
-        p5.textWrap(p5.WORD);
+  //         p5.createCanvas(300, 200);
+  //         p5.textSize(20);
+  //         p5.textWrap(p5.WORD);
 
-        let xPos = 20;
-        let yPos = 20;
-        const boxWidth = 100;
-        const boxHeight = 60;
+  //         let xPos = 20;
+  //         let yPos = 20;
+  //         const boxWidth = 100;
+  //         const boxHeight = 60;
 
-        alignments.forEach((alignment, i) => {
-          if (i % 3 === 0 && i !== 0) {
-            yPos += 70;
-            xPos = 20;
-          }
+  //         alignments.forEach((alignment, i) => {
+  //           if (i % 3 === 0 && i !== 0) {
+  //             yPos += 70;
+  //             xPos = 20;
+  //           }
 
-          p5.textAlign(alignment.alignX, alignment.alignY);
+  //           p5.textAlign(alignment.alignX, alignment.alignY);
 
-          p5.noFill();
-          p5.stroke(200);
-          p5.rect(xPos, yPos, boxWidth, boxHeight);
+  //           p5.noFill();
+  //           p5.stroke(200);
+  //           p5.rect(xPos, yPos, boxWidth, boxHeight);
 
-          p5.fill(0);
-          p5.text(
-            "A really long text that should wrap automatically as it reaches the end of the box",
-            xPos,
-            yPos,
-            boxWidth,
-            boxHeight
-          );
-          const bb = p5.textBounds(
-            "A really long text that should wrap automatically as it reaches the end of the box",
-            xPos,
-            yPos,
-            boxWidth,
-            boxHeight
-          );
-          p5.noFill();
-          p5.stroke("red");
-          p5.rect(bb.x, bb.y, bb.w, bb.h);
+  //           p5.fill(0);
+  //           p5.text(
+  //             "A really long text that should wrap automatically as it reaches the end of the box",
+  //             xPos,
+  //             yPos,
+  //             boxWidth,
+  //             boxHeight
+  //           );
+  //           const bb = p5.textBounds(
+  //             "A really long text that should wrap automatically as it reaches the end of the box",
+  //             xPos,
+  //             yPos,
+  //             boxWidth,
+  //             boxHeight
+  //           );
+  //           p5.noFill();
+  //           p5.stroke("red");
+  //           p5.rect(bb.x, bb.y, bb.w, bb.h);
 
-          xPos += 120;
-        });
-        screenshot();
-      }
-    );
+  //           xPos += 120;
+  //         });
+  //         screenshot();
+  //       }
+  //     );
 
-    visualTest(
-      "all alignments with multi-lines and wrap char",
-      function (p5, screenshot) {
-        const alignments = [
-          { alignX: p5.LEFT, alignY: p5.TOP },
-          { alignX: p5.CENTER, alignY: p5.TOP },
-          { alignX: p5.RIGHT, alignY: p5.TOP },
-          { alignX: p5.LEFT, alignY: p5.CENTER },
-          { alignX: p5.CENTER, alignY: p5.CENTER },
-          { alignX: p5.RIGHT, alignY: p5.CENTER },
-          { alignX: p5.LEFT, alignY: p5.BOTTOM },
-          { alignX: p5.CENTER, alignY: p5.BOTTOM },
-          { alignX: p5.RIGHT, alignY: p5.BOTTOM },
-        ];
+  //     visualTest(
+  //       "all alignments with multi-lines and wrap char",
+  //       function (p5, screenshot) {
+  //         const alignments = [
+  //           { alignX: p5.LEFT, alignY: p5.TOP },
+  //           { alignX: p5.CENTER, alignY: p5.TOP },
+  //           { alignX: p5.RIGHT, alignY: p5.TOP },
+  //           { alignX: p5.LEFT, alignY: p5.CENTER },
+  //           { alignX: p5.CENTER, alignY: p5.CENTER },
+  //           { alignX: p5.RIGHT, alignY: p5.CENTER },
+  //           { alignX: p5.LEFT, alignY: p5.BOTTOM },
+  //           { alignX: p5.CENTER, alignY: p5.BOTTOM },
+  //           { alignX: p5.RIGHT, alignY: p5.BOTTOM },
+  //         ];
 
-        p5.createCanvas(300, 200);
-        p5.textSize(20);
-        p5.textWrap(p5.CHAR);
+  //         p5.createCanvas(300, 200);
+  //         p5.textSize(20);
+  //         p5.textWrap(p5.CHAR);
 
-        let xPos = 20;
-        let yPos = 20;
-        const boxWidth = 100;
-        const boxHeight = 60;
+  //         let xPos = 20;
+  //         let yPos = 20;
+  //         const boxWidth = 100;
+  //         const boxHeight = 60;
 
-        alignments.forEach((alignment, i) => {
-          if (i % 3 === 0 && i !== 0) {
-            yPos += 70;
-            xPos = 20;
-          }
+  //         alignments.forEach((alignment, i) => {
+  //           if (i % 3 === 0 && i !== 0) {
+  //             yPos += 70;
+  //             xPos = 20;
+  //           }
 
-          p5.textAlign(alignment.alignX, alignment.alignY);
+  //           p5.textAlign(alignment.alignX, alignment.alignY);
 
-          p5.noFill();
-          p5.stroke(200);
-          p5.rect(xPos, yPos, boxWidth, boxHeight);
+  //           p5.noFill();
+  //           p5.stroke(200);
+  //           p5.rect(xPos, yPos, boxWidth, boxHeight);
 
-          p5.fill(0);
-          p5.text(
-            "A really long text that should wrap automatically as it reaches the end of the box",
-            xPos,
-            yPos,
-            boxWidth,
-            boxHeight
-          );
-          const bb = p5.textBounds(
-            "A really long text that should wrap automatically as it reaches the end of the box",
-            xPos,
-            yPos,
-            boxWidth,
-            boxHeight
-          );
-          p5.noFill();
-          p5.stroke("red");
-          p5.rect(bb.x, bb.y, bb.w, bb.h);
+  //           p5.fill(0);
+  //           p5.text(
+  //             "A really long text that should wrap automatically as it reaches the end of the box",
+  //             xPos,
+  //             yPos,
+  //             boxWidth,
+  //             boxHeight
+  //           );
+  //           const bb = p5.textBounds(
+  //             "A really long text that should wrap automatically as it reaches the end of the box",
+  //             xPos,
+  //             yPos,
+  //             boxWidth,
+  //             boxHeight
+  //           );
+  //           p5.noFill();
+  //           p5.stroke("red");
+  //           p5.rect(bb.x, bb.y, bb.w, bb.h);
 
-          xPos += 120;
-        });
-        screenshot();
-      }
-    );
+  //           xPos += 120;
+  //         });
+  //         screenshot();
+  //       }
+  //     );
 
-    visualTest(
-      "all alignments with multi-line manual text",
-      function (p5, screenshot) {
-        const alignments = [
-          { alignX: p5.LEFT, alignY: p5.TOP },
-          { alignX: p5.CENTER, alignY: p5.TOP },
-          { alignX: p5.RIGHT, alignY: p5.TOP },
-          { alignX: p5.LEFT, alignY: p5.CENTER },
-          { alignX: p5.CENTER, alignY: p5.CENTER },
-          { alignX: p5.RIGHT, alignY: p5.CENTER },
-          { alignX: p5.LEFT, alignY: p5.BOTTOM },
-          { alignX: p5.CENTER, alignY: p5.BOTTOM },
-          { alignX: p5.RIGHT, alignY: p5.BOTTOM },
-        ];
+  //     visualTest(
+  //       "all alignments with multi-line manual text",
+  //       function (p5, screenshot) {
+  //         const alignments = [
+  //           { alignX: p5.LEFT, alignY: p5.TOP },
+  //           { alignX: p5.CENTER, alignY: p5.TOP },
+  //           { alignX: p5.RIGHT, alignY: p5.TOP },
+  //           { alignX: p5.LEFT, alignY: p5.CENTER },
+  //           { alignX: p5.CENTER, alignY: p5.CENTER },
+  //           { alignX: p5.RIGHT, alignY: p5.CENTER },
+  //           { alignX: p5.LEFT, alignY: p5.BOTTOM },
+  //           { alignX: p5.CENTER, alignY: p5.BOTTOM },
+  //           { alignX: p5.RIGHT, alignY: p5.BOTTOM },
+  //         ];
 
-        p5.createCanvas(300, 200);
-        p5.textSize(20);
+  //         p5.createCanvas(300, 200);
+  //         p5.textSize(20);
 
-        let xPos = 20;
-        let yPos = 20;
-        const boxWidth = 100;
-        const boxHeight = 60;
+  //         let xPos = 20;
+  //         let yPos = 20;
+  //         const boxWidth = 100;
+  //         const boxHeight = 60;
 
-        alignments.forEach((alignment, i) => {
-          if (i % 3 === 0 && i !== 0) {
-            yPos += 70;
-            xPos = 20;
-          }
+  //         alignments.forEach((alignment, i) => {
+  //           if (i % 3 === 0 && i !== 0) {
+  //             yPos += 70;
+  //             xPos = 20;
+  //           }
 
-          p5.textAlign(alignment.alignX, alignment.alignY);
+  //           p5.textAlign(alignment.alignX, alignment.alignY);
 
-          p5.noFill();
-          p5.stroke(200);
-          p5.rect(xPos, yPos, boxWidth, boxHeight);
+  //           p5.noFill();
+  //           p5.stroke(200);
+  //           p5.rect(xPos, yPos, boxWidth, boxHeight);
 
-          p5.fill(0);
-          p5.text("Line 1\nLine 2\nLine 3", xPos, yPos, boxWidth, boxHeight);
-          const bb = p5.textBounds(
-            "Line 1\nLine 2\nLine 3",
-            xPos,
-            yPos,
-            boxWidth,
-            boxHeight
-          );
-          p5.noFill();
-          p5.stroke("red");
-          p5.rect(bb.x, bb.y, bb.w, bb.h);
+  //           p5.fill(0);
+  //           p5.text("Line 1\nLine 2\nLine 3", xPos, yPos, boxWidth, boxHeight);
+  //           const bb = p5.textBounds(
+  //             "Line 1\nLine 2\nLine 3",
+  //             xPos,
+  //             yPos,
+  //             boxWidth,
+  //             boxHeight
+  //           );
+  //           p5.noFill();
+  //           p5.stroke("red");
+  //           p5.rect(bb.x, bb.y, bb.w, bb.h);
 
-          xPos += 120;
-        });
-        screenshot();
-      }
-    );
-  });
+  //           xPos += 120;
+  //         });
+  //         screenshot();
+  //       }
+  //     );
+  //   });
 
   visualSuite("textStyle", function () {
     visualTest("all text styles", function (p5, screenshot) {
