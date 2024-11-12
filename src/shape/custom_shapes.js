e38b3b98ea9f4d3a8e62975a21d271aea5ba5eb7
Greg Stanton <highermathhelp@gmail.com>
2024-11-11 23:03:32 -0700

    Set up files for custom shape refactor

--- src/shape/custom_shapes.js
+++ src/shape/custom_shapes.js
@@ -6,26 +6,415 @@
  * @requires constants
  */
 
-// declare MyClass
+// uncomment the following once you need it (otherwise VS Code complains):
+// import * as constants from '../core/constants';
+
+// ---- GENERAL CLASSES ----
+
+class Shape {
+    constructor() {
+    
+    }
+}
+
+class Contour {
+    constructor() {
+    
+    }
+}
+
+// abstract class
+class ShapePrimitive {
+    constructor() {
+    
+    }
+}
+
+class Vertex {
+    constructor() {
+    
+    }
+}
+
+// ---- PATH PRIMITIVES ----
+
+class Anchor {
+    constructor() {
+    
+    }
+}
+
+// abstract class
+class Segment extends ShapePrimitive {
+    constructor() {
+    
+    }
+}
+
+class LineSegment extends Segment {
+    constructor() {
+    
+    }
+}
+
+class BezierSegment extends Segment {
+    constructor() {
+    
+    }
+}
+
+// consider type and end modes -- see #6766)
+// may want to use separate classes, but maybe not
+class SplineSegment extends Segment {
+    constructor() {
+    
+    }
+}
+
+// ---- ISOLATED PRIMITIVES ----
+
+class Point extends ShapePrimitive {
+    constructor() {
+    
+    }
+}
+
+class Line extends ShapePrimitive {
+    constructor() {
+    
+    }
+}
+
+class Triangle extends ShapePrimitive {
+    constructor() {
+    
+    }
+}
+
+class Quad extends ShapePrimitive {
+    constructor() {
+    
+    }
+}
+
+// ---- TESSELLATION PRIMITIVES ----
+
+class TriangleFan extends ShapePrimitive {
+    constructor() {
+    
+    }
+}
+
+class TriangleStrip extends ShapePrimitive {
+    constructor() {
+    
+    }
+}
+
+class QuadStrip extends ShapePrimitive {
+    constructor() {
+    
+    }
+}
+
+// ---- PRIMITIVE VISITORS ----
+
+// abstract class
+class PrimitiveVisitor {
+    constructor() {
+    
+    }
+}
+
+// using this instead of PrimitiveToContext2DConverter for now
+class PrimitiveToPath2DConverter extends PrimitiveVisitor {
+    constructor() {
+    
+    }
+}
+
+class PrimitiveToVerticesConverter extends PrimitiveVisitor {
+    constructor() {
+    
+    }
+}
+
+class PointAtLengthGetter extends PrimitiveVisitor {
+    constructor() {
+    
+    }
+}
 
 function customShapes(p5, fn) {
+    // ---- GENERAL CLASSES ----
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.Shape = Shape;
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.Contour = Contour;
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.ShapePrimitive = ShapePrimitive;
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.Vertex = Vertex;
+
+    // ---- PATH PRIMITIVES ----
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.Anchor = Anchor;
+
+    /**
+     * @private
+     * A class responsible for...
+     */
+
+    p5.Segment = Segment;
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.LineSegment = LineSegment;
+
+    /**
+     * @private
+     * A class responsible for...
+     */
+
+    p5.BezierSegment = BezierSegment;
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.SplineSegment = SplineSegment;
+
+    // ---- ISOLATED PRIMITIVES ----
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.Point = Point;
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.Line = Line;
+
+    /**
+     * @private
+     * A class responsible for...
+     */
+
+    p5.Triangle = Triangle;
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.Quad = Quad;
+
+    // ---- TESSELLATION PRIMITIVES ----
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.TriangleFan = TriangleFan;
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.TriangleStrip = TriangleStrip;
+
+    /**
+     * @private
+     * A class responsible for...
+     */
+
+    p5.QuadStrip = QuadStrip;
+
+    // ---- PRIMITIVE VISITORS ----
+
+    /**
+     * @private
+     * A class responsible for... 
+     */
+
+    p5.PrimitiveVisitor = PrimitiveVisitor;
+
+    /**
+     * @private
+     * A class responsible for...
+     */
+
+    p5.PrimitiveToPath2DConverter = PrimitiveToPath2DConverter;
+
+    /**
+     * @private
+     * A class responsible for...
+     */
+
+    p5.PrimitiveToVerticesConverter = PrimitiveToVerticesConverter;
+
+    /**
+     * @private
+     * A class responsible for...
+     */
+
+    p5.PointAtLengthGetter = PointAtLengthGetter;
     
     // ---- FUNCTIONS ----
+
+    // Note: Code is commented out for now, to avoid conflicts with the existing implementation.
     
-    // documentation here
+    /**
+     * Top-line description
+     *
+     * More details...
+     */
 
-    // fn.myFunction = function() {
-    //     this.background('yellow'); // call an existing p5 function
-    // }; 
+    // fn.beginContour = function() {
+    //     // example of how to call an existing p5 function:
+    //     // this.background('yellow');
+    // };
+    
+    /**
+     * Top-line description
+     *
+     * More details...
+     */
 
-    // ---- CLASSES ----
+    // fn.beginShape = function() {
 
-    // documentation here
+    // };
 
-    // p5.MyClass = MyClass;
+    /**
+     * Top-line description
+     *
+     * More details...
+     */
+
+    // fn.bezierVertex = function() {
+
+    // };
+
+    /**
+     * Top-line description
+     *
+     * More details...
+     */
+
+    // fn.curveVertex = function() {
+
+    // };
+
+    /**
+     * Top-line description
+     *
+     * More details...
+     */
+
+    // fn.endContour = function() {
+
+    // };
+
+    /**
+     * Top-line description
+     *
+     * More details...
+     */
+
+    // fn.endShape = function() {
+
+    // };
+
+    /**
+     * Top-line description
+     *
+     * More details...
+     */
+
+    // fn.vertex = function() {
+
+    // };
+
+    /**
+     * Top-line description
+     *
+     * More details...
+     */
+
+    // fn.normal = function() {
+
+    // };
+
+    /**
+     * Top-line description
+     *
+     * More details...
+     */
+
+    // fn.vertexProperty = function() {
+
+    // };
 }
 
 export default customShapes;
+export {
+    Shape,
+    Contour,
+    ShapePrimitive,
+    Vertex,
+    Anchor,
+    Segment,
+    LineSegment,
+    BezierSegment,
+    SplineSegment,
+    Point,
+    Line,
+    Triangle,
+    Quad,
+    TriangleFan,
+    TriangleStrip,
+    QuadStrip,
+    PrimitiveVisitor,
+    PrimitiveToPath2DConverter,
+    PrimitiveToVerticesConverter,
+    PointAtLengthGetter
+};
 
 if (typeof p5 !== 'undefined') {
     customShapes(p5, p5.prototype);
