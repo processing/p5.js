9755b1df97cdae1e03a677e33e563f77f11c9374
Perminder <permindersingh089@gmail.com>
2024-12-12 03:46:19 +0530

    for-testing

--- src/webgl/p5.Shader.js
+++ src/webgl/p5.Shader.js
@@ -625,12 +625,11 @@ class Shader {
         'The shader being run is attached to a different context. Do you need to copy it to this context first with .copyToContext()?'
       );
     } else if (this._glProgram === 0) {
-      this._renderer = context._renderer.filterRenderer
-        ? context._renderer.filterRenderer._renderer
-        : context._renderer;
+      this._renderer = context?._renderer?.filterRenderer?._renderer || context?._renderer;
       this.init();
     }
   }
+  
   
   /**
    * Queries the active attributes for this shader and loads
