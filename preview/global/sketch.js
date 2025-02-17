9ef9cc7f7a4a2d177602247a68f79097db92b8b7
23036879 <l.plowden0620231@arts.ac.uk>
2025-02-17 12:56:46 +0000

    updating sketch

--- preview/global/sketch.js
+++ preview/global/sketch.js
@@ -1,14 +1,21 @@
 let myShader;
+p5.disableFriendlyErrors = true;
 function setup(){
   createCanvas(windowWidth, windowHeight, WEBGL);
 
   // Create and use the custom shader.
   myShader = baseMaterialShader().modify(
     () => {
-      const x = createVector3(1, 2)
-
       getWorldPosition((pos) => {
-        pos = pos.add(x);
+        let a = createVector3(1, 2, 3);
+        let b = createVector3(3,4,5);
+        a = a.add(b);
+
+        let c = a.add(b);
+        
+        c.x = b.x.add(1).sin();
+        pos = pos.add(c);
+
         return pos;
       })
     }
