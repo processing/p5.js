02d5853e9cc6d5b71df434c8a0aa0d0543cbd696
Dr. Holomorfo <holomorfo.mx@gmail.com>
2025-02-18 22:12:43 -0800

    Vector docs test

--- src/math/p5.Vector.js
+++ src/math/p5.Vector.js
@@ -52,7 +52,7 @@ class Vector {
   }
 
   /**
-   * Gets the values of the vector.
+   * Gets the values of the N-dimentional vector.
    *
    * This method returns an array of numbers that represent the vector.
    * Each number in the array corresponds to a different component of the vector,
