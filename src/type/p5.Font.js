dbe7b7a00d75bf35425a4658a83d6fceb3bb0c09
Perminder <permindersingh089@gmail.com>
2025-03-18 04:47:19 +0530

    fixes

--- src/type/p5.Font.js
+++ src/type/p5.Font.js
@@ -154,8 +154,6 @@ class Font {
       * @return {p5.Geometry} A geometry object representing the 3D model of the text.
       *
       * ```js example
-      * <div modernizr='webgl'>
-      * <code>
       * let font;
       * let geom;
       *
@@ -176,8 +174,6 @@ class Font {
       * scale(min(width, height) / 300);
       * model(geom);
       * }
-      * </code>
-      * </div>
       * ```
       * Once your 3D text is ready, you can rotate it in 3D space using orbitControl() 
       * — just click and drag with your mouse to see it from all angles!
@@ -188,126 +184,6 @@ class Font {
       * You can also choose from various fonts such as "Anton", "Montserrat", or "Source Serif", 
       * much like selecting fancy fonts in a word processor,
       * 
-      * @example
-      * <div modernizr='webgl'>
-      * <code>
-      * let font;
-      * let geom;
-      *
-      * async function setup() {
-      * createCanvas(200, 200, WEBGL);
-      * fonts = {
-      *   Anton: await loadFont('https://fonts.gstatic.com/s/anton/v25/1Ptgg87LROyAm0K08i4gS7lu.ttf'),
-      *   Montserrat: await loadFont('https://fonts.gstatic.com/s/montserrat/v29/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Ew-Y3tcoqK5.ttf'),
-      *   'Source Serif': await loadFont('https://fonts.gstatic.com/s/sourceserif4/v8/vEFy2_tTDB4M7-auWDN0ahZJW3IX2ih5nk3AucvUHf6OAVIJmeUDygwjihdqrhxXD-wGvjU.ttf'),
-      *  }
-      * geom = fonts['Anton'].textToModel("Hello World!", 50, 0, { sampleFactor: 2, extrude: 5 });
-      * geom.clearColors();
-      * geom.normalize();
-      * }
-      *
-      * function draw() {
-      * background(255);
-      * orbitControl();
-      * fill("red");
-      * strokeWeight(4);
-      * scale(min(width, height) / 300);
-      * model(geom);
-      * }
-      * </code>
-      * </div>
-      * 
-      * The generated model (a Geometry object) can be manipulated further—rotated, scaled, 
-      * or styled with shaders—to create engaging, interactive visual art.
-      * 
-      * @example
-      * <div modernizr='webgl'>
-      * <code>
-      * let geom;
-      * let fonts;
-      * let artShader;
-      * let lineShader;
-      *
-      * // Define parameters as simple variables
-      * let words = 'Word Art!';
-      * let font = 'Anton';
-      * let warp = 1;
-      * let extrude = 5;
-      * let palette = ["#ffe03d", "#fe4830", "#d33033", "#6d358a", "#1c509e", "#00953c"];
-      *
-      * async function setup() {
-      * createCanvas(windowWidth, windowHeight, WEBGL);
-      * fonts = {
-      * Anton: await loadFont('https://fonts.gstatic.com/s/anton/v25/1Ptgg87LROyAm0K08i4gS7lu.ttf'),
-      * Montserrat: await loadFont('https://fonts.gstatic.com/s/montserrat/v29/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Ew-Y3tcoqK5.ttf'),
-      * 'Source Serif': await loadFont('https://fonts.gstatic.com/s/sourceserif4/v8/vEFy2_tTDB4M7-auWDN0ahZJW3IX2ih5nk3AucvUHf6OAVIJmeUDygwjihdqrhxXD-wGvjU.ttf'),
-      * };
-      *
-      * artShader = baseMaterialShader().modify({
-      * uniforms: {
-      * 'float time': () => millis(),
-      * 'float warp': () => warp,
-      * 'float numColors': () => palette.length,
-      * 'vec3[6] colors': () => palette.flatMap((c) => [red(c)/255, green(c)/255, blue(c)/255]),
-      * },
-      * vertexDeclarations: 'out vec3 vPos;',
-      * fragmentDeclarations: 'in vec3 vPos;',
-      * 'Vertex getObjectInputs': `(Vertex inputs) {
-      * vPos = inputs.position;
-      * inputs.position.x += 5. * warp * sin(inputs.position.y * 0.1 + time * 0.001) / (1. + warp);
-      * inputs.position.y += 5. * warp * sin(inputs.position.x * 0.1 + time * 0.0009) / (1. + warp);
-      * return inputs;
-      * }`,
-      * 'vec4 getFinalColor': `(vec4 _c) {
-      * float x = vPos.x * 0.005;
-      * float a = floor(fract(x) * numColors);
-      * float b = a == numColors - 1. ? 0. : a + 1.;
-      * float t = fract(x * numColors);
-      * vec3 c = mix(colors[int(a)], colors[int(b)], t);
-      * return vec4(c, 1.);
-      * }`
-      * });
-      *
-      * lineShader = baseStrokeShader().modify({
-      * uniforms: {
-      * 'float time': () => millis(),
-      * 'float warp': () => warp,
-      * },
-      * 'StrokeVertex getObjectInputs': `(StrokeVertex inputs) {
-      * inputs.position.x += 5. * warp * sin(inputs.position.y * 0.1 + time * 0.001) / (1. + warp);
-      * inputs.position.y += 5. * warp * sin(inputs.position.x * 0.1 + time * 0.0009) / (1. + warp);
-      * return inputs;
-      * }`,
-      * });
-      * }
-      *
-      * let prevWords = '';
-      * let prevFont = '';
-      * let prevExtrude = -1;
-      *
-      * function draw() {
-      * if (words !== prevWords || prevFont !== font || prevExtrude !== extrude) {
-      * if (geom) freeGeometry(geom);
-      *
-      * geom = fonts[font].textToModel(words, 0, 50, { sampleFactor: 2, extrude });
-      * geom.clearColors();
-      * geom.normalize();
-      *
-      * prevWords = words;
-      * prevFont = font;
-      * prevExtrude = extrude;
-      * }
-      *
-      * background(255);
-      * orbitControl();
-      * shader(artShader);
-      * strokeShader(lineShader);
-      * strokeWeight(4);
-      * scale(min(width, height) / 300);
-      * model(geom);
-      * }
-      * </code>
-      * </div>
       */
   textToModel(str, x, y, width, height, options) {
     ({ width, height, options } = this._parseArgs(width, height, options));
