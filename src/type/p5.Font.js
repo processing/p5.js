2cc6e1bde9a931c0b7e51e3708dd8df2c8beffe0
Perminder <permindersingh089@gmail.com>
2025-03-18 05:26:04 +0530

    fixes

--- src/type/p5.Font.js
+++ src/type/p5.Font.js
@@ -171,21 +171,26 @@ class Font {
       * let geom;
       *
       * async function setup() {
-      * createCanvas(200, 200, WEBGL);
-      * fonts = {
-      *   Anton: await loadFont('https://fonts.gstatic.com/s/anton/v25/1Ptgg87LROyAm0K08i4gS7lu.ttf')
-      *  }
-      * geom = fonts['Anton'].textToModel("Hello World!", 50, 0, { sampleFactor: 2 });
-      * geom.clearColors();
-      * geom.normalize();
+      *   createCanvas(200, 200, WEBGL);
+      *   fonts = {
+      *     Anton: await loadFont('https://fonts.gstatic.com/s/anton/v25/1Ptgg87LROyAm0K08i4gS7lu.ttf')
+      *   };
+      *
+      *   // Create 3D geometry from text using the Anton font
+      *   geom = fonts['Anton'].textToModel("Hello", 50, 0, { sampleFactor: 2 });
+      *   geom.clearColors();
+      *   geom.normalize();
       * }
       *
       * function draw() {
-      * background(255);
-      * fill("red");
-      * strokeWeight(4);
-      * scale(min(width, height) / 300);
-      * model(geom);
+      *   background(255);
+      *   orbitControl(); // Enables mouse control to rotate the 3D text
+      *   fill("red");
+      *   strokeWeight(4);
+      *   scale(min(width, height) / 300);
+      *   model(geom);
+      *
+      *   describe('A red non-extruded "Hello" in Anton on white canvas, rotatable via mouse.');
       * }
       * </code>
       * </div>
@@ -197,24 +202,28 @@ class Font {
       * let geom;
       *
       * async function setup() {
-      * createCanvas(200, 200, WEBGL);
-      * fonts = {
-      *   Anton: await loadFont('https://fonts.gstatic.com/s/anton/v25/1Ptgg87LROyAm0K08i4gS7lu.ttf'),
-      *   Montserrat: await loadFont('https://fonts.gstatic.com/s/montserrat/v29/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Ew-Y3tcoqK5.ttf'),
-      *   'Source Serif': await loadFont('https://fonts.gstatic.com/s/sourceserif4/v8/vEFy2_tTDB4M7-auWDN0ahZJW3IX2ih5nk3AucvUHf6OAVIJmeUDygwjihdqrhxXD-wGvjU.ttf'),
-      *  }
-      * geom = fonts['Anton'].textToModel("Hello World!", 50, 0, { sampleFactor: 2, extrude: 5 });
-      * geom.clearColors();
-      * geom.normalize();
+      *   createCanvas(200, 200, WEBGL);
+      *   fonts = {
+      *     Anton: await loadFont('https://fonts.gstatic.com/s/anton/v25/1Ptgg87LROyAm0K08i4gS7lu.ttf'),
+      *     Montserrat: await loadFont('https://fonts.gstatic.com/s/montserrat/v29/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Ew-Y3tcoqK5.ttf'),
+      *     'Source Serif': await loadFont('https://fonts.gstatic.com/s/sourceserif4/v8/vEFy2_tTDB4M7-auWDN0ahZJW3IX2ih5nk3AucvUHf6OAVIJmeUDygwjihdqrhxXD-wGvjU.ttf'),
+      *   };
+      *
+      *   // You can change fonts from here.
+      *   geom = fonts['Source Serif'].textToModel("Hello", 50, 0, { sampleFactor: 2, extrude: 5 });
+      *   geom.clearColors();
+      *   geom.normalize();
       * }
       *
       * function draw() {
-      * background(255);
-      * orbitControl();
-      * fill("red");
-      * strokeWeight(4);
-      * scale(min(width, height) / 300);
-      * model(geom);
+      *   background(255);
+      *   orbitControl(); // Enables mouse control to rotate the 3D text.
+      *   fill("red");
+      *   strokeWeight(4);
+      *   scale(min(width, height) / 300);
+      *   model(geom);
+      *
+      *   describe('3D red extruded "Hello" in Source Serif on white, rotatable via mouse.');
       * }
       * </code>
       * </div>
@@ -228,56 +237,56 @@ class Font {
       * let lineShader;
       *
       * // Define parameters as simple variables
-      * let words = 'Word Art!';
+      * let words = 'HELLO';
       * let font = 'Anton';
       * let warp = 1;
       * let extrude = 5;
       * let palette = ["#ffe03d", "#fe4830", "#d33033", "#6d358a", "#1c509e", "#00953c"];
       *
       * async function setup() {
-      * createCanvas(windowWidth, windowHeight, WEBGL);
-      * fonts = {
-      * Anton: await loadFont('https://fonts.gstatic.com/s/anton/v25/1Ptgg87LROyAm0K08i4gS7lu.ttf'),
-      * Montserrat: await loadFont('https://fonts.gstatic.com/s/montserrat/v29/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Ew-Y3tcoqK5.ttf'),
-      * 'Source Serif': await loadFont('https://fonts.gstatic.com/s/sourceserif4/v8/vEFy2_tTDB4M7-auWDN0ahZJW3IX2ih5nk3AucvUHf6OAVIJmeUDygwjihdqrhxXD-wGvjU.ttf'),
-      * };
+      *   createCanvas(200, 200, WEBGL);
+      *   fonts = {
+      *     Anton: await loadFont('https://fonts.gstatic.com/s/anton/v25/1Ptgg87LROyAm0K08i4gS7lu.ttf'),
+      *     Montserrat: await loadFont('https://fonts.gstatic.com/s/montserrat/v29/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCtr6Ew-Y3tcoqK5.ttf'),
+      *     'Source Serif': await loadFont('https://fonts.gstatic.com/s/sourceserif4/v8/vEFy2_tTDB4M7-auWDN0ahZJW3IX2ih5nk3AucvUHf6OAVIJmeUDygwjihdqrhxXD-wGvjU.ttf'),
+      *   };
       *
-      * artShader = baseMaterialShader().modify({
-      * uniforms: {
-      * 'float time': () => millis(),
-      * 'float warp': () => warp,
-      * 'float numColors': () => palette.length,
-      * 'vec3[6] colors': () => palette.flatMap((c) => [red(c)/255, green(c)/255, blue(c)/255]),
-      * },
-      * vertexDeclarations: 'out vec3 vPos;',
-      * fragmentDeclarations: 'in vec3 vPos;',
-      * 'Vertex getObjectInputs': `(Vertex inputs) {
-      * vPos = inputs.position;
-      * inputs.position.x += 5. * warp * sin(inputs.position.y * 0.1 + time * 0.001) / (1. + warp);
-      * inputs.position.y += 5. * warp * sin(inputs.position.x * 0.1 + time * 0.0009) / (1. + warp);
-      * return inputs;
-      * }`,
-      * 'vec4 getFinalColor': `(vec4 _c) {
-      * float x = vPos.x * 0.005;
-      * float a = floor(fract(x) * numColors);
-      * float b = a == numColors - 1. ? 0. : a + 1.;
-      * float t = fract(x * numColors);
-      * vec3 c = mix(colors[int(a)], colors[int(b)], t);
-      * return vec4(c, 1.);
-      * }`
-      * });
+      *   artShader = baseMaterialShader().modify({
+      *     uniforms: {
+      *       'float time': () => millis(),
+      *       'float warp': () => warp,
+      *       'float numColors': () => palette.length,
+      *       'vec3[6] colors': () => palette.flatMap((c) => [red(c)/255, green(c)/255, blue(c)/255]),
+      *     },
+      *     vertexDeclarations: 'out vec3 vPos;',
+      *     fragmentDeclarations: 'in vec3 vPos;',
+      *     'Vertex getObjectInputs': `(Vertex inputs) {
+      *       vPos = inputs.position;
+      *       inputs.position.x += 5. * warp * sin(inputs.position.y * 0.1 + time * 0.001) / (1. + warp);
+      *       inputs.position.y += 5. * warp * sin(inputs.position.x * 0.1 + time * 0.0009) / (1. + warp);
+      *       return inputs;
+      *     }`,
+      *     'vec4 getFinalColor': `(vec4 _c) {
+      *       float x = vPos.x * 0.005;
+      *       float a = floor(fract(x) * numColors);
+      *       float b = a == numColors - 1. ? 0. : a + 1.;
+      *       float t = fract(x * numColors);
+      *       vec3 c = mix(colors[int(a)], colors[int(b)], t);
+      *       return vec4(c, 1.);
+      *     }`
+      *   });
       *
-      * lineShader = baseStrokeShader().modify({
-      * uniforms: {
-      * 'float time': () => millis(),
-      * 'float warp': () => warp,
-      * },
-      * 'StrokeVertex getObjectInputs': `(StrokeVertex inputs) {
-      * inputs.position.x += 5. * warp * sin(inputs.position.y * 0.1 + time * 0.001) / (1. + warp);
-      * inputs.position.y += 5. * warp * sin(inputs.position.x * 0.1 + time * 0.0009) / (1. + warp);
-      * return inputs;
-      * }`,
-      * });
+      *   lineShader = baseStrokeShader().modify({
+      *     uniforms: {
+      *       'float time': () => millis(),
+      *       'float warp': () => warp,
+      *     },
+      *     'StrokeVertex getObjectInputs': `(StrokeVertex inputs) {
+      *       inputs.position.x += 5. * warp * sin(inputs.position.y * 0.1 + time * 0.001) / (1. + warp);
+      *       inputs.position.y += 5. * warp * sin(inputs.position.x * 0.1 + time * 0.0009) / (1. + warp);
+      *       return inputs;
+      *     }`,
+      *   });
       * }
       *
       * let prevWords = '';
@@ -285,25 +294,26 @@ class Font {
       * let prevExtrude = -1;
       *
       * function draw() {
-      * if (words !== prevWords || prevFont !== font || prevExtrude !== extrude) {
-      * if (geom) freeGeometry(geom);
+      *   if (words !== prevWords || prevFont !== font || prevExtrude !== extrude) {
+      *     if (geom) freeGeometry(geom);
       *
-      * geom = fonts[font].textToModel(words, 0, 50, { sampleFactor: 2, extrude });
-      * geom.clearColors();
-      * geom.normalize();
+      *     geom = fonts[font].textToModel(words, 0, 50, { sampleFactor: 2, extrude });
+      *     geom.clearColors();
+      *     geom.normalize();
       *
-      * prevWords = words;
-      * prevFont = font;
-      * prevExtrude = extrude;
-      * }
+      *     prevWords = words;
+      *     prevFont = font;
+      *     prevExtrude = extrude;
+      *   }
       *
-      * background(255);
-      * orbitControl();
-      * shader(artShader);
-      * strokeShader(lineShader);
-      * strokeWeight(4);
-      * scale(min(width, height) / 300);
-      * model(geom);
+      *   background(255);
+      *   orbitControl();
+      *   shader(artShader);
+      *   strokeShader(lineShader);
+      *   strokeWeight(4);
+      *   scale(min(width, height) / 210);
+      *   model(geom);
+      *   describe('3D wavy with different color sets "Hello" in Anton on white canvas, rotatable via mouse.');
       * }
       * </code>
       * </div>
