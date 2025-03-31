286c404205123637f5b5623933542317fd7e4e2a
Perminder Singh <127239756+perminder-17@users.noreply.github.com>
2025-04-01 01:13:00 +0530

    removing the font object

--- src/type/p5.Font.js
+++ src/type/p5.Font.js
@@ -169,24 +169,20 @@ class Font {
       *
       * async function setup() {
       *   createCanvas(200, 200, WEBGL);
-      *   fonts = {
-      *     Anton: await loadFont('https://fonts.gstatic.com/s/anton/v25/1Ptgg87LROyAm0K08i4gS7lu.ttf')
-      *   };
+      *   font = await loadFont('https://fonts.gstatic.com/s/anton/v25/1Ptgg87LROyAm0K08i4gS7lu.ttf');
       *
-      *   // Create 3D geometry from text using the Anton font
-      *   geom = fonts['Anton'].textToModel("Hello", 50, 0, { sampleFactor: 2 });
+      *   geom = font.textToModel("Hello", 50, 0, { sampleFactor: 2 });
       *   geom.clearColors();
       *   geom.normalize();
       * }
       *
       * function draw() {
       *   background(255);
-      *   orbitControl(); // Enables mouse control to rotate the 3D text
+      *   orbitControl();
       *   fill("red");
       *   strokeWeight(4);
       *   scale(min(width, height) / 300);
       *   model(geom);
-      *
       *   describe('A red non-extruded "Hello" in Anton on white canvas, rotatable via mouse.');
       * }
       * </code>
