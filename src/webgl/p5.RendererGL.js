d19fefcb84761c719be44fd1bf7cc016252ff99e
Perminder Singh <127239756+perminder-17@users.noreply.github.com>
2025-04-18 20:42:44 +0530

    fixing setAttributes()

--- src/webgl/p5.RendererGL.js
+++ src/webgl/p5.RendererGL.js
@@ -2615,11 +2615,10 @@ function rendererGL(p5, fn) {
    * }
    * </code>
    * </div>
-   * <br>
-   * Now with the antialias attribute set to true.
-   * <br>
+   * 
    * <div>
    * <code>
+   *  // Now with the antialias attribute set to true.
    * function setup() {
    *   setAttributes('antialias', true);
    *   createCanvas(100, 100, WEBGL);
