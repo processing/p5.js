c5f9c0e1a66746964619ed214a4bb6e791d906d0
Jens B <jens.bachmayer@gmx.de>
2025-06-11 22:02:02 +0200

    fix: mitigate and cleanup recommended js rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,10 +16,15 @@ const off = 0;
 const warn = 1;
 // const error = 2;
 
+const mitigatedJsRules = Object.fromEntries(
+  Object.entries(js.configs.recommended.rules).map(([rule]) => [rule, warn])
+);
+
 /**  @type {import('eslint').Linter.RulesRecord} */
 const commonRules = {
   // https://github.com/eslint/eslint/blob/main/packages/js/src/configs/eslint-recommended.js
-  ...js.configs.recommended.rules,
+  // ...js.configs.recommended.rules,
+  ...mitigatedJsRules,
 
   // https://eslint.org/docs/latest/rules/eqeqeq
   eqeqeq: [warn, 'smart'],
@@ -33,64 +38,20 @@ const commonRules = {
   // https://eslint.org/docs/latest/rules/no-caller
   'no-caller': warn,
 
-  // https://eslint.org/docs/latest/rules/no-case-declarations
-  // @eslint recommended
-  'no-case-declarations': warn,
-
   // https://eslint.org/docs/latest/rules/no-cond-assign
   'no-cond-assign': [warn, 'except-parens'],
 
   // https://eslint.org/docs/latest/rules/no-console
   'no-console': off,
 
-  // https://eslint.org/docs/latest/rules/no-constant-binary-expression
-  // @eslint recommended
-  'no-constant-binary-expression': warn,
-
-  // https://eslint.org/docs/latest/rules/no-constant-condition
-  // @eslint recommended
-  'no-constant-condition': warn,
-
-  // https://eslint.org/docs/latest/rules/no-dupe-class-members
-  // @eslint recommended
-  'no-dupe-class-members': warn,
-
-  // https://eslint.org/docs/latest/rules/no-dupe-keys
-  // @eslint recommended
-  'no-dupe-keys': warn,
-
   // https://eslint.org/docs/latest/rules/no-empty
   'no-empty': [warn, { allowEmptyCatch: true }],
-
-  // https://eslint.org/docs/latest/rules/no-fallthrough
-  // @eslint recommended
-  'no-fallthrough': warn,
 
   // https://eslint.org/docs/latest/rules/no-prototype-builtins
   'no-prototype-builtins': off,
 
-  // https://eslint.org/docs/latest/rules/no-redeclare
-  // @eslint recommended
-  'no-redeclare': warn,
-
-  // https://eslint.org/docs/latest/rules/no-shadow-restricted-names
-  // @eslint recommended
-  'no-shadow-restricted-names': warn,
-
-  // https://eslint.org/docs/latest/rules/no-unexpected-multiline
-  // @eslint recommended
-  'no-unexpected-multiline': warn,
-
   // https://eslint.org/docs/latest/rules/no-undef
   'no-undef': off,
-
-  // https://eslint.org/docs/latest/rules/no-unreachable
-  // @eslint recommended
-  'no-unreachable': warn,
-
-  // https://eslint.org/docs/latest/rules/no-unused-private-class-members
-  // @eslint recommended
-  'no-unused-private-class-members': warn,
 
   // https://eslint.org/docs/latest/rules/no-unused-vars
   'no-unused-vars': [
@@ -102,10 +63,6 @@ const commonRules = {
 
   // https://eslint.org/docs/latest/rules/no-use-before-define
   'no-use-before-define': [warn, { functions: false }],
-
-  // https://eslint.org/docs/latest/rules/no-useless-escape
-  // @eslint recommended
-  'no-useless-escape': warn,
 
   // https://eslint.style/rules/js/arrow-parens#arrow-parens
   '@stylistic/arrow-parens': [warn, 'as-needed'],
