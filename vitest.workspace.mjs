a2580885a423ac3b3bd6f072484b134c0fa34883
Dave Pagurek <davepagurek@gmail.com>
2025-07-29 12:26:32 -0400

    Reset millis() after setup

--- vitest.workspace.mjs
+++ vitest.workspace.mjs
@@ -1,4 +1,4 @@
-import { defineWorkspace } from 'vitest/config';
+import { defineWorkspace, configDefaults } from 'vitest/config';
 import vitePluginString from 'vite-plugin-string';
 
 const plugins = [
@@ -38,7 +38,10 @@ export default defineWorkspace([
         name: 'chrome',
         provider: 'webdriverio',
         screenshotFailures: false
-      }
+      },
+      fakeTimers: {
+        toFake: [...(configDefaults.fakeTimers.toFake ?? []), 'performance'],
+      },
     }
   }
 ]);
