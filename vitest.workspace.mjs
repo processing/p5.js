88dec1b8b3a9bffda5b3e8dffdf274d74306fa35
Dave Pagurek <davepagurek@gmail.com>
2025-09-14 10:26:08 -0400

    Try chrome on windows

--- vitest.workspace.mjs
+++ vitest.workspace.mjs
@@ -23,7 +23,54 @@ export default defineWorkspace([
       ],
     },
     test: {
-      name: 'unit-tests-chrome',
+      name: 'unit-tests',
+      root: './',
+      include: [
+        './test/unit/**/*.js',
+      ],
+      exclude: [
+        './test/unit/spec.js',
+        './test/unit/assets/**/*',
+        './test/unit/visual/visualTest.js',
+        './test/unit/visual/cases/webgpu.js',
+      ],
+      testTimeout: 10000,
+      globals: true,
+      browser: {
+        enabled: true,
+        name: 'chrome',
+        provider: 'webdriverio',
+        screenshotFailures: false,
+        providerOptions: {
+          capabilities: process.env.CI ? {
+            'goog:chromeOptions': {
+              args: [
+                '--no-sandbox',
+                '--headless=new',
+                '--enable-unsafe-webgpu',
+                '--use-vulkan=swiftshader',
+                '--use-webgpu-adapter=swiftshader',
+                '--use-angle=vulkan',
+                '--no-sandbox',
+              ]
+            }
+          } : undefined
+        }
+      }
+    }
+  },
+  {
+    plugins,
+    publicDir: './test',
+    bench: {
+      name: 'bench',
+      root: './',
+      include: [
+        './test/bench/**/*.js'
+      ],
+    },
+    test: {
+      name: 'unit-tests-webgpu',
       root: './',
       include: [
         // './test/unit/**/*.js',
