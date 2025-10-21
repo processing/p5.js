7f28dcc905c916bcd6dc13cb02fac017870cb880
Dave Pagurek <davepagurek@gmail.com>
2025-10-21 13:06:46 -0400

    Fix errors in simplifyThreshold in textToPoints

--- src/type/p5.Font.js
+++ src/type/p5.Font.js
@@ -1092,6 +1092,35 @@ function pathToPoints(cmds, options, font) {
     return num;
   };
 
+  const collinear = (a, b, c, thresholdAngle) => {
+    if (!thresholdAngle) {
+      return areaTriangle(a, b, c) === 0;
+    }
+
+    if (typeof collinear.tmpPoint1 === 'undefined') {
+      collinear.tmpPoint1 = [];
+      collinear.tmpPoint2 = [];
+    }
+
+    const ab = collinear.tmpPoint1,
+      bc = collinear.tmpPoint2;
+    ab.x = b.x - a.x;
+    ab.y = b.y - a.y;
+    bc.x = c.x - b.x;
+    bc.y = c.y - b.y;
+
+    const dot = ab.x * bc.x + ab.y * bc.y,
+      magA = Math.sqrt(ab.x * ab.x + ab.y * ab.y),
+      magB = Math.sqrt(bc.x * bc.x + bc.y * bc.y),
+      angle = Math.acos(dot / (magA * magB));
+
+    return angle < thresholdAngle;
+  };
+
+  const areaTriangle = (a, b, c) => {
+    return (b[0] - a[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (b[1] - a[1]);
+  };
+
   const path = createFromCommands(arrayCommandsToObjects(cmds));
   let opts = parseOpts(options, {
     sampleFactor: 0.1,
