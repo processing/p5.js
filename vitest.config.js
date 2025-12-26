5cf37be54010a418476e0cd190d632d54df7c4b9
limzykenneth <limzy.kenneth@gmail.com>
2025-12-26 12:13:54 +0000

    Re-enable webgpu test

--- vitest.config.js
+++ vitest.config.js
@@ -16,7 +16,6 @@ export default defineConfig({
     projects: [
       {
         plugins,
-        // publicDir: './test',
         bench: {
           name: 'bench',
           root: './',
@@ -68,59 +67,59 @@ export default defineConfig({
           }
         }
       },
-      // {
-      //   plugins,
-      //   publicDir: './test',
-      //   bench: {
-      //     name: 'bench',
-      //     root: './',
-      //     include: [
-      //       './test/bench/**/*.js'
-      //     ],
-      //   },
-      //   test: {
-      //     name: 'unit-tests-webgpu',
-      //     root: './',
-      //     include: [
-      //       // './test/unit/**/*.js',
-      //       './test/unit/visual/cases/webgpu.js',
-      //       './test/unit/webgpu/*.js',
-      //     ],
-      //     exclude: [
-      //       './test/unit/spec.js',
-      //       './test/unit/assets/**/*',
-      //       './test/unit/visual/visualTest.js',
-      //       // './test/unit/visual/cases/webgpu.js',
-      //       './test/types/**/*'
-      //     ],
-      //     testTimeout: 1000,
-      //     globals: true,
-      //     browser: {
-      //       enabled: true,
-      //       name: 'chrome',
-      //       provider: 'webdriverio',
-      //       screenshotFailures: false,
-      //       providerOptions: {
-      //         capabilities: process.env.CI ? {
-      //           'goog:chromeOptions': {
-      //             args: [
-      //               '--no-sandbox',
-      //               '--headless=new',
-      //               '--enable-unsafe-webgpu',
-      //               '--use-vulkan=swiftshader',
-      //               '--use-webgpu-adapter=swiftshader',
-      //               '--use-angle=vulkan',
-      //               '--no-sandbox',
-      //             ]
-      //           }
-      //         } : undefined
-      //       }
-      //     },
-      //     fakeTimers: {
-      //       toFake: [...(configDefaults.fakeTimers.toFake ?? []), 'performance']
-      //     }
-      //   }
-      // }
+      {
+        plugins,
+        bench: {
+          name: 'bench',
+          root: './',
+          include: [
+            './test/bench/**/*.js'
+          ]
+        },
+        test: {
+          name: 'unit-tests-webgpu',
+          root: './',
+          include: [
+            // './test/unit/**/*.js',
+            './test/unit/visual/cases/webgpu.js',
+            './test/unit/webgpu/*.js'
+          ],
+          exclude: [
+            './test/unit/spec.js',
+            './test/unit/assets/**/*',
+            './test/unit/visual/visualTest.js',
+            // './test/unit/visual/cases/webgpu.js',
+            './test/types/**/*'
+          ],
+          testTimeout: 1000,
+          globals: true,
+          browser: {
+            enabled: true,
+            provider: webdriverio({
+              capabilities: process.env.CI ? {
+                'goog:chromeOptions': {
+                  args: [
+                    '--no-sandbox',
+                    '--headless=new',
+                    '--enable-unsafe-webgpu',
+                    '--use-vulkan=swiftshader',
+                    '--use-webgpu-adapter=swiftshader',
+                    '--use-angle=vulkan',
+                    '--no-sandbox'
+                  ]
+                }
+              } : undefined
+            }),
+            instances: [
+              { browser: 'chrome' }
+            ],
+            screenshotFailures: false
+          },
+          fakeTimers: {
+            toFake: [...(configDefaults.fakeTimers.toFake ?? []), 'performance']
+          }
+        }
+      }
     ]
   }
 });
