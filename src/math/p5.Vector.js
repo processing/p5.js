bd3cb736234f698c46f24ffc0ee0a4ff84eebe59
Reshma R <reshmareshu045@gmail.com>
2025-12-31 19:12:28 +0900

    fix

--- src/math/p5.Vector.js
+++ src/math/p5.Vector.js
@@ -2189,21 +2189,19 @@ class Vector {
    */
   setHeading(a) {
     if (this.isPInst) a = this._toRadians(a);
-    if (this.z !== 0) {
-      p5._friendlyError(
-        'p5.Vector.setHeading() only supports 2D vectors (z === 0). ' +
-        'For 3D or higher-dimensional vectors, use rotate() or another ' +
-        'appropriate method instead.',
-        'p5.Vector.setHeading'
-      );
+    if (this.y === undefined || (this.z !== undefined && this.z !== 0)) {
+      const p5inst = this.isPInst ? this._pInst : (typeof p5 !== 'undefined' ? p5 : null);
+      if (p5inst && p5inst._friendlyError) {
+        p5inst._friendlyError(
+          'p5.Vector.setHeading() only supports 2D vectors (z === 0). ' +
+          'For 3D or higher-dimensional vectors, use rotate() or another ' +
+          'appropriate method instead.',
+          'p5.Vector.setHeading'
+        );
+      }
       return this;
     }
     const m = this.mag();
-    if (m === 0) {
-      this.x = 0;
-      this.y = 0;
-      return this;
-    }
     this.x = m * Math.cos(a);
     this.y = m * Math.sin(a);
     return this;
