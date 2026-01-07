eece985ff5b6691605329a871f2b0d63a9982f3a
Piyushrathoree <meonpath008@gmail.com>
2026-01-07 22:31:48 +0000

    updated the hsb_test file

--- test/unit/color/hsb.js
+++ test/unit/color/hsb.js
@@ -15,13 +15,6 @@
 
 import HSBSpace from '../../../src/color/color_spaces/hsb.js';
 
-assert.arrayApproximately = function (arr1, arr2, delta, desc) {
-  assert.equal(arr1.length, arr2.length);
-  for (var i = 0; i < arr1.length; i++) {
-    assert.approximately(arr1[i], arr2[i], delta, desc);
-  }
-}
-
 suite('color/HSB Color Space', function() {
   const accuracy = 0.01;
 
