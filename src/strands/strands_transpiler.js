9584786142343f95692bd8faa521c2afd91dfba2
Dave Pagurek <davepagurek@gmail.com>
2026-01-13 13:58:06 -0500

    Fix p5.strands parsing of named function callbacks

--- src/strands/strands_transpiler.js
+++ src/strands/strands_transpiler.js
@@ -1197,7 +1197,7 @@ const ASTCallbacks = {
     recursive(ast, { varyings: {}, inControlFlow: 0 }, postOrderControlFlowTransform);
     const transpiledSource = escodegen.generate(ast);
     const scopeKeys = Object.keys(scope);
-    const match = /\(?\s*(?:function)?\s*\(([^)]*)\)\s*(?:=>)?\s*{((?:.|\n)*)}\s*;?\s*\)?/
+    const match = /\(?\s*(?:function)?\s*\w*\s*\(([^)]*)\)\s*(?:=>)?\s*{((?:.|\n)*)}\s*;?\s*\)?/
       .exec(transpiledSource);
     if (!match) {
       console.log(transpiledSource);
