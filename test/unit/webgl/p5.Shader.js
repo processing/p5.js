30fcf4db5ee11bde27d4d866dea14aec912e455a
Perminder Singh <127239756+perminder-17@users.noreply.github.com>
2026-01-26 00:06:57 +0530

    Fixing tests
    Removed redundant assertions for mxInHook and wInHook.

--- test/unit/webgl/p5.Shader.js
+++ test/unit/webgl/p5.Shader.js
@@ -456,14 +456,14 @@ suite('p5.Shader', function() {
           mxInHook = window.mouseX;
           wInHook = window.width;
           inputs.color = [1, 0, 0, 1];
+          assert.isTrue(mxInHook && mxInHook.isStrandsNode);
+          assert.isTrue(wInHook && wInHook.isStrandsNode);
           return inputs;
         });
       }, { myp5 });
 
       const mx = window.mouseX;
       const w = window.width;
-      assert.isTrue(mxInHook && mxInHook.isStrandsNode);
-      assert.isTrue(wInHook && wInHook.isStrandsNode);
       assert.isNumber(mx);
       assert.isNumber(w);
       assert.strictEqual(w, myp5.width);
