492b9e9fbe39cd5a3025b0c8d7a281900a900cc0
ksen0 <kit@processingfoundation.org>
2026-02-06 15:16:52 +0100

    Vector decorator does not use Proxy

--- src/math/patch-vector.js
+++ src/math/patch-vector.js
@@ -2,10 +2,9 @@ export default function patchVector(p5, fn, lifecycles){
   p5.decorateHelper('createVector', function(target){
     return function(...args){
       if(args.length === 0){
-        // console.log('empty call to createVector');
-        return new Proxy(new p5.Vector(0, 0, 0), {
+        return new p5.Vector(0, 0, 0), {
 
-        });
+        };
       }else{
         return target.call(this, ...args);
       }
