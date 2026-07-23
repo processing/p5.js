349458200b5bfc060f842da41716d2cd8b95042d
Roy Macdonald <macdonald.roy@gmail.com>
2026-07-22 20:31:35 -0400

    Fixed typo in quaternion multiplication

--- src/webgl/p5.Quat.js
+++ src/webgl/p5.Quat.js
@@ -41,7 +41,7 @@ class Quat {
   multiply(quat) {
 
     return new Quat(
-      this.w * quat.w - this.vec.x * quat.vec.x - this.vec.y * quat.vec.y - this.vec.z - quat.vec.z,
+      this.w * quat.w - this.vec.x * quat.vec.x - this.vec.y * quat.vec.y - this.vec.z * quat.vec.z,
       this.w * quat.vec.x + this.vec.x * quat.w + this.vec.y * quat.vec.z - this.vec.z * quat.vec.y,
       this.w * quat.vec.y - this.vec.x * quat.vec.z + this.vec.y * quat.w + this.vec.z * quat.vec.x,
       this.w * quat.vec.z + this.vec.x * quat.vec.y - this.vec.y * quat.vec.x + this.vec.z * quat.w
