746904f4a0c53240b6532a32f73ea0b917ad1545
Dave Pagurek <dave@davepagurek.com>
2026-08-23 09:15:47 -0400

    Update condition

--- src/strands/strands_transpiler.js
+++ src/strands/strands_transpiler.js
@@ -564,9 +564,13 @@ const ASTCallbacks = {
       // Only inject the variable name if the first argument isn't already a string
       if (
         node.init.arguments.length === 0 ||
-        node.init.arguments[0].type !== 'Literal' ||
-        node.init.arguments[0].type !== 'TemplateLiteral' ||
-        typeof node.init.arguments[0].value !== 'string'
+        !(
+          (
+            node.init.arguments[0].type === 'Literal' &&
+            typeof node.init.arguments[0].value === 'string'
+          ) ||
+          node.init.arguments[0].type === 'TemplateLiteral'
+        )
       ) {
         const uniformName = getOrCreateInternalShaderName(
           state.shaderNameMap,
